2ba4dbf93a0db53cbb546dafcb8866b652d40daf
rhymn <david@pnd.se>
2023-02-23 14:44:06 +0100

    song

---
+++ src/routes/hp/components/song.svelte
@@ -0,0 +1,14 @@
+<div class="anthem text-center hidden-print">
+    <audio loop="loop" controls="controls">
+        <source src="/audio/Kaddio (Give Better).mp3" type="audio/mpeg">
+    </audio>
+</div>
+
+
+<style>
+    .anthem {
+        padding-top: 2.5em;
+        padding-bottom: 2em;
+        background: #9BF linear-gradient(to top left, #9BF, #FFDBF3)
+    }
+</style>
