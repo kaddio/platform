a0d72afc162fae694b73253d1e011f435f07e699
rhymn <david@pnd.se>
2023-02-24 21:28:58 +0100

    ref

--- src/routes/hp/sv/referencesSv.svelte
+++ src/routes/hp/sv/referencesSv.svelte
@@ -1,38 +1,30 @@
-<div class="container">
+<div class="container jumbotron">
     <div class="row">
-        <div class="col-sm-1 col-xs-12">
-        </div>
-        <div class="col-sm-2 col-xs-12">
-            <center>
+        <div class="col-sm-2 col-xs-6">
             <a href="https://www.mau.se" target="_blank"><img src="/customer-logos/malmo.png" width="100"></a>
-            </center>
-            <br> <br>
         </div>
-        <div class="col-sm-2 col-xs-12">
-            <center>
+        <div class="col-sm-2 col-xs-6">
             <a href="https://www.rfsl.se" target="_blank"><img src="/customer-logos/rfsl.png" width="100"></a>
-            </center>
-            <br> <br>
         </div>
-        <div class="col-sm-2 col-xs-12">
-            <center>
+        <div class="col-sm-2 col-xs-6">
             <a href="https://twitchhealth.se/" target="_blank"><img src="/customer-logos/Twitch.png" width="100"></a>
-            </center>
-            <br> <br>
         </div>
-        <div class="col-sm-2 col-xs-12">
-            <center>
-            <a href="https://www.foreningenstorasyster.se/wordpress/" target="_blank"><img src="/customer-logos/Storasyster.jpg" width="100"></a>
-            </center>
-            <br> <br>
+        <div class="col-sm-2 col-xs-6">
+            <a href="https://www.foreningenstorasyster.se/wordpress/" target="_blank"><img src="/customer-logos/storasyster_ny.png" width="100"></a>
         </div>
-        <div class="col-sm-2 col-xs-12">
-            <center>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-2 col-xs-6">
+            <a href="https://www.sveakbt.se" target="_blank"><img src="/customer-logos/sveakbt.png" width="100"></a>
+        </div>
+        
+        <div class="col-sm-2 col-xs-6">
+            <a href="https://www.dinpsykolog.se" target="_blank"><img src="/customer-logos/dinpsykolog.png" width="100"></a>
+        </div>
+
+        <div class="col-sm-2 col-xs-6">
             <a href="https://www.iksu.se/" target="_blank"><img src="/customer-logos/IKSU Orange 2019 (kopia).png" width="100"></a>
-            </center>
-            <br> <br>
-        </div>
-        <div class="col-sm-1 col-xs-12">
         </div>
     </div>
 </div>
