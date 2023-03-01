d9bed3aa4dea11d507014eebd5a2e940ecf18cca
rhymn <david@pnd.se>
2023-03-01 20:51:43 +0100

    style

--- src/routes/hp/components/song.svelte
+++ src/routes/hp/components/song.svelte
@@ -1,12 +1,11 @@
-<div class="anthem text-center hidden-print">
+<div class="text-center hidden-print">
     <audio loop="loop" controls="controls">
         <source src="/audio/Kaddio (Give Better).mp3" type="audio/mpeg">
     </audio>
 </div>
 
-
 <style>
-    .anthem {
+    div {
         padding-top: 2.5em;
         padding-bottom: 2em;
         background: #9BF linear-gradient(to top left, #9BF, #FFDBF3)
