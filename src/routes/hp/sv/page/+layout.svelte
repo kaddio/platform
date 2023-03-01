a06848fa74005c6291864234acb048f94e33fef2
rhymn <david@pnd.se>
2023-03-01 11:34:17 +0100

    page layout

---
+++ src/routes/hp/sv/page/+layout.svelte
@@ -0,0 +1,23 @@
+
+
+<script lang="ts">
+	import Header from '../../components/header.svelte';
+    import Footer from '../../components/footer.svelte';
+	// import './styles.css';
+	// import './justify.css';
+	// import "../css/_sass/main.scss"
+
+    export let title: string;
+</script>
+
+<Header></Header>
+<div class="app">
+	<main>
+		<slot />
+	</main>
+</div>
+<Footer></Footer>
+
+<style>
+
+</style>
