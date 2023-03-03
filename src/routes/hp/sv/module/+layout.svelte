b3141b667c45c385688c5560a3c4736294ac3b29
rhymn <david@pnd.se>
2023-03-03 09:52:50 +0100

    module page layout

---
+++ src/routes/hp/sv/module/+layout.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import HeaderWhite from '../../components/header-white.svelte';
+    import Footer from '../../components/footer.svelte';
+	import ModulesSv from '../modulesSv.svelte';
+	// import './styles.css';
+	// import './justify.css';
+	// import "../css/_sass/main.scss"
+</script>
+
+<HeaderWhite></HeaderWhite>
+<div class="app">
+	<main>
+		<slot />
+	</main>
+</div>
+
+<section class="container-fluid text-center">
+    <h3 class="more-features">Fler funktioner i Kaddio</h3>
+</section>
+
+<hr>
+
+<section id="features">
+	<ModulesSv></ModulesSv>
+</section>
+
+
+<Footer></Footer>
+
+<style>
+
+</style>
