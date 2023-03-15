34f5f9492a0bed6879f3c2b187f033cf5cf2db18
rhymn <david@pnd.se>
2023-03-15 22:57:23 +0100

    flex

--- src/routes/hp/components/modulePresentation.svelte
+++ src/routes/hp/components/modulePresentation.svelte
@@ -6,11 +6,31 @@
 
 <a href="{href}">
     <aside>
-        <h4>
-            <slot name="title"></slot>
-        </h4>
-        <p>
-            <slot name="body"></slot>
-        </p>
+        <span class={iconClass}></span>
+        <article>
+            <h4><slot name="title" /></h4>
+            <p>
+                <slot name="body"></slot>
+            </p>
+    
+        </article>
     </aside>
 </a>
+
+<style>
+    aside{
+        display: flex;
+        gap: 10px;
+    }
+
+    h4{
+        font-weight: bold;
+    }
+
+    span{
+        color: #c38f9c;
+        font-size: 32px;
+        font-weight: bold;
+    }
+
+</style>
