292d1cac8b004579744d5b508b9839fdf510e13b
rhymn <david@pnd.se>
2023-03-18 17:28:10 +0100

    privacy

--- src/routes/hp/sv/page/privacy/+page.svelte
+++ src/routes/hp/sv/page/privacy/+page.svelte
@@ -1,224 +1,173 @@
-<script>
-	import Subprocessors from '../../../components/subprocessors.svelte';
-    import HeaderHero from '../../../components/header-hero.svelte';
-	import HeroBox from '../../../components/heroBox.svelte';
-</script>
 
-<HeaderHero>
-    <span slot="title">Sekretesspolicy</span>
-</HeaderHero>
+<div class="bg-white py-32 px-6 lg:px-8">
+    <div class="mx-auto max-w-3xl text-base leading-7 text-gray-700">
+      <p class="text-base font-semibold leading-7 text-indigo-600">Policy</p>
+      <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Sekretesspolicy</h1>
 
-<HeroBox variant="pink">
-    När du använder Kaddio ger du oss förtroendet att hantera dina uppgifter. Vi förklarar här vilka uppgifter vi samlar in, varför och hur vi använder dem.
-</HeroBox>
-<main>
-    <article>
-        <p>
-            Kaddio AB är ansvarigt för behandlingen av personuppgifter som samlas in i Kaddio. Vid behandling av personuppgifter
-            följer vi dataskyddsförordningen (GDPR).
-        </p>
+      <p class="mt-6 text-xl leading-8">
+        Kaddio AB är ansvarigt för behandlingen av personuppgifter som samlas in i Kaddio. Vid behandling av personuppgifter
+        följer vi dataskyddsförordningen (GDPR).
+    </p>
+
+      <div class="mt-10 max-w-2xl">
         <p>
             Samtliga personuppgifter vi behandlar är nödvändiga i avtalsförhållandet mellan Kaddio och kunden, alternativt sker
             med kundens samtycke för att tillhandahålla en så bra tjänst som möjligt. Vi sparar personuppgifterna så länge de
             behövs i vår affärsrelation med kunden och raderas därefter. Har du frågor eller är missnöjd är du välkommen att
-            höra av dig till oss eller till vårt dataskyddsombud <a href="mailto:dpo@kaddio.com">dpo@kaddio.com</a>.
+            höra av dig till oss eller till vårt dataskyddsombud <a class="font-semibold text-indigo-600" href="mailto:dpo@kaddio.com">dpo@kaddio.com</a>            
         </p>
-        
-        
-        <h4>Behandling av dina klienters personuppgifter</h4>
-        <p>
-            Kaddio har beträffande de personuppgifter avseende klienter som kund till Kaddio tillför systemet ansvar
-            enligt "Personuppgiftsbiträdesavtal" (som finns att ladda ner när ditt konto har skapats).
+    
+
+        <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">Behandling av dina klienters personuppgifter</h2>
+        <p class="mt-6">
+            Kaddio har beträffande de personuppgifter avseende klienter som kund till Kaddio tillför systemet ansvar enligt "Personuppgiftsbiträdesavtal" (som finns att ladda ner när ditt konto har skapats).
         </p>
-        
-        <p>
+
+        <p class="mt-6">
             Du som kund ska själv vidta lämpliga organisatoriska åtgärder för att uppfylla GDPR såvitt avser
             behandlingen av personuppgifter. Varje enskild kund har att bestämma var i organisationen som ansvaret för
-            dataskyddsfrågor skall ligga. Läs mer hos <a href="https://www.datainspektionen.se/dataskyddsreformen/dataskyddsombud/" rel="noreferrer" target="_blank">Datainspektionen</a>
-            om när man måste eller bör ha ett dataskyddsombud och vad ett
-            sådant ombud gör.
+            dataskyddsfrågor skall ligga. Läs mer hos Datainspektionen om när man måste eller bör ha ett dataskyddsombud och vad ett
+            sådant ombud gör.            
         </p>
-        <p>
+
+        <p class="mt-6">
             Kaddio ger dig som kund tekniska förutsättningar att uppfylla kraven i GDPR.
         </p>
-        
-        
-        <h4>Information vi samlar in</h4>
-        
-        <p>
-            <strong>Information du uppger</strong>
-        </p>
-        
-        <p> 
-            När du skapar en ny organisation ber vi dig om personuppgifter, bl.a. förnamn, efternamn och e-post
-        </p>
-        
-        <p>
-            När du använder Kaddio sparar vi information om bl.a. vilka moduler du aktiverat. Vi sparar info om när du loggat in i våra serverloggar.
-        </p>
-        
-        <h4>Så här används informationen</h4>
-        
-        <p>
-            Den information vi samlar in används för att tillhandahålla, förbättra och skydda våra tjänster.
-        </p>
-        
-        <p>
-            Kaddio använder cookies för lastbalansering och säkerhet. Inget annat.
-        </p>
-        
-        
-        <h4>Valmöjlighet</h4>
-        
-        <p>
+
+
+        <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">
+            Information vi samlar in
+        </h2>
+
+        <ul role="list" class="mt-8 max-w-xl space-y-8 text-gray-600">
+            <li class="flex gap-x-3">
+              <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+              </svg>
+              <span><strong class="font-semibold text-gray-900">Ny kund.</strong> När du skapar en ny organisation ber vi dig om personuppgifter, bl.a. förnamn, efternamn och e-post.</span>
+            </li>
+
+            <li class="flex gap-x-3">
+              <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+              </svg>
+              <span><strong class="font-semibold text-gray-900">Användning.</strong> När du använder Kaddio sparar vi information om bl.a. vilka moduler du aktiverat. Vi sparar info om när du loggat in i våra serverloggar..</span>
+            </li>
+          </ul>
+  
+
+        <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">
+            Så här används informationen
+        </h2>
+
+
+        <ul role="list" class="mt-8 max-w-xl space-y-8 text-gray-600">
+            <li class="flex gap-x-3">
+              <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+              </svg>
+              <span><strong class="font-semibold text-gray-900">Förbättring.</strong> Den information vi samlar in används för att tillhandahålla, förbättra och skydda våra tjänster.</span>
+            </li>
+
+            <li class="flex gap-x-3">
+              <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+              </svg>
+              <span><strong class="font-semibold text-gray-900">Cookies.</strong> Kaddio använder cookies för lastbalansering och säkerhet. Inget annat.</span>
+            </li>
+        </ul>
+
+        <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">
+            Valmöjlighet
+        </h2>
+
+        <p class="mt-6">
             Det är du som kund till Kaddio som väljer vilken information du vill dela med oss. De enda personuppgifter
             du måste dela med oss för att starta en organisation är för- och efternamn, e-post samt namnet på din
             organisation.
         </p>
-        
-        
-        
-        
-        <h4>Personuppgifter som behandlas</h4>
-        
-        <div class="table-responsive">
-            <table class="table">
-                <caption>
-                    <strong>x</strong> behandlare, student, administratör (hur Kaddio behandlar våra kunders
-                    personuppgifter)<br />
-                    <strong>p</strong> patient, klient (hur Kaddios kunder kan behandla sina patienters
-                    personuppgifter)<br />
-                </caption>
-        
-                <thead>
-                    <th>Personuppgift</th>
-                    <th>Identifikation i listor</th>
-                    <th>Inloggning</th>
-                    <th>SMS-påminnelser</th>
-                    <th>Bokningsbekräftelse</th>
-                    <th>Support</th>
-                    <th>Journallogg</th>
-                    <th>Fakturering</th>
-                </thead>
-        
-                <tbody>
-                    <tr>
-                        <th>Namn</th>
-                        <td>x p</td>
-                        <td></td>
-                        <td>x</td>
-                        <td></td>
-                        <td>x</td>
-                        <td>x</td>
-                        <td></td>
-                    </tr>
-        
-                    <tr>
-                        <th>Personnummer</th>
-                        <td>x p</td>
-                        <td>x<sup>1</sup> p<sup>1</sup></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <th>E-post</th>
-                        <td>x</td>
-                        <td>x p</td>
-                        <td></td>
-                        <td>x p</td>
-                        <td>x</td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-        
-                    <tr>
-                        <th>Telefonnummer</th>
-                        <td></td>
-                        <td>x<sup>2</sup> p<sup>2</sup></td>
-                        <td>p</td>
-                        <td></td>
-                        <td>x</td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-        
-                    <tr>
-                        <th>IP-adress</th>
-                        <td></td>
-                        <td>x</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>x</td>
-                        <td></td>
-                    </tr>
-        
-                    <tr>
-                        <th>Adress</th>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>x</td>
-                        <td></td>
-                        <td></td>
-                        <td>x p</td>
-                    </tr>
-        
-                    <tr>
-                        <th>Organisationsnummer</th>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>x</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        
-        <ol>
-            <li>Endast med modulen Mobilt BankID</li>
-            <li>Endast med modulen 2FA via SMS</li>
-        </ol>
-        
-        <h4>Var personuppgifter hanteras</h4>
-        <p> Dataskyddsförordningen (GDPR) gäller i hela EU/ESS och innebär att alla EU:s medlemsstater har ett
+
+        <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">
+            Var personuppgifter hanteras
+        </h2>
+
+        <p class="mt-6">
+            Dataskyddsförordningen (GDPR) gäller i hela EU/ESS och innebär att alla EU:s medlemsstater har ett
             likvärdigt skydd för personuppgifter och personlig integritet.
         </p>
-        <ul>
-            <li>Personuppgifter kan enligt GDPR behandlas fritt inom EU/EES utan begränsningar.</li>
-            <li>Utanför EU/ESS får personuppgifter hanteras förutsatt att databehandlaren följer nödvändiga
-                bestämmelser och har vidtagit lämpliga skyddsåtgärder. </li>
+
+
+
+        <ul role="list" class="mt-8 max-w-xl space-y-8 text-gray-600">
+            <li class="flex gap-x-3">
+              <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+              </svg>
+              <span>Personuppgifter kan enligt GDPR behandlas fritt inom EU/EES utan begränsningar.</span>
+            </li>
+
+            <li class="flex gap-x-3">
+              <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+              </svg>
+              <span>Utanför EU/ESS får personuppgifter hanteras förutsatt att databehandlaren följer nödvändiga
+                bestämmelser och har vidtagit lämpliga skyddsåtgärder.</span>
+            </li>
         </ul>
-        
-        <p>Samtliga företag som hanterar personuppgifter för Kaddios räkning utanför EU/ESS har vi avtal med som
-            innehåller standardavtalsklausuler som EU-kommissionen har beslutat om, vilket uppfyller skyddsnivå
-            enligt GDPR.</p>
-        
-        <p>Data för följande ändamål hanteras utanför EU/ESS (i USA):</p>
-        
-        <ul>
-            <li>System för att skicka nyhetsbrev från Kaddio till våra kunder</li>
-            <li>System för att leverera SMS till patienter som har en bokning i kalendern. (SMS-påminnelser)</li>
-            <li>System för automatiska utskick av e-post för: fakturering, inbjudan av behandlare och patienter,
-                kvitto vid bokning samt bokning- och avbokningsbekräftelse.</li>
-        </ul>
-        <p>Övrig data som innehåller personuppgifter hanteras inom EU/ESS. All lagring av data från Journal och
-            Kommunikation sker alltså inom EU/ESS.
+
+        <p class="mt-6">
+            Samtliga företag som hanterar personuppgifter för Kaddios räkning utanför EU/ESS har vi avtal med som
+    innehåller standardavtalsklausuler som EU-kommissionen har beslutat om, vilket uppfyller skyddsnivå
+    enligt GDPR.
         </p>
-        <br>
-        
-        <p>Sidan senast uppdaterad 2023-02-10.</p>
-        
-    </article>
 
-    <article>
-        <Subprocessors />        
-    </article>
+        
+        <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">
+            Data för följande ändamål hanteras utanför EU/ESS
+        </h2>
 
-</main>
+
+        <ul role="list" class="mt-8 max-w-xl space-y-8 text-gray-600">
+            <li class="flex gap-x-3">
+              <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+              </svg>
+              <span><strong class="font-semibold text-gray-900">Nyhetsbrev.</strong> System för att skicka nyhetsbrev från Kaddio till våra kunder</span>
+            </li>
+
+            <li class="flex gap-x-3">
+              <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+              </svg>
+              <span><strong class="font-semibold text-gray-900">SMS.</strong> System för att leverera SMS till patienter som har en bokning i kalendern. (SMS-påminnelser).</span>
+            </li>
+
+            <li class="flex gap-x-3">
+                <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+                </svg>
+                <span><strong class="font-semibold text-gray-900">E-post.</strong> System för automatiska utskick av e-post för: fakturering, inbjudan av behandlare och patienter,
+                    kvitto vid bokning samt bokning- och avbokningsbekräftelse.</span>
+              </li>
+          </ul>
+
+
+        <p class="mt-6">
+            Övrig data som innehåller personuppgifter hanteras inom EU/ESS. All lagring av data från Journal och Kommunikation sker inom EU/ESS.
+        </p>
+
+
+        <figure class="mt-10 border-l border-indigo-600 pl-9">
+          <blockquote class="font-semibold text-gray-900">
+            <p>“Våra kunder investerar i oss med tid, engagemang, pengar och information och anförtror oss med ett stort ansvar. Vi engagerar oss i dessa investeringar och det förklarar varför vi strävar efter att arbeta balanserat, hålla våra kostnader låga, hur vi hanterar riskfaktorer och varför vi strävar efter att växa organiskt och stabilt.”</p>
+          </blockquote>
+          <figcaption class="mt-6 flex gap-x-4">
+            <img class="h-6 w-6 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+            <div class="text-sm leading-6"><strong class="font-semibold text-gray-900">Lena Nyberg</strong> - Chief Information Officer</div>
+          </figcaption>
+        </figure>
+        
+      </div>
+    </div>
+  </div>
+  
