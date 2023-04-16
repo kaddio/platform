f43ef95646fda63a7e34957e12a7720228999389
rhymn <david@pnd.se>
2023-04-16 12:41:51 +0200

    copy

--- src/routes/hp/components/referral.svelte
+++ src/routes/hp/components/referral.svelte
@@ -24,7 +24,7 @@
 {#if show}
     <div class="flex items-center gap-x-6 bg-indigo-600 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
         <p class="text-sm leading-6 text-white">
-        <a href="#"><strong class="font-semibold">Rekommendationslänk</strong><svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>Join us in Denver from June 7 – 9 to see what’s coming next&nbsp;<span aria-hidden="true">&rarr;</span></a>
+        <a href="#"><strong class="font-semibold">Rekommendationslänk</strong><svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>Du surfar med Claras rekommendationslänk&nbsp;<span aria-hidden="true">&rarr;</span></a>
         </p>
         <div class="flex flex-1 justify-end">
         <button type="button" class="-m-3 p-3 focus-visible:outline-offset-[-4px]">
