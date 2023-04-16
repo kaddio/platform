4df70559e61de8a3008a640bb3b27cb30ab218e6
rhymn <david@pnd.se>
2023-04-16 12:40:44 +0200

    1 month

--- src/routes/hp/sv/pricesSv.svelte
+++ src/routes/hp/sv/pricesSv.svelte
@@ -47,6 +47,6 @@
 
 <Price currency="SEK" modules="{modules}">
 	<span slot="title">Betala bara för vad du använder.</span>
-	<span slot="sub">Ingen startkostnad. 14 dagar kostnadsfritt.</span>
+	<span slot="sub">Ingen startkostnad. 1 månad kostnadsfritt.</span>
 
 </Price>
