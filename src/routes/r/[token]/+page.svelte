84e0753afeab8563c9e129e5624a230c44ce216c
rhymn <david@pnd.se>
2023-04-17 00:15:04 +0200

    working

---
+++ src/routes/r/[token]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
+	import { token } from '../../../stores.js';
+
+    export let data;
+
+    if(data.referrerData){
+        $token = data.referrerData;
+    }
+
+    if(browser){
+        goto('/hp/sv')
+    }
+
+</script>
