0e1582fac2008660cc4a49a828ee5a6e8b6a9029
rhymn <david@pnd.se>
2023-04-17 12:20:05 +0200

    can jump to price

--- src/routes/hp/components/price.svelte
+++ src/routes/hp/components/price.svelte
@@ -14,7 +14,7 @@
 </script>
 
 
-<div class="bg-white py-24 sm:py-32">
+<div class="bg-white py-24 sm:py-32" id="price">
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <div class="grid grid-cols-1 items-center gap-x-8 gap-y-16 lg:grid-cols-2">
       <div class="mx-auto w-full max-w-xl lg:mx-0">
