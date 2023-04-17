c8a101565fb392cc9a5a772c852a922e17a63900
rhymn <david@pnd.se>
2023-04-17 10:58:23 +0200

    sessionStorage

--- src/stores.js
+++ src/stores.js
@@ -1,3 +1,20 @@
+import { browser } from "$app/environment";
 import { writable } from "svelte/store";
 
-export const token = writable();
+export let token = writable();
+
+if(browser){
+    if(sessionStorage.getItem('token')){
+        token = writable(JSON.parse(sessionStorage.getItem('token')));
+    }
+
+    token.subscribe(function(val){
+        if(val){
+            sessionStorage.setItem('token', JSON.stringify(val));
+        }
+
+        else{
+            sessionStorage.removeItem('token');
+        }
+    });
+}
