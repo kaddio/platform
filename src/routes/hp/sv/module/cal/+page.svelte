0fec303adc45541c08c69140be06ac25b6423609
rhymn <david@pnd.se>
2023-04-19 08:55:15 +0200

    personalliggare

--- src/routes/hp/sv/module/cal/+page.svelte
+++ src/routes/hp/sv/module/cal/+page.svelte
@@ -62,53 +62,11 @@
 			Skapa dina egna mallar för journalanteckningar så de passar din verksamhet. Välj din egna sökord/rubriker. Använd malltext, fraser, filuppladdning och mer.
 		</span>
 	</Qa>
-	
-	<Qa>
-		<span slot="q">Journalformulär</span>
-		<span slot="a">
-			Skapa dina egna formulär eller använd vanligt förekommande standardformulär direkt från Kaddio. Skicka skattningsformulär och hälsodeklarationer via e-post och sms. Journalformulär kan skickas automatiskt i samband med bokning eller manuellt vid behov. Svaren sparas i journalen.
-		</span>
-	</Qa>
 
 	<Qa>
-		<span slot="q">Brev och remisser</span>
+		<span slot="q">Personalliggare</span>
 		<span slot="a">
-			Skapa brev, remisser, remissvar och intyg. Skicka med post, via Kaddios chat eller ladda ner som PDF.
-		</span>
-	</Qa>
-
-	<Qa>
-		<span slot="q">ICD-10</span>
-		<span slot="a">
-			Sök med kod eller fritext och lägg till diagnoser i varje journalanteckning.
-		</span>
-	</Qa>
-
-	<Qa>
-		<span slot="q">Behörigheter</span>
-		<span slot="a">
-			Välj om alla i verksamheten ska kunna läsa klienters journal eller om behörigheten ska begränsas till vissa behandlare.
-		</span>
-	</Qa>
-
-	<Qa>
-		<span slot="q">Bild</span>
-		<span slot="a">
-			Rita på fram- och baksida av en människokropp för att dokumentera till exempel var klienten upplever smärta.
-		</span>
-	</Qa>
-
-	<Qa>
-		<span slot="q">Lista osignerade anteckningar</span>
-		<span slot="a">
-			Håll koll på anteckningar som väntar på att bli signerade.
-		</span>
-	</Qa>
-
-	<Qa>
-		<span slot="q">Exportera/skriv ut</span>
-		<span slot="a">
-			Exportera och skriv ut enskild journalanteckning eller hela journalen till PDF.
+			Kaddio kan användas som digital personalliggare genom Arbetstid i Kalendern, med hjälp av Loggning uppfylls även Skatteverkets regler kring kontroll.
 		</span>
 	</Qa>
 
