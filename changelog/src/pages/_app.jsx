81defe44cdf09a5bb70eb209a85048489507f6a4
rhymn <david@pnd.se>
2023-04-29 11:10:11 +0200

    changelog

---
+++ changelog/src/pages/_app.jsx
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import { ThemeProvider } from 'next-themes'
+import { MDXProvider } from '@mdx-js/react'
+
+import { Layout } from '@/components/Layout'
+import * as mdxComponents from '@/components/mdx'
+
+import '@/styles/tailwind.css'
+import 'focus-visible'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Kaddio Changelog</title>
+        <meta
+          name="description"
+          content="Kaddio is the complete health platform. With Booking, Medical Records, Video, Invoicing and much more - Control is at your fingertips. This is the Changelog that describes all our updates."
+        />
+      </Head>
+      <ThemeProvider attribute="class" disableTransitionOnChange>
+        <MDXProvider components={mdxComponents}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </MDXProvider>
+      </ThemeProvider>
+    </>
+  )
+}
