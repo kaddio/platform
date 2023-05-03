4826fbef8d2e7b7e0b589cdcdeaf2eeaeccc9068
rhymn <david@pnd.se>
2023-05-03 10:05:29 +0200

    slogan

--- web/src/routes/hp/sv/modulesSv.svelte
+++ web/src/routes/hp/sv/modulesSv.svelte
@@ -7,8 +7,8 @@
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <div class="mx-auto max-w-2xl lg:text-center">
       <h2 class="text-base font-semibold leading-7 text-purple-600">Kaddio</h2>
-      <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Ge(r) bättre hjälp</p>
-      <p class="mt-6 text-lg leading-8 text-gray-600">Gör som du vill med Kaddio. Kaddio är så enkelt att använda att du knappt behöver tänka när du använder det, den energi kan du lägga på t.ex. dina kunder. Genom att alla funktioner finns i samma enkla system slipper du trötta ut frontalloben i onödan. Din hjärna kommer att älska det.</p>
+      <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Gör som du vill / Do(es) what you want</p>
+      <p class="mt-6 text-lg leading-8 text-gray-600">Kaddio är så enkelt att använda att du knappt behöver tänka när du använder det, den energi kan du lägga på t.ex. dina kunder. Genom att alla funktioner finns i samma enkla system slipper du trötta ut frontalloben i onödan. Din hjärna kommer att älska det.</p>
     </div>
     <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
       <dl class="grid max-w-xl grid-cols-1 gap-y-10 gap-x-8 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
