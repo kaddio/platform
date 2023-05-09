d7effa70b6ba14ce0c0fba83696a969f821a9c1b
rhymn <david@pnd.se>
2023-05-09 10:34:46 +0200

    Kaddio Reports feature

---
+++ web/src/routes/hp/sv/feature/reports/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+	import Qa from "../../../components/qa.svelte";
+	import ModuleFeature from "../../../components/moduleFeature.svelte";
+	import ModuleSection from "../../../components/moduleSection.svelte";
+	import FaqSection from "../../../components/faqSection.svelte";
+</script>
+
+<svelte:head>
+	<title>Kaddio - Rapporter</title>
+	<meta name="description" content="..." />
+</svelte:head>
+
+<ModuleSection>
+	<span slot="title">Kaddio Rapporter</span>
+	<span slot="sub">...</span>
+
+	<ModuleFeature>
+		<span slot="title">Förbered</span>
+		<span slot="body">
+			... Kaddio kan automatiskt skicka de formulär eller hälsodeklarationen du vill ha ifyllda innan besöket.
+		</span>
+	</ModuleFeature>
+
+	<ModuleFeature>
+		<span slot="title">Mät</span>
+		<span slot="body">
+			...
+		</span>
+	</ModuleFeature>
+
+	<ModuleFeature>
+		<span slot="title">Återkoppla</span>
+		<span slot="body">
+			...
+		</span>
+	</ModuleFeature>
+</ModuleSection>
+  
+
+<FaqSection>
+	<span slot="title">Detaljer</span>
+
+	<Qa>
+		<span slot="q">Fråga</span>
+		<span slot="a">
+			Svar
+		</span>
+	</Qa>
+
+</FaqSection>
