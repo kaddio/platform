4ff544e0839b6a8b8fd0f46fcafff2da65a11e49
rhymn <david@pnd.se>
2023-05-10 11:19:41 +0200

    texter

--- web/src/routes/hp/sv/+page.svelte
+++ web/src/routes/hp/sv/+page.svelte
@@ -48,7 +48,7 @@
 			</div>
 			<div class="text-center">
 			<h1 class="text-4xl font-normal tracking-tight text-white sm:text-6xl">Kaddio är allt du behöver</h1>
-			<p class="mt-6 text-lg leading-8 font-light text-white">Kaddio är ett webbaserat allt-i-allo-system för dig som hjälper andra. Kaddio är så enkelt att du slipper trötta ut frontalloben i onödan. Din hjärna kommer att älska det.</p>
+			<p class="mt-6 text-lg leading-8 font-light text-white">Webbaserat allt-i-allo-system för dig som hjälper andra.</p>
 			<div class="mt-10 flex items-center justify-center gap-x-6">
 				<a href="https://demo.kaddio.com/login" target="_blank" rel="noreferrer" class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-oldpink shadow-sm hover:bg-oldpink hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-400">Prova demo</a>
 				<a href="/hp/sv/create" class="text-sm font-semibold leading-6 text-white">Skapa konto <span aria-hidden="true">→</span></a>
