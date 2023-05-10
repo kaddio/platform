2022d43066f6165a74fe68c74b8307c70a954322
rhymn <david@pnd.se>
2023-05-10 11:38:40 +0200

    booking

--- web/src/routes/hp/sv/feature/cal/+page.svelte
+++ web/src/routes/hp/sv/feature/cal/+page.svelte
@@ -41,6 +41,102 @@
 	<span slot="title">Detaljer</span>
 
 	<Qa>
+		<span slot="q">Kalender</span>
+		<span slot="a">
+			Administrera bokningar i kalendern, till exempel ändra eller avboka tid. I kalendern ser du bokningar sida vid sida med möten och andra aktiviteter.
+		</span>
+	</Qa>
+
+	<Qa>
+		<span slot="q">Öppen klientbokning</span>
+		<span slot="a">
+			Klienten bokar tid själv på en publik websida.
+		</span>
+	</Qa>
+
+	<Qa>
+		<span slot="q">Bokning med Mobilt BankID</span>
+		<span slot="a">
+			Klienten loggar in i Kaddio med Mobilt BankID för att kunna boka tid. Passar dig som vill säkerställa identiteten på den som bokar.
+		</span>
+	</Qa>
+
+	<Qa>
+		<span slot="q">SMS-påminnelser</span>
+		<span slot="a">
+			Välj om och när SMS-påminnelser skickas inför och efter bokade tid.
+		</span>
+	</Qa>
+
+	<Qa>
+		<span slot="q">Formulär</span>
+		<span slot="a">
+			Skicka formulär automatiskt i samband med bokning, till exempel skattningsformulär och hälsodeklaration.
+		</span>
+	</Qa>
+
+	<Qa>
+		<span slot="q">Behandlingsformer</span>
+		<span slot="a">
+			Erbjud olika typer av behandlingsformer. Koppla behandlingsformer till olika behandlare. Anpassa sms och mailutskick beroende på behandling.
+		</span>
+	</Qa>
+
+	<Qa>
+		<span slot="q">Gruppbokning</span>
+		<span slot="a">
+			Lägg upp tider som är bokningsbara för fler än en deltagare.
+		</span>
+	</Qa>
+
+	<Qa>
+		<span slot="q">Tidsoptimering</span>
+		<span slot="a">
+			Undvik onödiga hål i schemat. Kaddio optimerar bokningsbara tider utifrån behandlingformernas längd.
+		</span>
+	</Qa>
+
+	<Qa>
+		<span slot="q">Väntelistor</span>
+		<span slot="a">
+			Skapa väntelistor med datum, anteckningar och delegeringsfunktion.
+		</span>
+	</Qa>
+
+	<Qa>
+		<span slot="q">Kortbetalning vid bokning</span>
+		<span slot="a">
+			Låt klienten betala med kort direkt vid bokning online.
+		</span>
+	</Qa>
+
+	<Qa>
+		<span slot="q">Fakturering</span>
+		<span slot="a">
+			Automatiska faktureringsunderlag skapas utifrån bokningar.
+		</span>
+	</Qa>
+	<Qa>
+		<span slot="q">Visa bokning på din hemsida</span>
+		<span slot="a">
+			Integrera bokning direkt på din hemsida.
+		</span>
+	</Qa>
+	<Qa>
+		<span slot="q">Platser</span>
+		<span slot="a">
+			Kaddio har stöd för om du har mottagningar på flera platser. Information i mail och sms-påminnelser anpassas beroende på plats.
+		</span>
+	</Qa>
+
+	<Qa>
+		<span slot="q">Rumsbokning</span>
+		<span slot="a">
+			Schemaläggning för dina rum. Kaddio ser till att behandlingar bara bokas in när det finns tillgängliga rum.
+		</span>
+	</Qa>
+
+	<Qa>
 		<span slot="q">Personalliggare</span>
 		<span slot="a">
 			Kaddio kan användas som digital personalliggare genom Arbetstid i Kalendern, med hjälp av Loggning uppfylls även Skatteverkets regler kring kontroll.
@@ -49,13 +145,6 @@
 
 	<Qa>
 		<span slot="q">Loggning</span>
-		<span slot="a">
-			Läsning och redigering loggas. Loggen visar vad som gjorts med en viss klients journal och av vem, när och från vilken ip-adress.
-		</span>
-	</Qa>
-
-	<Qa>
-		<span slot="q">API</span>
 		<span slot="a">
 			Läsning och redigering loggas. Loggen visar vad som gjorts med en viss klients journal och av vem, när och från vilken ip-adress.
 		</span>
