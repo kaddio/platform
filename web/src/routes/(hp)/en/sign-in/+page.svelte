00df8a00eef8a6ed1956504941806b75f1c3955a
rhymn <david@pnd.se>
2023-05-12 15:17:52 +0200

    lang

---
+++ web/src/routes/(hp)/en/sign-in/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { setContext } from "svelte";
+	setContext('lang', 'en');
+
+	import LoginSection from "../../components/loginSection.svelte";
+
+</script>
+
+<svelte:head>
+	<title>Kaddio - Login</title>
+	<meta name="description" content="Login to Kaddio" />
+</svelte:head>
+
+<LoginSection />
---
+++ web/src/routes/(hp)/es/create/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">	
+	import { setContext } from 'svelte';
+	import Footer from '../../components/footer.svelte';
+	import CreateAccount from '../../components/createAccount.svelte';
+	import Nav from '../../components/nav.svelte';
+	
+	setContext('lang', 'es');
+
+</script>
+
+<svelte:head>
+	<title>Kaddio - skapa konto</title>
+	<meta name="description" content="Skapa konto" />
+</svelte:head>
+
+<Nav />
+<CreateAccount />
+<Footer />
