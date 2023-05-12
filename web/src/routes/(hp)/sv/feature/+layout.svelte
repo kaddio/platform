bdf8237a7f4f40ee3ecd5ff5ab00392334589f55
rhymn <david@pnd.se>
2023-05-12 11:48:53 +0200

    header

--- web/src/routes/(hp)/sv/feature/+layout.svelte
+++ web/src/routes/(hp)/sv/feature/+layout.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+	import { setContext } from 'svelte';
 	import Footer from '../../components/footer.svelte';
-	import HeaderSv from '../headerSv.svelte';
+	import Nav from '../../components/nav.svelte';
+	setContext('lang', 'sv');
 </script>
 
-<HeaderSv />
+<Nav />
 
 <slot />
-
 
 <Footer />
