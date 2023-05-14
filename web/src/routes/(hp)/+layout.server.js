6c3c134766c0df7cad07708c7bc3530e7ebf4248
rhymn <david@pnd.se>
2023-05-14 19:09:31 +0200

    value

--- web/src/routes/(hp)/+layout.server.js
+++ web/src/routes/(hp)/+layout.server.js
@@ -2,7 +2,7 @@ export function load({request, setHeaders, cookies}){
     console.log(request.headers);
 
     if(request.headers['cf-ipcountry']){
-        const geo = request.headers['cf-ipcountry'];
+        const geo = request.headers['cf-ipcountry'].value;
 
         cookies.set('geo', geo);
     }
