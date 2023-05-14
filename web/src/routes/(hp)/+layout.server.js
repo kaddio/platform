d20a72232992df139a3b83617b1748ebefc547e3
rhymn <david@pnd.se>
2023-05-14 19:09:55 +0200

    log

--- web/src/routes/(hp)/+layout.server.js
+++ web/src/routes/(hp)/+layout.server.js
@@ -2,6 +2,7 @@ export function load({request, setHeaders, cookies}){
     console.log(request.headers);
 
     if(request.headers['cf-ipcountry']){
+        console.log('Got cookie!')
         const geo = request.headers['cf-ipcountry'].value;
 
         cookies.set('geo', geo);
