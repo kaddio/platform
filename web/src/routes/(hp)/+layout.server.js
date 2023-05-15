8074089a8b2b3b2f1e10a9b4ef8a511066abc83c
rhymn <david@pnd.se>
2023-05-15 09:08:18 +0200

    comment

--- web/src/routes/(hp)/+layout.server.js
+++ web/src/routes/(hp)/+layout.server.js
@@ -1,9 +1,7 @@
 export function load({request}){
 
-    // Cloudflare should set this header
-    let geo = request.headers.get('cf-ipcountry');
-
-    // geo = 'CR';
+    // Cloudflare should set this header https://developers.cloudflare.com/support/network/configuring-ip-geolocation/
+    const geo = request.headers.get('cf-ipcountry');
 
     return {
         geo 
