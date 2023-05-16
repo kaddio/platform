e390b4da6dba04d9e0cfe03cb2c68c8653579dda
rhymn <david@pnd.se>
2023-05-17 00:10:20 +0200

    communication

--- web/src/routes/(hp)/en/feature/communication/+page.svelte
+++ web/src/routes/(hp)/en/feature/communication/+page.svelte
@@ -6,61 +6,60 @@
 </script>
 
 <svelte:head>
-	<title>Kaddio - Kommunikation</title>
-	<meta name="description" content="Chat och video för upp till 500 samtidiga deltagare" />
+	<title>Kaddio - Communication</title>
+	<meta name="description" content="Communicate securely and encrypted via chat or video. With two or 500 participants." />
 </svelte:head>
 
 <ModuleSection>
-	<span slot="title">Kaddio Kommunikation</span>
-	<span slot="sub">Chat och video för upp till 500 samtidiga deltagare</span>
+	<span slot="title">Kaddio Communication</span>
+	<span slot="sub">Communicate securely and encrypted via chat or video. With two or 500 participants.</span>
 
-	<ModuleFeature>
-		<span slot="title">Automatisera</span>
+	<!-- <ModuleFeature>
+		<span slot="title">Secure</span>
 		<span slot="body">
 			Automatisera ditt schema och låt dina kunder boka och betala själva eller ta kontroll över varje minut. Bokningstyper kan konfigureras i detalj och detta hjälper dig sen i bokningen så att det blir lätt där. Automatiskta påminnelser
 		</span>
 	</ModuleFeature>
 
 	<ModuleFeature>
-		<span slot="title">Interagera</span>
+		<span slot="title">Simple</span>
 		<span slot="body">
 			Det är enkelt att byta till video..
 		</span>
 	</ModuleFeature>
 
 	<ModuleFeature>
-		<span slot="title">Och du kan växa med Kaddio</span>
+		<span slot="title">Integrated</span>
 		<span slot="body">
 			Kaddio Video har stöd för minst 500 deltagare samtidigt.
 		</span>
-	</ModuleFeature>
+	</ModuleFeature> -->
 </ModuleSection>
   
 
 <FaqSection>
-	<span slot="title">Detaljer</span>
+	<span slot="title">Details</span>
 
 	<Qa>
-		<span slot="q">Skärmdelning</span>
+		<span slot="q">Screen sharing</span>
 		<span slot="a">
-			Dela vad du ser
+			Share what you see
 		</span>
 	</Qa>
 
 	<Qa>
-		<span slot="q">Spela in</span>
+		<span slot="q">Record</span>
 		<span slot="a">
-			Videosamtal är inspelningsbara, videon laddas upp i konversationen och är synlig för behandlare i 24 timmar.
+			Video calls are recordable, the video is uploaded in the conversation and is visible to handlers for 24 hours.
 		</span>
 	</Qa>
 
 	<Qa>
-		<span slot="q">Emojis och reaktioner</span>
+		<span slot="q">Emojis and reactions</span>
 		<span slot="a">
-			Kanske inte det mest meningsfulla, men alla andra stora videoleverantörer har ju det så atte..
+			Maybe not the most meaningful, but all the other major video providers have it right?
 		</span>
 	</Qa>
-
 
 
 </FaqSection>
