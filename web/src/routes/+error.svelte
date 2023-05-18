27acadc0e77c42bd5f6dd5aa79b8ef4216c5915d
rhymn <david@pnd.se>
2023-05-18 22:21:37 +0200

    404

--- web/src/routes/+error.svelte
+++ web/src/routes/+error.svelte
@@ -1,3 +1,6 @@
+<script>
+	import '/src/app.css';
+</script>
 
 <main class="relative isolate min-h-full">
     <img src="https://images.unsplash.com/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover object-top">
@@ -6,7 +9,7 @@
       <h1 class="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">Page not found</h1>
       <p class="mt-4 text-base text-white/70 sm:mt-6">Sorry, we couldn’t find the page you’re looking for.</p>
       <div class="mt-10 flex justify-center">
-        <a href="/sv" class="text-sm font-semibold leading-7 text-white"><span aria-hidden="true">&larr;</span> Back to home</a>
+        <a href="/" class="text-sm font-semibold leading-7 text-white"><span aria-hidden="true">&larr;</span> Back to home</a>
       </div>
     </div>
   </main>
