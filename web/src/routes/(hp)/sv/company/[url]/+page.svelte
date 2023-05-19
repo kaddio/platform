5ad80372f7a7dbfe1f08d2d3a8cd66a9b828d82a
rhymn <david@pnd.se>
2023-05-19 22:29:47 +0200

    company structured data draft

--- web/src/routes/(hp)/sv/company/[url]/+page.svelte
+++ web/src/routes/(hp)/sv/company/[url]/+page.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
-    export let data;
+	import { websiteSchema } from "$lib/json-ld";
+	import LdTag from "$lib/components/LDTag.svelte";
+	import { json } from "@sveltejs/kit";
 
-    // if(data.org){
-    // }
+    export let data;
+    let schema = websiteSchema;
+
+    if(data.orgData){
+        schema = {
+            ...websiteSchema,
+            name: "Kaddio ...",
+            description: `This is Kadd`
+        }
+    }
 
 </script>
 
-{JSON.stringify(data.orgData)}
+<LdTag schema={schema}></LdTag>
+
+<svelte:head>
+	<title>Kaddio - {data.orgData}</title>
+	<meta name="description" content="..." />
+</svelte:head>
+
+
+{@html JSON.stringify(data.orgData)}
