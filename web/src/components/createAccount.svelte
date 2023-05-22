f1eda22e7e7756085ff202dc670adbd642197d85
rhymn <david@pnd.se>
2023-05-22 21:05:56 +0200

    .kaddio.com

--- web/src/components/createAccount.svelte
+++ web/src/components/createAccount.svelte
@@ -100,36 +100,39 @@
                 <div>
                 <label for="firstname" class="block text-sm font-semibold leading-6 text-gray-900">{ $_('Förnamn')}</label>
                 <div class="mt-2.5">
-                    <input type="text" name="firstname" id="firstname" autocomplete="given-name" class="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-600 sm:text-sm sm:leading-6">
+                    <input type="text" name="firstname" id="firstname" autocomplete="given-name" class="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-oldpink sm:text-sm sm:leading-6">
                 </div>
                 </div>
                 <div>
                 <label for="lastname" class="block text-sm font-semibold leading-6 text-gray-900">{ $_('Efternamn')}</label>
                 <div class="mt-2.5">
-                    <input type="text" name="lastname" id="lastname" autocomplete="family-name" class="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-600 sm:text-sm sm:leading-6">
+                    <input type="text" name="lastname" id="lastname" autocomplete="family-name" class="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-oldpink sm:text-sm sm:leading-6">
                 </div>
                 </div>
 
                 <div class="sm:col-span-2">
                     <label for="orgname" class="block text-sm font-semibold leading-6 text-gray-900">{ $_('Företagsnamn')}</label>
                     <div class="mt-2.5">
-                        <input bind:value={orgName} type="text" name="orgname" id="orgname" autocomplete="organization" class="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-600 sm:text-sm sm:leading-6">
+                        <input bind:value={orgName} type="text" name="orgname" id="orgname" autocomplete="organization" class="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-oldpink sm:text-sm sm:leading-6">
                     </div>
                 </div>
 
                 <input type="hidden" name="create" value="">
 
-                <div class="sm:col-span-2">
+
+                <div class="rounded-md shadow-sm sm:col-span-2">
                     <label for="orgurl" class="block text-sm font-semibold leading-6 text-gray-900">URL</label>
-                    <div class="mt-2.5">
-                        <input bind:value={url} type="text" name="orgurl" id="orgurl" class="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-600 sm:text-sm sm:leading-6">
+                    <div class="mt-2.5 flex">
+                        <input type="text" bind:value={url} name="orgurl" id="orgurl" autocomplete="url" class="block w-full min-w-0 flex-1 rounded-none rounded-l-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-oldpink sm:text-sm sm:leading-6">
+                        <span class="inline-flex items-center rounded-r-md border border-l-0 border-gray-300 px-3 text-gray-500 sm:text-sm">.kaddio.com</span>
                     </div>
                 </div>
+        
 
                 <div class="sm:col-span-2">
                 <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">{ $_('E-post')}</label>
                 <div class="mt-2.5">
-                    <input bind:value={email} type="email" name="email" id="email" autocomplete="email" class="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-600 sm:text-sm sm:leading-6">
+                    <input bind:value={email} type="email" name="email" id="email" autocomplete="email" class="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-oldpink sm:text-sm sm:leading-6">
                 </div>
                 </div>
 
@@ -137,7 +140,7 @@
 
                     <label for="countrycode" class="block text-sm font-semibold leading-6 text-gray-900">{ $_('Land')}</label>
 
-                    <select bind:value={country} id="countrycode" name="countrycode" class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-purple-600 sm:text-sm sm:leading-6">
+                    <select bind:value={country} id="countrycode" name="countrycode" class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-oldpink sm:text-sm sm:leading-6">
                         {#each countries as country}
                             <option value={country.name}>{$_(country.label)}</option>
                         {/each}
@@ -154,7 +157,7 @@
             </div>
 
             <div class="mt-10">
-                <button disabled={!submitIsEnabled} type="submit" class="disabled:opacity-75 block w-full rounded-md bg-oldpink px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-oldpinkdarker focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600">{ $_('Skapa konto')}</button>
+                <button disabled={!submitIsEnabled} type="submit" class="disabled:opacity-75 block w-full rounded-md bg-oldpink px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-oldpinkdarker focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-oldpink">{ $_('Skapa konto')}</button>
             </div>
 
             {#if fail}
