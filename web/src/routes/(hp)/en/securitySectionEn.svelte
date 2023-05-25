f68dac4bcd164564d7e6fc22fbe24320531715e5
Agustin Martinez <agustin.ok@icloud.com>
2023-05-25 11:46:26 +0200

    trans

--- web/src/routes/(hp)/en/securitySectionEn.svelte
+++ web/src/routes/(hp)/en/securitySectionEn.svelte
@@ -2,7 +2,7 @@
 	import SecuritySection from "$components/securitySection.svelte";
 
 </script>
-<SecuritySection src="https://ayuda.kaddio.com/help">
+<SecuritySection src="https://help.kaddio.com/help">
 	<span slot="title">Handle sensitive date securely</span>
 	<span slot="subtitle">Everything in Kaddio is encrypted. Kaddio supports multi-factor authentication via Mobile BankID and one-time password via SMS.</span>
 </SecuritySection>
