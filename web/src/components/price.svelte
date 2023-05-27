98976c1b8f5190820d1549476f161289ec7a28c7
rhymn <david@pnd.se>
2023-05-27 23:14:52 +0200

    price fix

--- web/src/components/price.svelte
+++ web/src/components/price.svelte
@@ -48,7 +48,6 @@
                         
                         <td class="py-4 pl-4 pr-3 text-sm sm:pl-0">
                           <div class="font-medium text-gray-900">{module.name}</div>
-                          <div class="mt-0.5 text-gray-500 sm:hidden">{module.price}</div>
                       </td>
                   
                       <td class="py-4 pl-3 pr-4 text-right text-sm text-gray-500 sm:pr-0">{module.price}</td>
