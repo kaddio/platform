17b27b3ae70689ada297ad48f8c2473127bbf9d2
rhymn <david@pnd.se>
2023-05-31 00:10:39 +0200

    dölj

--- web/src/components/nav.svelte
+++ web/src/components/nav.svelte
@@ -25,7 +25,7 @@
 <header class="relative inset-x-0 top-0 z-50">
 
   <LangBanner />
-  <StatusBanner />
+  <!-- <StatusBanner /> -->
   <Referral />
 
   <nav class="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8" aria-label="Global">
