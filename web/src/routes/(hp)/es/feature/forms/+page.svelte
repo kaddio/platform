eae8896de25df52cb9349024db0bacc4b36b68fb
Agustin Martinez <agustin.ok@icloud.com>
2023-06-12 11:30:52 +0200

    trans

--- web/src/routes/(hp)/es/feature/forms/+page.svelte
+++ web/src/routes/(hp)/es/feature/forms/+page.svelte
@@ -7,12 +7,12 @@
 
 <svelte:head>
 	<title>Kaddio - Formularios</title>
-	<meta name="description" content="En Kaddio, puedes usar formularios estándar y crear los tuyos propios. Envía formularios clínicos automáticamente al hacer la reserva y reciba las respuestas directamente en el." />
+	<meta name="description" content="En Kaddio, puedes usar formularios estándar y crear los tuyos propios. Envía formularios clínicos automáticamente al hacer la reserva y recibe las respuestas directamente en el." />
 </svelte:head>
 
 <ModuleSection>
 	<span slot="title">Formularios</span>
-	<span slot="sub">En Kaddio, puedes usar formularios estándar y crear los tuyos propios. Envía formularios clínicos automáticamente al hacer la reserva y reciba las respuestas directamente en el.</span>
+	<span slot="sub">En Kaddio, puedes usar formularios estándar y crear los tuyos propios. Envía formularios clínicos automáticamente al hacer la reserva y recibe las respuestas directamente en el.</span>
 
 	<!-- <ModuleFeature>
 		<span slot="title">Förbered</span>
