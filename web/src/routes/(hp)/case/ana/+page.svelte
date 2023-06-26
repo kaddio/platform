44b805f6ba274b85db90df4c055ea0fcc27ae50b
rhymn <david@pnd.se>
2023-06-26 11:56:06 +0200

    case

--- web/src/routes/(hp)/case/ana/+page.svelte
+++ web/src/routes/(hp)/case/ana/+page.svelte
@@ -16,7 +16,7 @@ import Legal from "$components/legal.svelte";
     
     <span slot="title">Meet Ana Casanova</span>
     <span slot="subtitle">
-      "I have always been attracted to methods that are natural or traditional, both in my work and in my hobbies."
+      Watch the video to learn about Ana's work and hobbies, and how Kaddio allows for more flexibility in her work with medical records.
     </span>
 
     <span slot="intro">
@@ -36,7 +36,7 @@ import Legal from "$components/legal.svelte";
     </LegalParagraph>
 
     <LegalParagraph>
-      Watch the video to hear more about Ana's work and hobbies, and how Kaddio allows for more flexibility in her work with medical records.        
+      "I have always been attracted to methods that are natural or traditional, both in my work and in my hobbies."
     </LegalParagraph>
 
     <figure class="mt-16">
@@ -92,16 +92,16 @@ enligt GDPR.
         Övrig data som innehåller personuppgifter hanteras inom EU/ESS. All lagring av data från Journal och Kommunikation sker inom EU/ESS.
       </LegalParagraph>
 
-
     <figure class="mt-10 border-l border-oldpink pl-9">
       <blockquote class="font-semibold text-gray-900">
-        <p>“Våra kunder investerar i oss med tid, engagemang, pengar och information och anförtror oss med ett stort ansvar. Vi engagerar oss i dessa investeringar och det förklarar varför vi strävar efter att arbeta balanserat, hålla våra kostnader låga, hur vi hanterar riskfaktorer och varför vi strävar efter att växa organiskt och stabilt.”</p>
+        <p>“Ana säger....”</p>
       </blockquote>
       <figcaption class="mt-6 flex gap-x-4">
         <img class="h-6 w-6 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-        <div class="text-sm leading-6"><strong class="font-semibold text-gray-900">Lena Nyberg</strong> - Chief Information Officer</div>
+        <div class="text-sm leading-6"><strong class="font-semibold text-gray-900">Ana Nyberg</strong> - Chief Information Officer</div>
       </figcaption>
-    </figure> -->
+    </figure>
+--->
 
 </Legal>
 
