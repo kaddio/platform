15fc97de7e0e533baaa189318e5fef826754b5fe
rhymn <david@pnd.se>
2023-06-27 18:28:27 +0200

    case

--- web/src/routes/(hp)/case/+page.svelte
+++ web/src/routes/(hp)/case/+page.svelte
@@ -16,11 +16,15 @@
         <p class="mt-2 text-lg leading-8 text-gray-600">How other successful businesses use Kaddio.</p>
         <div class="mt-16 space-y-20 lg:mt-20 lg:space-y-20">
 
-        <BlogPost href="/case/ana" title="Traditional meets modern" name="Spanda Healthcare" nameTitle="" location="Selva, Spain" tags={["Medical record"]} img={imageHandler('/img/ana.png', {format: "auto", width: 640})}>
+        <BlogPost href="/case/spanda-healthcare" title="Traditional meets modern" name="Spanda Healthcare" nameTitle="" location="Selva, Spain" tags={["Medical record"]} img={imageHandler('/img/ana.png', {format: "auto", width: 640})}>
           Ana is an osteopath and physiotherapist with more than 20 years of experience in the field. She runs Spanda Healthcare in the beautiful Tramuntana mountains of Mallorca.
         </BlogPost>
 
-        <BlogPost href="/case/silvia" title="Digital transition" name="Osteopatia Mallorca" nameTitle="" location="Mallorca, Spain" tags={["Medical record", "Booking"]} img={imageHandler('/img/silvia.png', {format: "auto", width: 640})}>
+        <BlogPost href="/case/majornas-osteopati" title="New perspectives" name="Majornas Osteopati" nameTitle="" location="Göteborg, Sweden" tags={["Medical record"]} img={imageHandler('/img/petter.jpg', {format: "auto", width: 640})}>
+          Petter runs two clinics in Sweden. In his daily practice he meets all kinds of patients which gives him new perspectives every day.          
+        </BlogPost>
+
+        <BlogPost href="/case/osteopatia-mallorca" title="Digital transition" name="Osteopatía Mallorca" nameTitle="" location="Mallorca, Spain" tags={["Medical record", "Booking"]} img={imageHandler('/img/silvia.png', {format: "auto", width: 640})}>
           Silvia and her husband run the center Osteopatía Mallorca in Palma, Mallorca. In 2022, Silvia started the transition from working with pen and paper to taking digital medical records, using Kaddio.
         </BlogPost>
 
--- web/src/routes/(hp)/case/majornas-osteopati/+page.svelte
+++ web/src/routes/(hp)/case/majornas-osteopati/+page.svelte
@@ -5,36 +5,53 @@ import Legal from "$components/legal.svelte";
 	import LegalParagraph from "$components/legalParagraph.svelte";
 	import LegalTitle from "$components/legalTitle.svelte";
 	import LegalUl from "$components/legalUl.svelte";
+	import { imageHandler } from "$lib/img";
 
 </script>
-
-<svelte:head>
-    <meta name="robots" content="noindex">
-</svelte:head>
 
 <Legal>
     <span slot="pretitle">
         Case study @ Göteborg, Sweden
-        
     </span>
     
     <span slot="title">Meet Petter</span>
     <span slot="subtitle">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+      "Each client teaches me something. By seeing all kinds of clients with all sorts of problems I can evolve my skills in osteopathic practice every day".
     </span>
 
     <span slot="intro">
-        Samtliga personuppgifter vi behandlar är nödvändiga i avtalsförhållandet mellan Kaddio och kunden, alternativt sker
-        med kundens samtycke för att tillhandahålla en så bra tjänst som möjligt. Vi sparar personuppgifterna så länge de
+      Petter Jormin is inspired by the client meeting. With two busy clinics he is gaining a lot of experience.
     </span>
+<!-- 
+
+    <figure class="mt-16">
+        <img class="aspect-video rounded-xl bg-gray-50 object-cover" src={imageHandler('/img/ana.png', {format: "auto", width: 1200})} alt="">
+    </figure>
+ -->
+
+
+    <LegalParagraph>
+      Petter graduated in 2011 from Skandinaviska Osteopathögskolan. In the video he describes how Kaddio allows him to have more time for his clients.
+    </LegalParagraph>
 
 
     <figure class="mt-16">
-
-        <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/331174627?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Petter Jormin, Osteopat"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+      <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/331174627?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Petter Jormin, Osteopat"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
 
         <!-- <img class="aspect-video rounded-xl bg-gray-50 object-cover" src="/img/ana.png" alt=""> -->
     </figure>
+
+
+
+    <!-- <figure class="mt-16">
+        <img class="aspect-video rounded-xl bg-gray-50 object-cover" src="/img/ana.png" alt="">
+        <figcaption class="mt-4 flex gap-x-2 text-sm leading-6 text-gray-500">
+          <svg class="mt-0.5 h-5 w-5 flex-none text-gray-300" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z" clip-rule="evenodd" />
+          </svg>
+          Faucibus commodo massa rhoncus, volutpat.
+        </figcaption>
+      </figure> -->
     
 <!-- 
     <LegalTitle>
@@ -71,16 +88,16 @@ enligt GDPR.
         Övrig data som innehåller personuppgifter hanteras inom EU/ESS. All lagring av data från Journal och Kommunikation sker inom EU/ESS.
       </LegalParagraph>
 
-
     <figure class="mt-10 border-l border-oldpink pl-9">
       <blockquote class="font-semibold text-gray-900">
-        <p>“Våra kunder investerar i oss med tid, engagemang, pengar och information och anförtror oss med ett stort ansvar. Vi engagerar oss i dessa investeringar och det förklarar varför vi strävar efter att arbeta balanserat, hålla våra kostnader låga, hur vi hanterar riskfaktorer och varför vi strävar efter att växa organiskt och stabilt.”</p>
+        <p>“Ana säger....”</p>
       </blockquote>
       <figcaption class="mt-6 flex gap-x-4">
         <img class="h-6 w-6 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-        <div class="text-sm leading-6"><strong class="font-semibold text-gray-900">Lena Nyberg</strong> - Chief Information Officer</div>
+        <div class="text-sm leading-6"><strong class="font-semibold text-gray-900">Ana Nyberg</strong> - Chief Information Officer</div>
       </figcaption>
-    </figure> -->
+    </figure>
+--->
 
 </Legal>
 
