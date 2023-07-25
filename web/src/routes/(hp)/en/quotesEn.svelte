7ad681bd0de8d64a1e90752b2749b102a53691db
LenaKaddio <lena@kaddio.com>
2023-07-25 14:24:14 +0200

    trans en quotes

--- web/src/routes/(hp)/en/quotesEn.svelte
+++ web/src/routes/(hp)/en/quotesEn.svelte
@@ -7,51 +7,54 @@
 
 <QuotesSection>
     <span slot="title">
-        Hello! Läs vad några av våra 2000 kunder tycker om Kaddio        
+        Testimonials from some of our 2000 customers        
     </span>
 
     <Quote>
-        Vi på Din Psykolog har haft stor hjälp av Kaddio i vårt arbete i att utveckla framtidens digitala vård, och de har varit väldigt tillmötesgående i utvecklingen av specifika funktioner vi behövt. Vi kan varmt rekommendera Kaddio!
+        At Din Psykolog, we have had great help from Kaddio in our work with developing digital healthcare for the future, and they have been most accommodating with developing specific functions for our needs. We warmly recommend Kaddio!
         <span slot="quoted">
-            Ramzi Jelassi <cite>Grundare  <a href="https://dinpsykolog.se" target="_blank" rel="noreferrer">Din Psykolog</a></cite>
+            Ramzi Jelassi <cite>Founder  <a href="https://dinpsykolog.se" target="_blank" rel="noreferrer">Din Psykolog</a></cite>
         </span>
     </Quote>
     <Quote>
-        Det är ett rent nöje att fakturera med kaddio! Så smidigt och tydligt. Och så gör det ju skillnad att det är en snygg miljö också, mina andra program har sett ut som ... 😉. Tack för allt ni gör, min jobbdag blir så mycket härligare i kaddio!
+        It is a pleasure to do invoicing with Kaddio! So smooth and clear. And the neat environment makes a difference too, my other programs have looked like... 😉. Thanks for everything you do, my work day is so much nicer with Kaddio. 
         <span slot="quoted">
-            Ulrika Hiscoke <cite>Leg Psykolog, medicine doktor <a href="https://arbor.kaddio.com" target="_blank" rel="noreferrer">Arbor Psykologi</a></cite>
-        </span>
-    </Quote>
-    
-    <Quote>
-        Kaddio är användarvänligt, funktionellt och passar vår verksamhet perfekt!
-        <span slot="quoted">
-            Jimmy Lundqvist <cite>Chef IKSU fysio <a href="https://iksu.se" target="_blank" rel="noreferrer">IKSU</a></cite>
+            Ulrika Hiscoke <cite>Lic. Psychologist, Medical Doctor <a href="https://arbor.kaddio.com" target="_blank" rel="noreferrer">Arbor Psykologi</a></cite>
         </span>
     </Quote>
     
     <Quote>
-        Jag har samma krav på mitt journalföringssystem som mina behandlingar: effektivitet. Därför använder jag Kaddio.
+        Kaddio is user friendly, functional and suits our business perfectly!
         <span slot="quoted">
-            Jonas Evander <cite>Leg Psykolog <a href="https://afobia.se" target="_blank" rel="noreferrer">Afobia</a></cite>
+            Jimmy Lundqvist <cite>CEO IKSU fysio <a href="https://iksu.se" target="_blank" rel="noreferrer">IKSU</a></cite>
         </span>
     </Quote>
+    
     <Quote>
-        Kaddio är det journalprogram jag valt pga enkelheten. Programmet är enkelt att navigera och fungerar smidigt att anpassa efter mina behov, både i journalföring och tidsbokning.
+        I have the same demand for my medical records program as for my treatments: Efficiency. That is why I use Kaddio.
         <span slot="quoted">
-            Amanda Follin <cite>Certifierad massageterapeut <a href="https://www.endorfinkliniken.com" target="_blank" rel="noreferrer">Endorfinkliniken</a></cite>
+            Jonas Evander <cite>Lic. Psychologist <a href="https://afobia.se" target="_blank" rel="noreferrer">Afobia</a></cite>
         </span>
     </Quote>
+
     <Quote>
-        Programmet har allt jag behöver och det är lätt att använda och anpassa efter mina behov. Jag kan verkligen rekommendera detta journalsystem.
+        Kaddio is my chosen program for medical records due to its ease of use. The program is easy to navigate and adjust to my needs, both for medical records and for time bookings.
         <span slot="quoted">
-            Sofia Viotti <cite>Leg Psykolog Compassionfocus
+            Amanda Follin <cite>Lic. Massage Therapist <a href="https://www.endorfinkliniken.com" target="_blank" rel="noreferrer">Endorfinkliniken</a></cite>
         </span>
     </Quote>
+
     <Quote>
-        Ett funktionellt och enkelt journalsystem, med snabb och proffsig support. Fungerar perfekt med samlingsräkning.
+        The program has everything I need and is easy to adjust to my needs. I can really recommend this program for medical records.
         <span slot="quoted">
-            Kim Olausson <cite>Leg Fysioterapeut <a href="http://fysiokompassen.se" target="_blank" rel="noreferrer">Fysiokompassen</a></cite>
+            Sofia Viotti <cite>Lic. Psychologist Compassionfocus
+        </span>
+    </Quote>
+
+    <Quote>
+        A functional and easy program for medical records, with a quick and professional support team.
+        <span slot="quoted">
+            Kim Olausson <cite>Lic. Physiotherapist <a href="http://fysiokompassen.se" target="_blank" rel="noreferrer">Fysiokompassen</a></cite>
         </span>
     </Quote>    
 </QuotesSection>
