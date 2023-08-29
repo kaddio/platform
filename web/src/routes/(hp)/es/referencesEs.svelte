2c9ae81ddbe274e63b09120ed2f39c8d4739f25c
Agustin Martinez <agustin.ok@icloud.com>
2023-08-29 11:22:28 +0200

    trans

--- web/src/routes/(hp)/es/referencesEs.svelte
+++ web/src/routes/(hp)/es/referencesEs.svelte
@@ -8,7 +8,7 @@
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <div class="grid grid-cols-1 items-center gap-x-8 gap-y-16 lg:grid-cols-2">
       <div class="mx-auto w-full max-w-xl lg:mx-0">
-        <h2 class="text-3xl font-bold tracking-tight text-gray-900">Elige un sistema que se adapte al crecimiento de tu negocio.</h2>
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900">Elige un sistema que se adapte al crecimiento de tu negocio</h2>
         <p class="mt-6 text-lg leading-8 text-gray-600">Kaddio es utilizado por más de 1500 empresas, desde pequeñas clínicas hasta actores multinacionales. Nos preocupamos por tus necesidades, sin importar el tamaño de tu negocio. </p>
         <div class="mt-8 flex items-center gap-x-6">
           <!-- <a href="/sv/create" class="rounded-md bg-oldpink px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-oldpinkdarker focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600">OK jag är helt såld, nu kör vi!</a> -->
