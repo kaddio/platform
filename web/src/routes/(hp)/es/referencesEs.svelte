e777072d5271cad75bd1684c490c9038420f2cdd
Agustin Martinez <agustin.ok@icloud.com>
2023-08-29 11:17:59 +0200

    referencesES added

---
+++ web/src/routes/(hp)/es/referencesEs.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Customer from "$components/customer.svelte";
+</script>
+
+
+
+<div class="bg-white py-24 sm:py-32">
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="grid grid-cols-1 items-center gap-x-8 gap-y-16 lg:grid-cols-2">
+      <div class="mx-auto w-full max-w-xl lg:mx-0">
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900">Elige un sistema que se adapte al crecimiento de tu negocio.</h2>
+        <p class="mt-6 text-lg leading-8 text-gray-600">Kaddio es utilizado por más de 1500 empresas, desde pequeñas clínicas hasta actores multinacionales. Nos preocupamos por tus necesidades, sin importar el tamaño de tu negocio. </p>
+        <div class="mt-8 flex items-center gap-x-6">
+          <!-- <a href="/sv/create" class="rounded-md bg-oldpink px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-oldpinkdarker focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600">OK jag är helt såld, nu kör vi!</a> -->
+          <a href="/sv/support" class="text-sm font-semibold text-gray-900">Tengo algo que quiero preguntar <span aria-hidden="true">&rarr;</span></a>
+        </div>
+      </div>
+      <div class="mx-auto grid w-full max-w-xl grid-cols-2 items-center gap-y-12 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:pl-8">
+        <Customer src="/customer-logos/malmo.png" alt="Malmö Universitet"></Customer>
+        <Customer src="/customer-logos/rfsl.png" alt="RFSL"></Customer>
+        <Customer src="/customer-logos/Twitch.png" alt="Twitch"></Customer>
+        <Customer src="/customer-logos/storasyster_ny.png" alt="Storasyster"></Customer>
+        <Customer src="/customer-logos/sveakbt.png" alt="Svea KBT"></Customer>
+        <Customer src="/customer-logos/dinpsykolog.png" alt="Din Psykolog"></Customer>
+        <Customer src="/customer-logos/IKSU Orange 2019 (kopia).png" alt="IKSU"></Customer>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+  
