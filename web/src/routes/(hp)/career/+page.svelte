b106f1217e1ab2458ef969c905a70414e5ab6488
rhymn <david@pnd.se>
2023-09-12 14:48:00 +0200

    career img

--- web/src/routes/(hp)/career/+page.svelte
+++ web/src/routes/(hp)/career/+page.svelte
@@ -24,7 +24,7 @@
       <div class="w-full lg:max-w-lg lg:flex-auto">
         <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">We're always looking for awesome people</h2>
         <p class="mt-6 text-xl leading-8 text-gray-600">At Kaddio, we have a lot of freedom with flexible work hours, you can work remotely 50% if you prefer. We have a super cosy office in central Göteborg, Sweden. We all speak English and Swedish, some speak Spanish.</p>
-        <img src="https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1344&h=1104&q=80" alt="" class="mt-16 aspect-[6/5] w-full rounded-2xl bg-gray-50 object-cover lg:aspect-auto lg:h-[34.5rem]">
+        <img src="/img/david-arvid-kaffe.jpg" alt="" class="mt-16 aspect-[6/5] w-full rounded-2xl bg-gray-50 object-cover lg:aspect-auto lg:h-[34.5rem]">
       </div>
       <div class="w-full lg:max-w-xl lg:flex-auto">
         <h3 class="sr-only">Job openings</h3>
