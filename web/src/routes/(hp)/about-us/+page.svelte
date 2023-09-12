c6cef80d6af9686ef8d20a047d99a4508bc0da60
rhymn <david@pnd.se>
2023-09-12 14:57:41 +0200

    about us timeline

--- web/src/routes/(hp)/about-us/+page.svelte
+++ web/src/routes/(hp)/about-us/+page.svelte
@@ -28,6 +28,58 @@
 <Nav />
   
 
+<div class="bg-white py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <div class="mx-auto grid max-w-2xl grid-cols-1 gap-8 overflow-hidden lg:mx-0 lg:max-w-none lg:grid-cols-4">
+        <div>
+          <time datetime="2021-08" class="flex items-center text-sm font-semibold leading-6 text-oldpink">
+            <svg viewBox="0 0 4 4" class="mr-4 h-1 w-1 flex-none" aria-hidden="true">
+              <circle cx="2" cy="2" r="2" fill="currentColor" />
+            </svg>
+            Mar 2016
+            <div class="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0" aria-hidden="true"></div>
+          </time>
+          <p class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">Company founded</p>
+          <p class="mt-1 text-base leading-7 text-gray-600">Kaddio AB, at the time named ePsy is founded by Arvid and David.</p>
+        </div>
+        <div>
+          <time datetime="2021-12" class="flex items-center text-sm font-semibold leading-6 text-oldpink">
+            <svg viewBox="0 0 4 4" class="mr-4 h-1 w-1 flex-none" aria-hidden="true">
+              <circle cx="2" cy="2" r="2" fill="currentColor" />
+            </svg>
+            Dec 2016
+            <div class="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0" aria-hidden="true"></div>
+          </time>
+          <p class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">3rd employee</p>
+          <p class="mt-1 text-base leading-7 text-gray-600">Ylva joins our team.</p>
+        </div>
+        <div>
+          <time datetime="2022-02" class="flex items-center text-sm font-semibold leading-6 text-oldpink">
+            <svg viewBox="0 0 4 4" class="mr-4 h-1 w-1 flex-none" aria-hidden="true">
+              <circle cx="2" cy="2" r="2" fill="currentColor" />
+            </svg>
+            Nov 2017
+            <div class="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0" aria-hidden="true"></div>
+          </time>
+          <p class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">1st paying customer</p>
+          <p class="mt-1 text-base leading-7 text-gray-600">Until now, Kaddio was freeware. We wanted to start charging our users 6 months earlier but it took longer than we expected to "automate" the billing process :)</p>
+        </div>
+        <div>
+          <time datetime="2022-12" class="flex items-center text-sm font-semibold leading-6 text-oldpink">
+            <svg viewBox="0 0 4 4" class="mr-4 h-1 w-1 flex-none" aria-hidden="true">
+              <circle cx="2" cy="2" r="2" fill="currentColor" />
+            </svg>
+            Dec 2020
+            <div class="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0" aria-hidden="true"></div>
+          </time>
+          <p class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">Black numbers!</p>
+          <p class="mt-1 text-base leading-7 text-gray-600">Kaddio has been independent from external investors since the beginning, now we are also economically independent from out parent company.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+
 
 <div class="bg-white py-24 sm:py-32">
     <div class="mx-auto grid max-w-7xl gap-y-20 gap-x-8 px-6 lg:px-8 xl:grid-cols-3">
