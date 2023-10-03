83266aaa83a6d910d09b15dd1b5fbd7e104a45db
Arvid Kästel <arvid.kastel@gmail.com>
2023-10-03 08:23:40 +0200

    landing page fix

--- web/src/routes/(hp)/sv/hitta-klinik/[[keyword]]/[[place]]/[[page]]/+page.svelte
+++ web/src/routes/(hp)/sv/hitta-klinik/[[keyword]]/[[place]]/[[page]]/+page.svelte
@@ -145,163 +145,44 @@
 </svelte:head>
 
 <div class="w-sceen flex flex-col">
-    {#if !landingPage}
-        <div class="bg-purple-400 p-6">
-            <div class="container mx-auto text-white text-xl flex flex-row gap-5 align-middle">
-                <img src="https://kaddio.com/img/kaddio-logo.png" alt="" class="h-7" />
+    <div class="w-full relative" class:p-16={landingPage} class:p-4={!landingPage}>
+        <div
+            style="background-image: url('/img/sigmund-YUuSAJkS3U4-unsplash.jpg'); background-size: cover; background-position: center; "
+            class="w-full saturate-50 brightness-50 top-0 absolute left-0 bottom-0 right-0"
+        />
+        <div
+            class="mx-auto text-xl flex gap-5 align-middle justify-center items-center top-0 left-0 flex-col relative max-w-screen-lg"
+            class:md:flex-row={!landingPage}
+            class:md:justify-between={!landingPage}
+        >
+            <h1 class="text-5xl flex gap-8 text-white" class:text-xl={!landingPage}>
+                <img
+                    src="https://kaddio.com/img/kaddio-logo.png"
+                    alt=""
+                    class:h-12={landingPage}
+                    class:h-8={!landingPage}
+                /> Hitta klinik
+            </h1>
+            {#if landingPage}
+                <h2 class="text-lg mb-6 text-white">Sök och hitta rätt klinik för dina behov</h2>
+            {/if}
 
-                <h1>
-                    Hitta klinik › {[data.keyword, isLocation ? 'Min plats' : data.place]
-                        .filter(Boolean)
-                        .join(' › ')}
-                </h1>
-            </div>
-        </div>
-    {/if}
-    {#if landingPage}
-        <div class="w-full p-16 relative pt-24">
-            <div
-                style="background-image: url('/img/sigmund-YUuSAJkS3U4-unsplash.jpg'); background-size: cover; background-position: center; "
-                class="w-full saturate-50 brightness-50 top-0 absolute left-0 bottom-0 right-0"
-            />
-            <div
-                class="mx-auto text-white text-xl flex gap-5 align-middle justify-center items-center top-0 left-0 flex-col relative"
-            >
-                <h1 class="text-5xl flex gap-8">
-                    <img src="https://kaddio.com/img/kaddio-logo.png" alt="" class="h-12" /> Hitta klinik
-                </h1>
-                <h2 class="text-lg mb-6">Sök och hitta rätt klinik för dina behov</h2>
-
-                <form
-                    class="p-0 grid grid-cols-2 gap-5 w-full max-w-screen-sm"
-                    name="searchForm"
-                    bind:this={searchForm}
-                    method="GET"
-                    action={getUrl()}
-                >
-                    <div class="flex flex-col">
-                        <label class="font-semibold mb-2 ml-3">Sök</label>
-                        <AutoComplete
-                            showClear={true}
-                            dropdownClassName="rounded py-2 px-3 border-none shadow"
-                            onChange={submit}
-                            itemClass="p-5"
-                            noInputStyles={true}
-                            inputClassName="rounded py-2 px-3 border-none w-full"
-                            minCharactersToSearch={0}
-                            placeholder="Sök"
-                            labelFunction={(i) => i?.label}
-                            searchFunction={autocompleteSearch}
-                            bind:selectedItem
-                            delay={200}
-                        >
-                            <div slot="item" let:item let:label>
-                                <div class="py-1">
-                                    {#if item.type == 'KEYWORD'}
-                                        <i class="fa fa-magnifying-glass text-gray-400 mr-4" /><span
-                                            >{item.label}</span
-                                        >
-                                    {/if}
-                                    {#if item.type == 'FIND'}
-                                        <i class="fa fa-magnifying-glass text-gray-400 mr-4" /><span
-                                            >Sök {item.label}</span
-                                        >
-                                    {/if}
-                                    {#if item.type == 'ORG'}
-                                        <i class="fa fa-globe text-gray-400 mr-4" /><a
-                                            href="/c/{item.url}?backbutton=1">{item.label}</a
-                                        >
-                                    {/if}
-                                </div>
-                            </div>
-                        </AutoComplete>
-                        <div class="px-4 py-3 flex flex-wrap gap-2 hidden md:flex">
-                            {#each defaultKeywords as keyword}
-                                <a
-                                    class="text-sm text-white"
-                                    href
-                                    on:click={() => (selectedItem = { label: keyword })}
-                                    >{keyword}</a
-                                >
-                            {/each}
-                        </div>
-                    </div>
-                    <div class="flex flex-col col-span-1">
-                        <div class="flex flex-row justify-between mb-2 ml-3 items-baseline">
-                            <label class="font-semibold">Plats</label>
-                            <a
-                                class="text-sm text-white font-semibold"
-                                href=""
-                                on:click={setMyLocation}
-                            >
-                                Min plats
-                                {#if loadingPlace}
-                                    <i class="fa-duotone fa-spinner-third fa-spin" />
-                                {:else}
-                                    <i class="fa fa-location-arrow" />
-                                {/if}
-                            </a>
-                        </div>
-
-                        <AutoComplete
-                            showClear={true}
-                            dropdownClassName="rounded py-2 px-3 border-none shadow"
-                            onChange={submit}
-                            itemClass="p-5"
-                            labelFunction={(i) => i?.name}
-                            valueFunction={(i) => i?.name}
-                            noInputStyles={true}
-                            inputClassName="rounded py-2 px-3 border-none w-full"
-                            minCharactersToSearch={0}
-                            placeholder="Sök plats"
-                            searchFunction={autocompletePlace}
-                            bind:selectedItem={selectedPlace}
-                            delay={200}
-                        >
-                            <div slot="item" let:item let:label>
-                                <!-- {@html label} -->
-                                <!-- to render the default higliglighted item label -->
-                                <!-- render anything else -->
-                                <div class="py-1">
-                                    <i class="fa fa-magnifying-glass text-gray-400 mr-4" /><span
-                                        >{item.name}</span
-                                    > <span class="text-gray-400">{item.secondaryName}</span>
-                                </div>
-                            </div>
-                        </AutoComplete>
-                        <div class="px-4 py-3 md:flex md:flex-wrap gap-2 hidden">
-                            {#each defaultPlaces as place}
-                                <a
-                                    class="text-sm text-white"
-                                    href
-                                    on:click={() => (selectedPlace = { name: place })}>{place}</a
-                                >
-                            {/each}
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    {/if}
-    <div class="bg-gray-100 p-4 lg:p-12 pt-4 lg:pt-20 flex grow">
-        <div class="flex flex-col gap-10 lg:w-full lg:flex-row container mx-auto">
             <form
-                class="p-0 grid grid-cols-2 gap-8 h-fit w-full sm:w-96"
+                class="p-0 grid md:grid-cols-2 grid-cols-1 gap-5 w-full max-w-screen-sm"
                 name="searchForm"
                 bind:this={searchForm}
                 method="GET"
                 action={getUrl()}
-                class:hidden={landingPage}
             >
-                <div class="flex flex-col col-span-2 md:col-span-1 lg:col-span-2">
-                    <label class="font-semibold mb-2 ml-3">Sök</label>
+                <div class="flex flex-col col-span-2 md:col-span-1">
+                    <label class="font-semibold mb-2 ml-3 text-white">Sök</label>
                     <AutoComplete
                         showClear={true}
                         dropdownClassName="rounded py-2 px-3 border-none shadow"
                         onChange={submit}
                         itemClass="p-5"
                         noInputStyles={true}
-                        inputClassName="rounded py-2 px-3 border-none w-full"
+                        inputClassName="rounded py-2 px-3 border-none w-full text-black"
                         minCharactersToSearch={0}
                         placeholder="Sök"
                         labelFunction={(i) => i?.label}
@@ -329,21 +210,24 @@
                             </div>
                         </div>
                     </AutoComplete>
-                    <div class="px-4 py-3 flex flex-wrap gap-2 hidden md:flex">
-                        {#each defaultKeywords as keyword}
-                            <a
-                                class="text-xs text-gray-400"
-                                href
-                                on:click={() => (selectedItem = { label: keyword })}>{keyword}</a
-                            >
-                        {/each}
-                    </div>
+                    {#if landingPage}
+                        <div class="px-4 py-3 flex flex-wrap gap-2 hidden md:flex">
+                            {#each defaultKeywords as keyword}
+                                <a
+                                    class="text-sm text-white"
+                                    href
+                                    on:click={() => (selectedItem = { label: keyword })}
+                                    >{keyword}</a
+                                >
+                            {/each}
+                        </div>
+                    {/if}
                 </div>
-                <div class="flex flex-col col-span-2 md:col-span-1 lg:col-span-2">
-                    <div class="flex flex-row justify-between mb-2 ml-3 items-baseline">
+                <div class="flex flex-col col-span-2 md:col-span-1">
+                    <div class="flex flex-row justify-between mb-2 ml-3 items-baseline text-white">
                         <label class="font-semibold">Plats</label>
                         <a
-                            class="text-xs text-gray-400 font-semibold"
+                            class="text-sm text-white font-semibold"
                             href=""
                             on:click={setMyLocation}
                         >
@@ -364,7 +248,7 @@
                         labelFunction={(i) => i?.name}
                         valueFunction={(i) => i?.name}
                         noInputStyles={true}
-                        inputClassName="rounded py-2 px-3 border-none w-full"
+                        inputClassName="rounded py-2 px-3 border-none w-full text-black"
                         minCharactersToSearch={0}
                         placeholder="Sök plats"
                         searchFunction={autocompletePlace}
@@ -382,23 +266,25 @@
                             </div>
                         </div>
                     </AutoComplete>
-                    <div class="px-4 py-3 md:flex md:flex-wrap gap-2 hidden">
-                        {#each defaultPlaces as place}
-                            <a
-                                class="text-xs text-gray-400"
-                                href
-                                on:click={() => (selectedPlace = { name: place })}>{place}</a
-                            >
-                        {/each}
-                    </div>
+                    {#if landingPage}
+                        <div class="px-4 py-3 md:flex md:flex-wrap gap-2 hidden">
+                            {#each defaultPlaces as place}
+                                <a
+                                    class="text-sm text-white"
+                                    href
+                                    on:click={() => (selectedPlace = { name: place })}>{place}</a
+                                >
+                            {/each}
+                        </div>
+                    {/if}
                 </div>
             </form>
+        </div>
+    </div>
 
-            <div
-                class="flex flex-col h-full grow"
-                class:max-w-screen-lg={landingPage}
-                class:mx-auto={landingPage}
-            >
+    <div class="bg-gray-100 p-4 lg:p-12 pt-4 lg:pt-20 flex grow">
+        <div class="flex flex-col gap-10 lg:w-full lg:flex-row container mx-auto">
+            <div class="flex flex-col h-full grow max-w-screen-lg mx-auto">
                 {#if data.organizations.length}
                     <div class="flex justify-between">
                         <label class="font-semibold mb-2 ml-3"
