d7642576f56c3b603089b60b64194ce2368cf713
rhymn <david@pnd.se>
2023-10-04 22:12:43 +0200

    white

--- web/src/components/header.svelte
+++ web/src/components/header.svelte
@@ -180,7 +180,7 @@ $: $page.url && (navOpen = false);
       <div class="flex lg:hidden">
         <button on:click={() => {show = true}} type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
           <span class="sr-only">Open main menu</span>
-          <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+          <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
         </button>
