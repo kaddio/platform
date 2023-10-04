b6ef6c5573f92dacdcbce3a9dd024f570b6d5e1e
rhymn <david@pnd.se>
2023-10-04 22:03:48 +0200

    gap

--- web/src/components/header.svelte
+++ web/src/components/header.svelte
@@ -58,7 +58,7 @@ $: $page.url && (navOpen = false);
 />
 
 <header class="relative isolate z-10">
-    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 gap-x-6 lg:px-8" aria-label="Global">
       <div class="flex lg:flex-1">
         <a href="/{lang}" class="-m-1.5 p-1.5">
           <span class="sr-only">Kaddio</span>
