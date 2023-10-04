66e06b5104d13d067bf731c1c3f1f8a58b686743
rhymn <david@pnd.se>
2023-10-04 22:37:20 +0200

    toggle text

--- web/src/components/header.svelte
+++ web/src/components/header.svelte
@@ -173,14 +173,14 @@ $: $page.url && (navOpen = false);
 
 
       <div class="flex flex-1 items-center justify-end gap-x-6">
-        <a href="/{lang}/sign-in" class="{lightText ? 'lg:text-white' : 'lg:text-gray-900'} hidden lg:block lg:text-sm lg:font-semibold lg:leading-6">{ $_('Logga in')}</a>
+        <a href="/{lang}/sign-in" class:text-white={lightText} class="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6">{ $_('Logga in')}</a>
         <a href="/{lang}/create" class="rounded-md bg-oldpink py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-oldpinkdarker focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600">{ $_('Skapa konto')}</a>
       </div>
 
       <div class="flex lg:hidden">
         <button on:click={() => {show = true}} type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
           <span class="sr-only">Open main menu</span>
-          <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+          <svg class="h-6 w-6" class:text-white={lightText} fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
         </button>
