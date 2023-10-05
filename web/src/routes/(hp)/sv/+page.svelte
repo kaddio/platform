fd0d2c18d942f0842c6c38a73cecaedad853b42e
rhymn <david@pnd.se>
2023-10-05 22:00:38 +0200

    seo

--- web/src/routes/(hp)/sv/+page.svelte
+++ web/src/routes/(hp)/sv/+page.svelte
@@ -32,8 +32,8 @@ srcset="
 </script>
 
 <svelte:head>
-	<title>Kaddio</title>
-	<meta name="description" content="Webbaserat helhetssystem för dig som hjälper andra" />
+	<title>Journalsystem, onlinebokning, kommunikation, fakturering</title>
+	<meta name="description" content="Kaddio är journalsystem, onlinebokning, kommunikation, fakturering i samma system, för dig som hjälper andra" />
 </svelte:head>
 
 <div class="bg-white">
@@ -45,7 +45,7 @@ srcset="
 		<div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
 			<div class="text-center">
 				<h1 class="text-4xl font-normal tracking-tight text-white sm:text-6xl">Kaddio är allt du behöver</h1>
-				<p class="mt-6 text-lg leading-8 font-light text-white">Webbaserat helhetssystem för dig som hjälper andra.</p>
+				<p class="mt-6 text-lg leading-8 font-light text-white">Onlinebokning, journalsystem, kommunikation och fakturering i samma system. Samt mycket mer.</p>
 				<div class="mt-10 flex items-center justify-center gap-x-6">
 					<a href="https://demo.kaddio.com/login-demo" target="_blank" rel="noreferrer" class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-oldpink shadow-sm hover:bg-oldpink hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-400">Prova demo</a>
 					<button on:click={() => (scroll('functions'))} class="text-sm font-semibold leading-6 text-white">Läs mer <span aria-hidden="true">→</span></button>
@@ -73,6 +73,8 @@ srcset="
 </div>
 
 
+
+
 <ModulesSv />
 <!-- <CrmSv /> -->
 <QuotesSv />
@@ -82,3 +84,68 @@ srcset="
 <ReferencesSv />
 <ContactDanielSv />
 <Footer />
+
+<!-- 
+<div class="bg-white py-24 sm:py-32">
+	<div class="mx-auto max-w-7xl px-6 lg:px-8">
+	  <div class="mx-auto grid max-w-2xl grid-cols-1 gap-8 overflow-hidden lg:mx-0 lg:max-w-none lg:grid-cols-5">
+		<div>
+		  <time datetime="2021-08" class="flex items-center text-sm font-semibold leading-6 text-oldpink">
+			<svg viewBox="0 0 4 4" class="mr-4 h-1 w-1 flex-none" aria-hidden="true">
+			  <circle cx="2" cy="2" r="2" fill="currentColor" />
+			</svg>
+			Bli hittad
+			<div class="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0" aria-hidden="true"></div>
+		  </time>
+		  <p class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">... med Hitta klinik</p>
+		  <p class="mt-1 text-base leading-7 text-gray-600">
+			Kaddio Hitta klinik hjälper dig på ett enkelt sätt att bli hittad 
+		  </p>
+		</div>
+		<div>
+		  <time datetime="2021-12" class="flex items-center text-sm font-semibold leading-6 text-oldpink">
+			<svg viewBox="0 0 4 4" class="mr-4 h-1 w-1 flex-none" aria-hidden="true">
+			  <circle cx="2" cy="2" r="2" fill="currentColor" />
+			</svg>
+			Få bokning
+			<div class="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0" aria-hidden="true"></div>
+		  </time>
+		  <p class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">... med Onlinebokning</p>
+		  <p class="mt-1 text-base leading-7 text-gray-600">Ylva joins our team.</p>
+		</div>
+		<div>
+		  <time datetime="2022-02" class="flex items-center text-sm font-semibold leading-6 text-oldpink">
+			<svg viewBox="0 0 4 4" class="mr-4 h-1 w-1 flex-none" aria-hidden="true">
+			  <circle cx="2" cy="2" r="2" fill="currentColor" />
+			</svg>
+			Journalför
+			<div class="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0" aria-hidden="true"></div>
+		  </time>
+		  <p class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">Journalsystem</p>
+		  <p class="mt-1 text-base leading-7 text-gray-600">Until now, Kaddio was freeware. We wanted to start charging our users 6 months earlier but it took longer than we expected to "automate" the billing process :)</p>
+		</div>
+		<div>
+		  <time datetime="2022-12" class="flex items-center text-sm font-semibold leading-6 text-oldpink">
+			<svg viewBox="0 0 4 4" class="mr-4 h-1 w-1 flex-none" aria-hidden="true">
+			  <circle cx="2" cy="2" r="2" fill="currentColor" />
+			</svg>
+			Ta betalt
+			<div class="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0" aria-hidden="true"></div>
+		  </time>
+		  <p class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">Onlinebetalning & fakturering</p>
+		  <p class="mt-1 text-base leading-7 text-gray-600">Kaddio has been independent from external investors since the beginning, now we are also economically independent from out parent company.</p>
+		</div>		<div>
+		  <time datetime="2022-12" class="flex items-center text-sm font-semibold leading-6 text-oldpink">
+			<svg viewBox="0 0 4 4" class="mr-4 h-1 w-1 flex-none" aria-hidden="true">
+			  <circle cx="2" cy="2" r="2" fill="currentColor" />
+			</svg>
+			Utvärdera
+			<div class="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0" aria-hidden="true"></div>
+		  </time>
+		  <p class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">Autoformulär & statistik</p>
+		  <p class="mt-1 text-base leading-7 text-gray-600">Kaddio has been independent from external investors since the beginning, now we are also economically independent from out parent company.</p>
+		</div>
+	  </div>
+	</div>
+  </div>
+   -->
