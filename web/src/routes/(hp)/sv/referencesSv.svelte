9ade315e3c7417d719c1ae2f93dbfc2dacb300f0
rhymn <david@pnd.se>
2023-10-06 16:56:29 +0200

    ehrm

--- web/src/routes/(hp)/sv/referencesSv.svelte
+++ web/src/routes/(hp)/sv/referencesSv.svelte
@@ -9,7 +9,7 @@
     <div class="grid grid-cols-1 items-center gap-x-8 gap-y-16 lg:grid-cols-2">
       <div class="mx-auto w-full max-w-xl lg:mx-0">
         <h2 class="text-3xl font-bold tracking-tight text-gray-900">Välj ett system som skalar med din verksamhet</h2>
-        <p class="mt-6 text-lg leading-8 text-gray-600">Kaddio används av fler än 1500 företag, från den lilla mottagningen till multinationella aktörer. Vi bryr oss om dina behov, oavsett verksamhetens storlek. </p>
+        <p class="mt-6 text-lg leading-8 text-gray-600">Kaddio används av fler än 1100 företag, från den lilla mottagningen till multinationella aktörer. Vi bryr oss om dina behov, oavsett verksamhetens storlek. </p>
         <div class="mt-8 flex items-center gap-x-6">
           <!-- <a href="/sv/create" class="rounded-md bg-oldpink px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-oldpinkdarker focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600">OK jag är helt såld, nu kör vi!</a> -->
           <a href="/case" class="text-sm font-semibold text-gray-900">Kundberättelser <span aria-hidden="true">&rarr;</span></a>
