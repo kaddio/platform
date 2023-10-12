0fd574508fdb9540d5003c3066f0e7656f8b931b
rhymn <david@pnd.se>
2023-10-12 11:16:22 +0200

    daniel

--- web/src/routes/(hp)/case/+layout.svelte
+++ web/src/routes/(hp)/case/+layout.svelte
@@ -1,15 +1,17 @@
 <script>
-	import Footer from "$components/footer.svelte";
-	import Nav from "$components/nav.svelte";
-	import { setContext } from "svelte";
+    import Footer from '$components/footer.svelte';
+    import Nav from '$components/nav.svelte';
+    import { setContext } from 'svelte';
+    import ContactDanielEn from '../en/contactDanielEn.svelte';
 
     setContext('lang', 'en');
-	setContext('hideLangBanner', true);
-
+    setContext('hideLangBanner', true);
 </script>
 
 <Nav />
 
 <slot />
+
+<ContactDanielEn />
 
 <Footer />
