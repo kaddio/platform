45dd94a51bf16d8214b526afbbf2ebbda967aa27
rhymn <david@pnd.se>
2023-10-27 14:23:20 +0200

    correct title

--- web/src/routes/(hp)/case/afobia/+page.svelte
+++ web/src/routes/(hp)/case/afobia/+page.svelte
@@ -42,7 +42,7 @@ import Legal from "$components/legal.svelte";
     </LegalParagraph>
 
     <figure class="mt-16">
-      <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/877785631?h=48a4ba7452" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Petter Jormin, Osteopat"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+      <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/877785631?h=48a4ba7452" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Jonas Evander, Psychologist"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
 
         <!-- <img class="aspect-video rounded-xl bg-gray-50 object-cover" src="/img/ana.png" alt=""> -->
     </figure>
