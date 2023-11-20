b1d5e030eacbc834f7c23196649c9d3c16e9c033
rhymn <david@pnd.se>
2023-11-20 11:02:22 +0100

    fix

--- web/src/components/marketplace-banner.svelte
+++ web/src/components/marketplace-banner.svelte
@@ -12,10 +12,10 @@
 
     const region = getRegion($page.data.geo)
 
-    $: showBannerComputed = ($regionBannerDismiss !== undefined && !$regionBannerDismiss) && geo && (region.lang !== lang) && !hideLangBanner;
+    $: showBannerComputed = lang == 'sv';// ($regionBannerDismiss !== undefined && !$regionBannerDismiss) && geo && (region.lang !== lang) && !hideLangBanner;
 </script>
 
-{#if true}
+{#if showBannerComputed}
      <div class="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
         <div class="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl" aria-hidden="true">
         <div class="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30" style="clip-path: polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)"></div>
@@ -25,11 +25,11 @@
         </div>
         <div class="flex flex-wrap items-center gap-x-4 gap-y-2">
             <p class="text-sm leading-6 text-gray-900">
-                <strong class="font-semibold">{$_('Region', region.lang)}</strong><svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>{$_('Välj annat land eller region för att se innehåll som finns där du är.', region.lang)}
+                <strong class="font-semibold">{$_('Hitta klinik', region.lang)}</strong><svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>{$_('Sök på hitta klinik efter din favoritklinik', region.lang)}
             </p>
-            <a href="{region.href}" class="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900">{$_(region.label, region.lang)}</a>
+            <a href="/sv/hitta-klinik" class="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900">{$_("Sök", region.lang)}</a>
 
-            <a href="/choose-country-region" class="whitespace-nowrap text-sm">{$_('Välj annat', region.lang)}&nbsp;<span aria-hidden="true">&rarr;</span></a>            
+            <a href="/choose-country-region" class="whitespace-nowrap text-sm">{$_('Kaddio för företag', region.lang)}&nbsp;<span aria-hidden="true">&rarr;</span></a>            
         </div>
         <div class="flex flex-1 justify-end">
         <button on:click={() => {$regionBannerDismiss = true}} type="button" class="-m-3 p-3 focus-visible:outline-offset-[-4px]">
