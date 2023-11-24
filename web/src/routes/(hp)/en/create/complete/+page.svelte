3416a0a7c7df7e5a7b26685be78e6f94f89a172e
rhymn <david@pnd.se>
2023-11-24 12:11:38 +0100

    lang

---
+++ web/src/routes/(hp)/en/create/complete/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">	
+	import { setContext } from 'svelte';
+	import Footer from '$components/footer.svelte';
+	import Nav from '$components/nav.svelte';
+    import { _ } from '$lib/stores';
+    import ThankYou from '$components/thankYou.svelte';
+
+	setContext('lang', 'en');
+	
+</script>
+
+<svelte:head>
+    <meta name="robots" content="noindex">
+</svelte:head>
+
+<Nav />
+
+<ThankYou>
+	<span slot="title">Thank you!</span>
+	<span slot="p">
+		Your account is created, check your mail to log in.
+	</span>
+</ThankYou>
+
+<Footer />
