e0a7eb232992cecbd296a8b56293e064053c1074
rhymn <david@pnd.se>
2024-02-19 14:22:58 +0100

    öppettider

--- web/src/routes/(hp)/sv/contactDanielSv.svelte
+++ web/src/routes/(hp)/sv/contactDanielSv.svelte
@@ -1,13 +1,10 @@
 <script>
     import SalesSection from "$components/salesSection.svelte";
-
-
-
 </script>
 
 <SalesSection>
 	<span slot="title">Funderar du på om Kaddio kan passa dig?</span>
-	<span slot="intro">Kontakta Daniel så hjälper han dig. Tillgänglig på vardagar kl 9 - 15.</span>
+	<span slot="intro">Kontakta Daniel eller Marina så hjälper de dig. Tillgängliga på vardagar kl 9 - 12.</span>
 	<span slot="location">Göteborg, Sverige</span>
 	<span slot="phone">+46 (0) 31-320 44 14</span>
 </SalesSection>
