384d4f06a8c3e30a5ad7bc037542ecc37f9de650
rhymn <david@pnd.se>
2024-03-01 13:29:46 +0100

    lot's of descriptions

--- web/src/lib/keywords.ts
+++ web/src/lib/keywords.ts
@@ -33,13 +33,19 @@ export const keywords = [
     }
 ];
 
+export const shouldShowMetaForPage = (keyword: string, matches: number): boolean => {
+    // if (matches == 0) return false;
+
+    return keywordExists(keyword);
+};
+
 export const keywordExists = (keyword: string): boolean => {
     if (keyword === 'Alla') return true;
 
     return !!allKeywords.find((k) => k.name === keyword);
 };
 
-export const descriptionFromKeywordAndPlace = (keyword: string, place: string) => {
+export const descFromKeywordAndPlace = (keyword: string, place: string) => {
     // console.log(keyword, place);
     let placeStr = '';
 
@@ -65,43 +71,47 @@ export const descriptionFromKeywordAndPlace = (keyword: string, place: string) =
 
 const allKeywords = [
     { name: 'Psykolog', category: 'Psykologi', desc: 'Boka tid med psykolog' },
-    { name: 'Psykoterapi', category: 'Psykologi' },
-    { name: 'Samtalsterapi', category: 'Psykologi' },
-    { name: 'KBT', category: 'Psykologi' },
-    { name: 'Psykodynamisk terapi', category: 'Psykologi' },
-    { name: 'ACT', category: 'Psykologi' },
-    { name: 'Depressionsbehandling', category: 'Psykologi' },
-    { name: 'Ångestbehandling', category: 'Psykologi' },
+    { name: 'Psykoterapi', category: 'Psykologi', desc: 'Boka tid för psykoterapi' },
+    { name: 'Samtalsterapi', category: 'Psykologi', desc: 'Boka tid för samtalsterapi' },
+    { name: 'KBT', category: 'Psykologi', desc: 'Boka tid för KBT' },
+    {
+        name: 'Psykodynamisk terapi',
+        category: 'Psykologi',
+        desc: 'Boka tid för psykodynamisk terapi'
+    },
+    { name: 'ACT', category: 'Psykologi', desc: 'Boka tid för ACT' },
+    { name: 'Depressionsbehandling', category: 'Psykologi', desc: 'Sök hjälp för depression' },
+    { name: 'Ångestbehandling', category: 'Psykologi', desc: 'Sök hjälp för ångest' },
     { name: 'Tvångssyndrom', category: 'Psykologi', desc: 'Sök hjälp för tvångssyndrom' },
-    { name: 'Fobibehandling', category: 'Psykologi' },
-    { name: 'Familjerådgivning', category: 'Psykologi' },
-    { name: 'Parterapi', category: 'Psykologi' },
-    { name: 'NPF-utredning', category: 'Psykologi' },
-    { name: 'Fysioterapeut', category: 'Manuell terapi' },
-    { name: 'Osteopat', category: 'Manuell terapi' },
-    { name: 'Kiropraktor', category: 'Manuell terapi' },
-    { name: 'Naprapat', category: 'Manuell terapi' },
-    { name: 'Massage', category: 'Manuell terapi' },
-    { name: 'Ryggont', category: 'Manuell terapi' },
-    { name: 'Nacksmärta', category: 'Manuell terapi' },
-    { name: 'Huvudvärk', category: 'Manuell terapi' },
-    { name: 'Ledvärk', category: 'Manuell terapi' },
-    { name: 'Muskelsmärta', category: 'Manuell terapi' },
-    { name: 'Idrottsskador', category: 'Manuell terapi' },
-    { name: 'Idrottsmassage', category: 'Manuell terapi' },
-    { name: 'Läkare', category: 'Medicin' },
-    { name: 'Arbetsterapeut', category: 'Medicin' },
-    { name: 'Funktionsmedicin', category: 'Medicin' },
-    { name: 'Provtagning', category: 'Medicin' },
-    { name: 'Läkarintyg', category: 'Medicin' },
-    { name: 'Vaccination', category: 'Medicin' },
-    { name: 'Dietist', category: 'Medicin' },
-    { name: 'Fotvård', category: 'Medicin' },
-    { name: 'Logoped', category: 'Övrigt' },
-    { name: 'Veterinär', category: 'Övrigt' },
-    { name: 'Hundfysioterapeut', category: 'Övrigt' },
-    { name: 'Coach', category: 'Övrigt' },
-    { name: 'Livsstilsterapeut', category: 'Övrigt' }
+    { name: 'Fobibehandling', category: 'Psykologi', desc: 'Sök hjälp för fobier' },
+    { name: 'Familjerådgivning', category: 'Psykologi', desc: 'Boka tid för familjerådgivning' },
+    { name: 'Parterapi', category: 'Psykologi', desc: 'Boka tid för parterapi' },
+    { name: 'NPF-utredning', category: 'Psykologi', desc: 'Boka tid för NPF-utredning' },
+    { name: 'Fysioterapeut', category: 'Manuell terapi', desc: 'Boka tid med fysioterapeut' },
+    { name: 'Osteopat', category: 'Manuell terapi', desc: 'Boka tid med osteopat' },
+    { name: 'Kiropraktor', category: 'Manuell terapi', desc: 'Boka tid med kiropraktor' },
+    { name: 'Naprapat', category: 'Manuell terapi', desc: 'Boka tid med naprapat' },
+    { name: 'Massage', category: 'Manuell terapi', desc: 'Boka tid för massage' },
+    { name: 'Ryggont', category: 'Manuell terapi', desc: 'Sök hjälp för ryggont' },
+    { name: 'Nacksmärta', category: 'Manuell terapi', desc: 'Sök hjälp för nacksmärta' },
+    { name: 'Huvudvärk', category: 'Manuell terapi', desc: 'Sök hjälp för huvudvärk' },
+    { name: 'Ledvärk', category: 'Manuell terapi', desc: 'Sök hjälp för ledvärk' },
+    { name: 'Muskelsmärta', category: 'Manuell terapi', desc: 'Sök hjälp för muskelsmärta' },
+    { name: 'Idrottsskador', category: 'Manuell terapi', desc: 'Sök hjälp för idrottsskador' },
+    { name: 'Idrottsmassage', category: 'Manuell terapi', desc: 'Boka tid för idrottsmassage' },
+    { name: 'Läkare', category: 'Medicin', desc: 'Boka tid med läkare' },
+    { name: 'Arbetsterapeut', category: 'Medicin', desc: 'Boka tid med arbetsterapeut' },
+    { name: 'Funktionsmedicin', category: 'Medicin', desc: 'Boka tid för funktionsmedicin' },
+    { name: 'Provtagning', category: 'Medicin', desc: 'Boka tid för provtagning' },
+    { name: 'Läkarintyg', category: 'Medicin', desc: 'Boka tid för läkarintyg' },
+    { name: 'Vaccination', category: 'Medicin', desc: 'Boka tid för vaccination' },
+    { name: 'Dietist', category: 'Medicin', desc: 'Boka tid med dietist' },
+    { name: 'Fotvård', category: 'Medicin', desc: 'Boka tid för fotvård' },
+    { name: 'Logoped', category: 'Övrigt', desc: 'Boka tid med logoped' },
+    { name: 'Veterinär', category: 'Övrigt', desc: 'Boka tid hos veterinär' },
+    { name: 'Hundfysioterapeut', category: 'Övrigt', desc: 'Boka tid hos hundfysioterapeut' },
+    { name: 'Coach', category: 'Övrigt', desc: 'Boka tid med coach' },
+    { name: 'Livsstilsterapeut', category: 'Övrigt', desc: 'Boka tid med livsstilsterapeut' }
 ];
 
 export const keywordsByCategory = allKeywords.reduce((acc, keyword) => {
