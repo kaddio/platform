27381075f126b8c231bbdb7028440375713e0f9f
rhymn <david@pnd.se>
2024-03-03 12:05:37 +0100

    copy and menu fix

--- web/src/routes/(hp)/en/referral_/+page.svelte
+++ web/src/routes/(hp)/en/referral_/+page.svelte
@@ -12,10 +12,7 @@
 
 <Nav />
 
-<div class="relative isolate overflow-hidden bg-white py-24 sm:py-32">
-    <div class="absolute -top-80 left-[max(6rem,33%)] -z-10 transform-gpu blur-3xl sm:left-1/2 md:top-20 lg:ml-20 xl:top-3 xl:ml-56" aria-hidden="true">
-        <div class="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30" style="clip-path: polygon(63.1% 29.6%, 100% 17.2%, 76.7% 3.1%, 48.4% 0.1%, 44.6% 4.8%, 54.5% 25.4%, 59.8% 49.1%, 55.3% 57.9%, 44.5% 57.3%, 27.8% 48%, 35.1% 81.6%, 0% 97.8%, 39.3% 100%, 35.3% 81.5%, 97.2% 52.8%, 63.1% 29.6%)"></div>
-    </div>
+<div class="bg-white py-24 sm:py-32">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto max-w-2xl lg:mx-0">
         <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Kaddio is recommended</h1>
