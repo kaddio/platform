fc555f8cdea117a41eafceaa1fa0a683a2f79dd4
rhymn <david@pnd.se>
2024-03-07 11:50:21 +0100

    prettier

--- web/src/lib/img.ts
+++ web/src/lib/img.ts
@@ -1,36 +1,38 @@
-import { dev } from "$app/environment";
+import { dev } from '$app/environment';
 
 type Options = {
-    format?: string,
-    width?: number,
-    fit?: string
+    format?: string;
+    width?: number;
+    fit?: string;
+};
+
+function prefixSrc(src: string) {
+    const possibleSlash = src.startsWith('/') ? '' : '/';
+
+    return `https://kaddio.com${possibleSlash}${src}`;
 }
 
-
-function prefixSrc(src: string){
-    const possibleSlash = (src.startsWith('/')) ? '' : '/';
-
-    return `https://kaddio.com${possibleSlash}${src}`
-}
-
-function getSrc(src: string){
+function getSrc(src: string) {
     const isAbsolute = src.startsWith('http');
 
-    return (isAbsolute) ? src : prefixSrc(src);
+    return isAbsolute ? src : prefixSrc(src);
 }
 
-export const imageHandler = function(src: string, options: Options, isDevelopment: boolean = dev): string{
-    if(!src || src.length == 0) return src;
+export const imageHandler = function (
+    src: string,
+    options: Options,
+    isDevelopment: boolean = dev
+): string {
+    if (!src || src.length == 0) return src;
 
-
-    if(isDevelopment){
+    if (isDevelopment) {
         return src;
-    }
-
-    else{
-        const optionsStr = Object.entries(options).map(([key, value]) => `${key}=${value}`).join(',');
+    } else {
+        const optionsStr = Object.entries(options)
+            .map(([key, value]) => `${key}=${value}`)
+            .join(',');
         console.log(optionsStr);
 
         return `https://kaddio.com/cdn-cgi/image/${optionsStr}/${getSrc(src)}`;
     }
-}
+};
