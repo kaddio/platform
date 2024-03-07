d709fffdf4721de0127063f3f682f53aa89f4c58
rhymn <david@pnd.se>
2024-03-07 12:56:22 +0100

    imageHandler

--- web/src/components/securitySection.svelte
+++ web/src/components/securitySection.svelte
@@ -1,5 +1,5 @@
 <script>
-
+    import { imageHandler } from "$lib/img";
 	import { _ } from "$lib/stores";
 
   export let src = 'https://help.kaddio.com/help';
@@ -7,7 +7,7 @@
 </script>
 <div class="relative bg-gray-100">
     <div class="relative h-80 overflow-hidden bg-purple-600 md:absolute md:left-0 md:h-full md:w-1/3 lg:w-1/2">
-      <img class="h-full w-full object-cover" src="/img/fan.jpg" alt="">
+      <img class="h-full w-full object-cover" src={imageHandler("/img/fan.jpg", {format: "auto", width: 640})} alt="">
       <svg viewBox="0 0 926 676" aria-hidden="true" class="absolute left-24 -bottom-24 w-[57.875rem] transform-gpu blur-[118px]">
         <path fill="url(#60c3c621-93e0-4a09-a0e6-4c228a0116d8)" fill-opacity=".4" d="m254.325 516.708-90.89 158.331L0 436.427l254.325 80.281 163.691-285.15c1.048 131.759 36.144 345.144 168.149 144.613C751.171 125.508 707.17-93.823 826.603 41.15c95.546 107.978 104.766 294.048 97.432 373.585L685.481 297.694l16.974 360.474-448.13-141.46Z" />
         <defs>
