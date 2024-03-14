73f8ab5cbc925e5a7c44ceadd65d90b4b30fd8ab
rhymn <david@pnd.se>
2024-03-14 12:26:11 +0100

    loading lazy footer img

--- web/src/components/footer.svelte
+++ web/src/components/footer.svelte
@@ -19,7 +19,7 @@
   <h2 id="footer-heading" class="sr-only">Footer</h2>
   <div class="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
     <div class="xl:grid xl:grid-cols-3 xl:gap-8">
-      <img class="h-7" src={imageHandler("/img/kaddio-black.png", {format: "auto"})} alt="Company name">
+      <img class="h-7" src={imageHandler("/img/kaddio-black.png", {format: "auto"})} loading="lazy" alt="Company name">
       <div class="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
         <div class="md:grid md:grid-cols-2 md:gap-8">
           <div>
