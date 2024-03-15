cf4a04b5bedee8d7926caeb02aa8ab3c7e95639e
rhymn <david@pnd.se>
2024-03-15 14:14:09 +0100

    loading eager etc

--- web/src/routes/(hp)/es/+page.svelte
+++ web/src/routes/(hp)/es/+page.svelte
@@ -9,6 +9,7 @@
 	import ReferencesEs from './referencesEs.svelte';
     import { hrefLangAlternates } from '$lib/stores';
     import { imageHandler } from '$lib/img';
+    import Hero from '$lib/components/hero.svelte';
 
 	setContext('lang', 'es');
 </script>
@@ -19,6 +20,18 @@
 	<title>Kaddio</title>
 	<meta name="description" content="Kaddio - Sistema todo en uno basado en la web para usted que ayuda a otros" />
 </svelte:head>
+
+
+<div class="bg-white">
+  <Nav darkHeader={true} />
+
+  <Hero manAlt="Bakgrundsbild för Kaddio, en person som sitter med en kaffekopp i handen" demoLink="https://demo-es.kaddio.com/login-demo">
+    <span slot="title">Kaddio es todo lo que necesitas</span>
+    <span slot="subtitle">Un sistema integral basado en la web diseñado para ti, que ayudas a otros.</span>
+
+  </Hero>
+
+</div>
 
 <div class="bg-white">
 	<Nav darkHeader={true} />
