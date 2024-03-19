848b9881bb8cdb202fa3f99de96c4c4ba26eab6a
rhymn <david@pnd.se>
2024-03-19 14:34:39 +0100

    lazy load images

--- web/src/routes/(hp)/c/[url]/components/hosts.svelte
+++ web/src/routes/(hp)/c/[url]/components/hosts.svelte
@@ -19,7 +19,7 @@
     <KdItem>
         <span slot="avatar">
             {#if host.imgUrl}
-                <img src={host.imgUrl} alt="Bild på {host.firstname} {host.lastname}" />
+                <img src={host.imgUrl} loading="lazy" alt="Bild på {host.firstname} {host.lastname}" />
             {:else}
                 <div class="flex items-center justify-center h-full w-full bg-gray-100">
                     <i class="fa fa-user text-3xl text-gray-300" />
