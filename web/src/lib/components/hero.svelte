e3f68a19d489df1c579787fa53b086faa80d558f
rhymn <david@pnd.se>
2024-05-02 21:22:21 +0200

    hero tracking

--- web/src/lib/components/hero.svelte
+++ web/src/lib/components/hero.svelte
@@ -16,7 +16,7 @@
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
             <div class="text-center">
-                <h1 class="text-4xl font-normal tracking-tight text-white sm:text-6xl">
+                <h1 class="text-4xl font-normal tracking-normal text-white sm:text-6xl">
                     <slot name="title" />
                 </h1>
                 <p class="mt-6 text-lg leading-8 font-light text-white">
