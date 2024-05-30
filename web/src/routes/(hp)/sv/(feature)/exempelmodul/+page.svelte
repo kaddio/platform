924080ce778ecd0e786a875b18302f456d6be854
rhymn <david@pnd.se>
2024-05-30 14:14:24 +0200

    quotes

--- web/src/routes/(hp)/sv/(feature)/exempelmodul/+page.svelte
+++ web/src/routes/(hp)/sv/(feature)/exempelmodul/+page.svelte
@@ -10,7 +10,8 @@
     import ModuleOverviewFeature from "$components/moduleOverviewFeature.svelte";
     import ModuleOverviewFeaturesSegment from "$components/moduleOverviewFeaturesSegment.svelte";
     import ProductFeatureImageOnLeft from "$components/productFeatureImageOnLeft.svelte";
-
+    import Quote from "$components/quote.svelte";
+    import QuotesSection from "$components/quotesSection.svelte";
 </script>
 
 <svelte:head>
@@ -48,7 +49,11 @@
 	</ModuleFeatureWithIcon>
 </ModuleSectionWithImage>
 
-<ProductFeatureImageOnLeft></ProductFeatureImageOnLeft>
+<ProductFeatureImageOnLeft src="/img/ana-with-skeleton.png" alt="Ana Casanovas is giving a treatment">
+	<span slot="pre">Själslig boost</span>
+	<span slot="title">Därför använder Ana Kaddio</span>
+	<span slot="body">Distinctio et nulla eum soluta et neque labore quibusdam. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione. Saepe et quasi iusto modi velit ut non voluptas in. Explicabo id ut laborum.</span>
+</ProductFeatureImageOnLeft>
 
 <ModuleOverviewFeaturesSection>
 	<span slot="pre">Innehåll</span>
@@ -89,6 +94,35 @@
 		<ModuleOverviewFeature>Visa bokning på din hemsida</ModuleOverviewFeature>
 	</ModuleOverviewFeaturesSegment>
 </ModuleOverviewFeaturesSection>  
+
+<QuotesSection>
+    <span slot="title">
+        Vad några kunder säger om Exempelmodulen       
+    </span>
+
+    <Quote>
+        Vi på Din Psykolog har haft stor hjälp av Kaddio i vårt arbete i att utveckla framtidens digitala vård, och de har varit väldigt tillmötesgående i utvecklingen av specifika funktioner vi behövt. Vi kan varmt rekommendera Kaddio!
+        <span slot="quoted">
+            Ramzi Jelassi <cite>Grundare  <a href="https://dinpsykolog.se" target="_blank" rel="noreferrer">Din Psykolog</a></cite>
+        </span>
+    </Quote>
+    <Quote>
+        Det är ett rent nöje att fakturera med kaddio! Så smidigt och tydligt. Och så gör det ju skillnad att det är en snygg miljö också, mina andra program har sett ut som ... 😉. Tack för allt ni gör, min jobbdag blir så mycket härligare i kaddio!
+        <span slot="quoted">
+            Ulrika Hiscoke <cite>Leg Psykolog, medicine doktor <a href="https://arbor.kaddio.com" target="_blank" rel="noreferrer">Arbor Psykologi</a></cite>
+        </span>
+    </Quote>
+    
+    <Quote>
+        Kaddio är användarvänligt, funktionellt och passar vår verksamhet perfekt!
+        <span slot="quoted">
+            Jimmy Lundqvist <cite>Chef IKSU fysio <a href="https://iksu.se" target="_blank" rel="noreferrer">IKSU</a></cite>
+        </span>
+    </Quote>
+    
+</QuotesSection>
+
+
 <!-- 
 <FaqSection>
 	<span slot="title">Vanliga frågor</span>
