2a12112de0fab3532ecbd9fe8a6c5914a978d241
rhymn <david@pnd.se>
2024-06-03 21:30:42 +0200

    product pricing

--- web/src/components/productPricingSection.svelte
+++ web/src/components/productPricingSection.svelte
@@ -6,10 +6,10 @@
     </div>
     <div class="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
       <div class="p-8 sm:p-10 lg:flex-auto">
-        <h3 class="text-2xl font-bold tracking-tight text-gray-900">Betala bara för vad du använder</h3>
-        <p class="mt-6 text-base leading-7 text-gray-600">Varje användare av Kaddio betalar en grundkostnad på 159 SEK/månad, vi kallar detta för "Core".</p>
+        <h3 class="text-2xl font-bold tracking-tight text-gray-900">Ingår alltid i Kaddio</h3>
+        <p class="mt-6 text-base leading-7 text-gray-600">Kaddio är ett helhetssystem och några saker ingår alltid</p>
         <div class="mt-10 flex items-center gap-x-4">
-          <h4 class="flex-none text-sm font-semibold leading-6 text-indigo-600">Vad ingår</h4>
+          <h4 class="flex-none text-sm font-semibold leading-6 text-indigo-600">Detta ingår alltid</h4>
           <div class="h-px flex-auto bg-gray-100"></div>
         </div>
         <ul role="list" class="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6">
@@ -23,26 +23,41 @@
             <svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
             </svg>
-            Member resources
+            Rapporter
           </li>
           <li class="flex gap-x-3">
             <svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
             </svg>
-            Entry to annual conference
+            Uppgifter
           </li>
           <li class="flex gap-x-3">
             <svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
             </svg>
-            Official member t-shirt
+            Hemsida
           </li>
+
+          <li class="flex gap-x-3">
+            <svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+              <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+            </svg>
+            Hemsida
+          </li>
+
+                    <li class="flex gap-x-3">
+            <svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+              <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+            </svg>
+            Hemsida
+          </li>
+
         </ul>
       </div>
       <div class="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
         <div class="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
           <div class="mx-auto max-w-xs px-8">
-            <p class="text-base font-semibold text-gray-600">Per användare och månad</p>
+            <p class="text-base font-semibold text-gray-600">1 månad kostnadfritt</p>
             <p class="mt-6 flex items-baseline justify-center gap-x-2">
               <span class="text-5xl font-bold tracking-tight text-gray-900">119</span>
               <span class="text-sm font-semibold leading-6 tracking-wide text-gray-600">SEK</span>
