d1cf3398e88c85d47dd0200c90598d1f8a4dac29
rhymn <david@pnd.se>
2024-06-12 14:34:42 +0200

    simpler signature

--- web/src/lib/browser-fingerprint.ts
+++ web/src/lib/browser-fingerprint.ts
@@ -1,15 +1,13 @@
 import type { RequestEvent } from '@sveltejs/kit';
 
-const fingerprint = function (userAgent: string, acceptLanguage: string, ip: string) {
+const fingerprint = function (str: string) {
     const stringToHash = function (s: string) {
         return s.split('').reduce((hash, char) => {
             return char.charCodeAt(0) + (hash << 6) + (hash << 16) - hash;
         }, 0);
     };
 
-    const validityInterval = Math.round(new Date() / 1000 / 3600 / 24 / 4);
-
-    return stringToHash(`${ip};${userAgent};${acceptLanguage};${validityInterval}`);
+    return stringToHash(str);
 };
 
 export const browserFingerprint = (request: RequestEvent, ip: string) => {
@@ -21,5 +19,5 @@ export const browserFingerprint = (request: RequestEvent, ip: string) => {
         ip = '127.0.0.1';
     }
 
-    return fingerprint(userAgent, acceptLanguage, ip);
+    return fingerprint(`${ip};${userAgent};${acceptLanguage}`);
 };
