c7f338951903e071fe9dd7e17b386dad2b5f484d
rhymn <david@pnd.se>
2024-06-12 21:43:38 +0200

    v2 uses functions

--- web/src/routes/(hp)/sv/hitta-klinik/[[keyword]]/[[place]]/[[page]]/+page.svelte
+++ web/src/routes/(hp)/sv/hitta-klinik/[[keyword]]/[[place]]/[[page]]/+page.svelte
@@ -219,8 +219,8 @@
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
             <div class="mx-auto max-w-2xl text-center">
                 {#if data.organizations.length}
-                    <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{data.keyword} {data.place}</h2>
-                    <p class="mt-2 text-lg leading-8 text-gray-600">Visar {data.organizations.length} av {data.count}</p>
+                    <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{titleMarketplace(data.keyword, data.place)}</h2>
+                    <p class="mt-2 text-lg leading-8 text-gray-600">{hits(data.count)}</p>
 
                 {:else}
                     <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Inga resultat</h2>
