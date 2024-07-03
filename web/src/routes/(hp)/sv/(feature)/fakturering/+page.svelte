deb662498addf19eb1dc5be6b291f259808b7134
LenaKaddio <lena@kaddio.com>
2024-07-03 16:18:16 +0200

    quote

--- web/src/routes/(hp)/sv/(feature)/fakturering/+page.svelte
+++ web/src/routes/(hp)/sv/(feature)/fakturering/+page.svelte
@@ -101,26 +101,13 @@
 
 <QuotesSection>
     <span slot="title">
-        Vad några kunder säger om Exempelmodulen       
+        Vad några kunder säger om Fakturering       
     </span>
 
-    <Quote>
-        Vi på Din Psykolog har haft stor hjälp av Kaddio i vårt arbete i att utveckla framtidens digitala vård, och de har varit väldigt tillmötesgående i utvecklingen av specifika funktioner vi behövt. Vi kan varmt rekommendera Kaddio!
-        <span slot="quoted">
-            Ramzi Jelassi <cite>Grundare  <a href="https://dinpsykolog.se" target="_blank" rel="noreferrer">Din Psykolog</a></cite>
-        </span>
-    </Quote>
     <Quote>
         Det är ett rent nöje att fakturera med kaddio! Så smidigt och tydligt. Och så gör det ju skillnad att det är en snygg miljö också, mina andra program har sett ut som ... 😉. Tack för allt ni gör, min jobbdag blir så mycket härligare i kaddio!
         <span slot="quoted">
             Ulrika Hiscoke <cite>Leg Psykolog, medicine doktor <a href="https://arbor.kaddio.com" target="_blank" rel="noreferrer">Arbor Psykologi</a></cite>
-        </span>
-    </Quote>
-    
-    <Quote>
-        Kaddio är användarvänligt, funktionellt och passar vår verksamhet perfekt!
-        <span slot="quoted">
-            Jimmy Lundqvist <cite>Chef IKSU fysio <a href="https://iksu.se" target="_blank" rel="noreferrer">IKSU</a></cite>
         </span>
     </Quote>
     
