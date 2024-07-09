b3efa28a9fbe469a5dc1e276ee1e8a8642a547a7
LenaKaddio <lena@kaddio.com>
2024-07-09 14:19:57 +0200

    copy

--- web/src/routes/(hp)/sv/(feature)/journalsystem/+page.svelte
+++ web/src/routes/(hp)/sv/(feature)/journalsystem/+page.svelte
@@ -29,7 +29,7 @@
 	<span slot="h1">Journalsystem</span>
 	<span slot="title">Kaddio Journal</span>
 	<span slot="sub">
-		Kaddio Journal är journalsystemet som har allt du behöver för att dokumentera verksamheten och journalföra klienter. Ett komplett journalsystem för vård helt enkelt.
+		Kaddio Journal har allt du behöver för att dokumentera verksamheten och journalföra klienter. Ett komplett journalsystem för vård helt enkelt.
 	</span>
 
 	<ModuleFeatureWithIcon icon="pen">
