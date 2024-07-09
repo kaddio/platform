851929dc634b03a42305b6137300bedb24f2e0e6
LenaKaddio <lena@kaddio.com>
2024-07-09 14:40:09 +0200

    copy

--- web/src/routes/(hp)/sv/(feature)/journalsystem/+page.svelte
+++ web/src/routes/(hp)/sv/(feature)/journalsystem/+page.svelte
@@ -66,7 +66,7 @@
 <ModuleOverviewFeaturesSection>
 	<span slot="pre">Innehåll</span>
 	<span slot="title">Detta är Kaddio Journal</span>
-	<span slot="body">Med Kaddio Journal journalför du säkert och smidigt på det sätt som passar din verksamhet. Kaddio sköter datalagringen. Du får tid över till annat.</span>
+	<span slot="body">Med Kaddio Journal journalför du säkert och smidigt på det sätt som passar din verksamhet. Kaddio sköter datalagring och säkerhetskopiering. Du får tid över till annat.</span>
 
 	<ModuleOverviewFeaturesSegment>
 		<span slot="title">Dokumentera</span>
