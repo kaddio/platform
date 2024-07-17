76618035fe4560e54b8d5764c8db6467cfe9dfef
LenaKaddio <lena@kaddio.com>
2024-07-17 09:32:23 +0200

    typo-en

--- web/src/routes/(hp)/en/securitySectionEn.svelte
+++ web/src/routes/(hp)/en/securitySectionEn.svelte
@@ -3,6 +3,6 @@
 
 </script>
 <SecuritySection src="/sv/legal/privacy">
-	<span slot="title">Handle sensitive date securely</span>
+	<span slot="title">Handle sensitive data securely</span>
 	<span slot="subtitle">Everything in Kaddio is encrypted. Kaddio supports multi-factor authentication via Mobile BankID and one-time password via SMS.</span>
 </SecuritySection>
