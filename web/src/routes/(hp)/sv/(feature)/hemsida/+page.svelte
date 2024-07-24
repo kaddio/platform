6554aac88c98e2ce5b80309bc48a81fd44f609e3
LenaKaddio <lena@kaddio.com>
2024-07-24 12:02:09 +0200

    copy-hemsida

--- web/src/routes/(hp)/sv/(feature)/hemsida/+page.svelte
+++ web/src/routes/(hp)/sv/(feature)/hemsida/+page.svelte
@@ -23,32 +23,33 @@
 <svelte:head>
     <meta name="robots" content="noindex">
 
-	<title>Exempel med allt du behöver</title>
+	<title>Kaddio Hemsida</title>
 	<meta name="description" content="Komplett bokningssystem och betalning som gör onlinebokningen för dina klienter supersmidig, Kaddio gör även allt annat du förväntar dig." />
 </svelte:head>
 
 <ModuleSectionWithImage src="/img/kaddio-screenshots/cal-se.png" alt="Kaddio onlinebokning och kalender">
-	<span slot="h1">Modulnamn</span>
-	<span slot="title">Kaddio Modulnamn</span>
+	<span slot="h1">Hemsida och söksida</span>
+	<span slot="title">Kaddio Hemsida</span>
 	<span slot="sub">
-		Superöversiktlig beskrivning av allt modulen gör med peppiga ord som komplett, smidig, effektiv, etc. Riktat till dig som kund.</span>
+		Kaddio Hemsida är en automatiskt genererad hemsida för din organisation och samtidigt en sökfunktion för blivande klienter. 
+		Modulen ingår i abonnemanget.</span>
 
 	<ModuleFeatureWithIcon icon="calendar-lines-pen">
-		<span slot="title">Nyckeverb 1</span>
+		<span slot="title">Presentera</span>
 		<span slot="body">
 			Kort text som beskriver hur du som användare gör och hur det löser alla dina problem. Starta med verb. Ej för detaljerat.
 		</span>
 	</ModuleFeatureWithIcon>
 
 	<ModuleFeatureWithIcon icon="people-pulling">
-		<span slot="title">Samarbeta</span>
+		<span slot="title">Effektivisera</span>
 		<span slot="body">
 			Använd kalendern för att boka möten och välj enkelt vilka som ska vara med. Bestäm själv vilka kollegor som ska se vad.
 		</span>
 	</ModuleFeatureWithIcon>
 
 	<ModuleFeatureWithIcon icon="stopwatch">
-		<span slot="title">Effektivisera</span>
+		<span slot="title">Optimera</span>
 		<span slot="body">
 			Skapa mallar och spara tid. 
 		</span>
@@ -57,13 +58,13 @@
 
 <ProductFeatureImageOnLeft src="/img/ana-with-skeleton.png" alt="Ana Casanovas is giving a treatment">
 	<span slot="pre">Inspirerande text</span>
-	<span slot="title">Därför använder X Kaddio Modulnamn</span>
+	<span slot="title">Därför använder X Kaddio Hemsida</span>
 	<span slot="body">Beskrivande text med exempel från verklig kund hur denna modul hjälper just dem i sitt arbete. Distinctio et nulla eum soluta et neque labore quibusdam. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione. Saepe et quasi iusto modi velit ut non voluptas in. Explicabo id ut laborum.</span>
 </ProductFeatureImageOnLeft>
 
 <ModuleOverviewFeaturesSection>
 	<span slot="pre">Innehåll</span>
-	<span slot="title">Detta är Kaddio Onlinebokning</span>
+	<span slot="title">Detta är Kaddio Hemsida</span>
 	<span slot="body">Kort intro till allt denna modul innehållerKaddio kommer fullsmackat med grejer, för att ge dig en härlig överblick så har vi gjort en tabell. Distinctio et nulla eum soluta et neque labore quibusdam. Saepe et quasi iusto modi velit ut non voluptas in. Explicabo id ut laborum.</span>
 
 	<ModuleOverviewFeaturesSegment>
@@ -103,7 +104,7 @@
 
 <QuotesSection>
     <span slot="title">
-        Vad våra kunder säger om Exempelmodulen       
+        Vad våra kunder säger om Hemsida       
     </span>
 
     <Quote>
