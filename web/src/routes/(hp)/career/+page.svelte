2bd665e99438123a8abefba01aa9af83a68080e1
Camilla <camilla@kaddio.com>
2024-08-20 14:17:06 +0200

    textändring

--- web/src/routes/(hp)/career/+page.svelte
+++ web/src/routes/(hp)/career/+page.svelte
@@ -24,7 +24,8 @@
       <div class="w-full lg:max-w-lg lg:flex-auto">
         <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">We're always looking for awesome people</h2>
         <p class="mt-6 text-xl leading-8 text-gray-600">At Kaddio, we are a small team that works in close collaboration with each other. If you like being involved in the entire development process, being able to influence the end result and at the same time having great freedom with what and how you work, you will enjoy working with us!</p>
-        <p class="mt-6 text-xl leading-8 text-gray-600">As a silverlining on our offer we only work 6 hour days at our cosy office in central Göteborg, or if we like we can work remotely.  We speak Swedish and English and as a bonus we get amazing coffee from Kafferäven.</p>
+        <p class="mt-6 text-xl leading-8 text-gray-600">As a silverlining on our offer we only work 6 hour days at our cosy office in central Göteborg, or if you like we can work remotely some days
+          .  We speak Swedish and English and as a bonus we get amazing coffee from Kafferäven.</p>
         <img src="/img/david-arvid-kaffe.jpg" alt="" class="mt-16 aspect-[6/5] w-full rounded-2xl bg-gray-50 object-cover lg:aspect-auto lg:h-[34.5rem]">
       </div>
       <div class="w-full lg:max-w-xl lg:flex-auto">
@@ -40,7 +41,8 @@
                 </a>
               </dd>
               <dt class="sr-only">Description</dt>
-              <dd class="mt-2 w-full flex-none text-base leading-7 text-gray-600">You will manage and develop our product "Kaddio" that helps 1000+ helath care proffessionals in their daily work. Our language is Javascript and you should like to work both frontend and backend. Experience of meteor and svelte is a big plus.   </dd>
+              <dd class="mt-2 w-full flex-none text-base leading-7 text-gray-600">You will manage and develop our product "Kaddio" that helps 1000+ helath care proffessionals in their daily work. <br><br> Our stack includes Javascript/typescript, Angular.js, Svelte, Tailwind, Meteor, MongoDB. You should like to work both frontend and backend. 
+                <br><br>...and if you have a good sense of rhythm we need a drummer for our office band!   </dd>
               <dt class="sr-only">Location</dt>
               <dd class="mt-4 flex items-center gap-x-3 text-base leading-7 text-gray-500">
                 <svg viewBox="0 0 2 2" class="h-0.5 w-0.5 flex-none fill-gray-300" aria-hidden="true">
@@ -50,6 +52,7 @@
               </dd>
             </dl>
           </li>
+          <!--
           <li class="py-8">
             <dl class="relative flex flex-wrap gap-x-3">
               <dt class="sr-only">Role</dt>
@@ -69,7 +72,7 @@
                 Göteborg, Sweden
               </dd>
             </dl>
-          </li>
+          </li>-->
         </ul>
 
         <!-- <div class="mt-8 flex border-t border-gray-100 pt-8">
