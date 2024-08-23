c0916f77e7d8a04a4db0f6f18ea00991d6ec68e4
rhymn <david@pnd.se>
2024-08-23 10:54:03 +0200

    quotes fix

--- web/src/components/quote.svelte
+++ web/src/components/quote.svelte
@@ -7,7 +7,9 @@
         <!-- <img class="h-10 w-10 rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""> -->
         <div>
           <div class="font-semibold text-gray-900"><slot name="quoted" /></div>
-          <!-- <div class="text-gray-600">@lesliealexander</div> -->
+          <div class="text-gray-600">
+              <slot name="author"/>
+          </div>
         </div>
       </figcaption>
     </figure>
--- web/src/routes/(hp)/sv/quotesSv.svelte
+++ web/src/routes/(hp)/sv/quotesSv.svelte
@@ -13,45 +13,66 @@
     <Quote>
         Vi på Din Psykolog har haft stor hjälp av Kaddio i vårt arbete i att utveckla framtidens digitala vård, och de har varit väldigt tillmötesgående i utvecklingen av specifika funktioner vi behövt. Vi kan varmt rekommendera Kaddio!
         <span slot="quoted">
-            Ramzi Jelassi <cite>Grundare  <a href="https://dinpsykolog.se" target="_blank" rel="noreferrer">Din Psykolog</a></cite>
+            Ramzi Jelassi
+        </span>
+        <span slot="author">
+            Grundare, <a href="https://dinpsykolog.se" target="_blank" rel="noreferrer">Din Psykolog</a>
         </span>
     </Quote>
     <Quote>
         Det är ett rent nöje att fakturera med kaddio! Så smidigt och tydligt. Och så gör det ju skillnad att det är en snygg miljö också, mina andra program har sett ut som ... 😉. Tack för allt ni gör, min jobbdag blir så mycket härligare i kaddio!
         <span slot="quoted">
-            Ulrika Hiscoke <cite>Leg Psykolog, medicine doktor <a href="https://arbor.kaddio.com" target="_blank" rel="noreferrer">Arbor Psykologi</a></cite>
+            Ulrika Hiscoke
+        </span>
+        <span slot="author">
+            Leg Psykolog, medicine doktor, <a href="https://arbor.kaddio.com" target="_blank" rel="noreferrer">Arbor Psykologi</a>
         </span>
     </Quote>
     
     <Quote>
         Kaddio är användarvänligt, funktionellt och passar vår verksamhet perfekt!
         <span slot="quoted">
-            Jimmy Lundqvist <cite>Chef IKSU fysio <a href="https://iksu.se" target="_blank" rel="noreferrer">IKSU</a></cite>
+            Jimmy Lundqvist
+        </span>
+        <span slot="author">
+            Chef, IKSU fysio <a href="https://iksu.se" target="_blank" rel="noreferrer">IKSU</a>
         </span>
     </Quote>
     
     <Quote>
         Jag har samma krav på mitt journalföringssystem som mina behandlingar: effektivitet. Därför använder jag Kaddio.
         <span slot="quoted">
-            Jonas Evander <cite>Leg Psykolog <a href="https://afobia.se" target="_blank" rel="noreferrer">Afobia</a></cite>
+            Jonas Evander
+        </span>
+        <span slot="author">
+            Leg Psykolog, <a href="https://afobia.se" target="_blank" rel="noreferrer">Afobia</a>            
         </span>
     </Quote>
     <Quote>
         Kaddio är det journalprogram jag valt pga enkelheten. Programmet är enkelt att navigera och fungerar smidigt att anpassa efter mina behov, både i journalföring och tidsbokning.
         <span slot="quoted">
-            Amanda Follin <cite>Certifierad massageterapeut <a href="https://www.endorfinkliniken.com" target="_blank" rel="noreferrer">Endorfinkliniken</a></cite>
+            Amanda Follin
+        </span>
+        <span slot="author">
+            Certifierad massageterapeut, <a href="https://www.endorfinkliniken.com" target="_blank" rel="noreferrer">Endorfinkliniken</a>
         </span>
     </Quote>
     <Quote>
         Programmet har allt jag behöver och det är lätt att använda och anpassa efter mina behov. Jag kan verkligen rekommendera detta journalsystem.
         <span slot="quoted">
-            Sofia Viotti <cite>Leg Psykolog Compassionfocus
+            Sofia Viotti
+        </span>
+        <span slot="author">
+            Leg Psykolog, Compassionfocus
         </span>
     </Quote>
     <Quote>
         Ett funktionellt och enkelt journalsystem, med snabb och proffsig support. Fungerar perfekt med samlingsräkning.
         <span slot="quoted">
-            Kim Olausson <cite>Leg Fysioterapeut <a href="https://fysiokompassen.se" target="_blank" rel="noreferrer">Fysiokompassen</a></cite>
+            Kim Olausson
+        </span>
+        <span slot="author">
+            Leg Fysioterapeut, <a href="https://fysiokompassen.se" target="_blank" rel="noreferrer">Fysiokompassen</a>
         </span>
     </Quote>    
 </QuotesSection>
