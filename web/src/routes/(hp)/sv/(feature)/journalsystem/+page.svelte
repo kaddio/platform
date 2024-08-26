1ea239491d6168368e639afe8c13f349fbaf5750
LenaKaddio <lena@kaddio.com>
2024-08-26 11:19:33 +0200

    daniel-sv

--- web/src/routes/(hp)/sv/(feature)/journalsystem/+page.svelte
+++ web/src/routes/(hp)/sv/(feature)/journalsystem/+page.svelte
@@ -24,6 +24,7 @@
 	import QuoteSvRamzi from "../../quote-sv-ramzi.svelte";
 	import QuoteSvSofia from "../../quote-sv-sofia.svelte";
 	import QuoteSvUlrika from "../../quote-sv-ulrika.svelte";
+	import ContactDanielSv from "../../contactDanielSv.svelte";
 </script>
 
 <svelte:head>
@@ -111,58 +112,10 @@
 	<QuoteSvRamzi />
 	<QuoteSvJimmy />
 	<QuoteSvJonas />
-	<Quote>
-        Vi på Din Psykolog har haft stor hjälp av Kaddio i vårt arbete i att utveckla framtidens digitala vård, och de har varit väldigt tillmötesgående i utvecklingen av specifika funktioner vi behövt. Vi kan varmt rekommendera Kaddio!
-        <span slot="quoted">
-            Ramzi Jelassi <cite>Grundare  <a href="https://dinpsykolog.se" target="_blank" rel="noreferrer">Din Psykolog</a></cite>
-        </span>
-    </Quote>
-	<Quote>
-        Kaddio är användarvänligt, funktionellt och passar vår verksamhet perfekt!
-        <span slot="quoted">
-            Jimmy Lundqvist <cite>Chef IKSU fysio <a href="https://iksu.se" target="_blank" rel="noreferrer">IKSU</a></cite>
-        </span>
-    </Quote>
-	<Quote>
-        Jag har samma krav på mitt journalföringssystem som mina behandlingar: effektivitet. Därför använder jag Kaddio.
-        <span slot="quoted">
-            Jonas Evander <cite>Leg psykolog  <a href="https://afobia.se" target="_blank" rel="noreferrer">Afobia</a></cite>
-        </span>
-    </Quote>
-	<Quote>
-        Kaddio är det journalprogram jag valt pga enkelheten. Programmet är enkelt att navigera och fungerar smidigt att anpassa efter mina behov, både i journalföring och tidsbokning.
-        <span slot="quoted">
-            Amanda Follin <cite>Certifierad massageterapeut  <a href="https://endorfinkliniken.com" target="_blank" rel="noreferrer">Endorfinkliniken</a></cite>
-        </span>
-    </Quote>
-	<Quote>
-        Programmet har allt jag behöver och det är lätt att använda och anpassa efter mina behov. Jag kan verkligen rekommendera detta journalsystem.
-        <span slot="quoted">
-            Sofia Viotti <cite>Leg Psykolog Compassionfocus
-        </span>
-    </Quote>
+	<QuoteSvAmanda />
+	<QuoteSvSofia />
 
-    
 </QuotesSection>
-
-<!-- <ProductPricingSection></ProductPricingSection> -->
-
-<!--<StatsSectionWithText>
-    <StatsSectionWithTextPart>
-		<span slot="title">användare</span>
-		<span slot="sub">47 000+</span>
-    </StatsSectionWithTextPart>
-
-    <StatsSectionWithTextPart>
-		<span slot="title">bokningar per dygn</span>
-		<span slot="sub">1300+</span>
-    </StatsSectionWithTextPart>
-
-    <StatsSectionWithTextPart>
-		<span slot="title">videomöten per månad</span>
-		<span slot="sub">8500+</span>
-    </StatsSectionWithTextPart>
-</StatsSectionWithText>-->
 
 <StatsSection>
 	<StatsSectionPart>
@@ -179,101 +132,4 @@
 	</StatsSectionPart>
 </StatsSection>
 
-<SalesSection></SalesSection>
-
-<!-- <FaqSection>
-	<span slot="title">Detaljer</span>
-
-	<Qa>
-		<span slot="q">Anteckningsmallar</span>
-		<span slot="a">
-			Skapa dina egna mallar för journalanteckningar så de passar din verksamhet. Välj dina egna sökord/rubriker. Använd malltext, fraser, filuppladdning och mer.
-		</span>
-	</Qa>
-
-	<Qa>
-		<span slot="q">Journalformulär</span>
-		<span slot="a">
-			Skapa dina egna formulär eller använd vanligt förekommande standardformulär direkt från Kaddio. Skicka skattningsformulär och hälsodeklarationer via e-post och sms. Journalformulär kan skickas automatiskt i samband med bokning eller manuellt vid behov. Svaren sparas i journalen.
-		</span>
-	</Qa>
-
-	<Qa>
-		<span slot="q">Brev och remisser</span>
-		<span slot="a">
-			Skapa brev, remisser, remissvar och intyg. Skicka med post, via Kaddios chat eller ladda ner som PDF.
-		</span>
-	</Qa>
-
-	<Qa>
-		<span slot="q">ICD-10</span>
-		<span slot="a">
-			Sök med kod eller fritext och lägg till diagnoser i varje journalanteckning.
-		</span>
-	</Qa>
-
-	<Qa>
-		<span slot="q">Behörigheter</span>
-		<span slot="a">
-			Välj om alla i verksamheten ska kunna läsa klienters journal eller om behörigheten ska begränsas till vissa behandlare.
-		</span>
-	</Qa>
-
-	<Qa>
-		<span slot="q">Rita på standardbilder</span>
-		<span slot="a">
-			Rita på Kaddios standardbilder för att dokumentera till exempel var klienten upplever smärta.
-		</span>
-	</Qa>
-
-	<Qa>
-		<span slot="q">Rita på egna bilder</span>
-		<span slot="a">
-			Ladda upp egna bilder att rita på så att det passar din verksamhet.
-		</span>
-	</Qa>
-
-	<Qa>
-		<span slot="q">Lista osignerade anteckningar</span>
-		<span slot="a">
-			Håll koll på anteckningar som väntar på att bli signerade.
-		</span>
-	</Qa>
-
-	<Qa>
-		<span slot="q">Exportera/skriv ut</span>
-		<span slot="a">
-			Exportera och skriv ut enskild journalanteckning eller hela journalen till PDF.
-		</span>
-	</Qa>
-
-	<Qa>
-		<span slot="q">Loggning</span>
-		<span slot="a">
-			Läsning och redigering loggas. Loggen visar vad som gjorts med en viss klients journal och av vem, när och från vilken ip-adress.
-		</span>
-	</Qa>
-
-	<Qa>
-		<span slot="q">API</span>
-		<span slot="a">
-			Läsning och redigering loggas. Loggen visar vad som gjorts med en viss klients journal och av vem, när och från vilken ip-adress.
-		</span>
-	</Qa>
-
-</FaqSection>
-
-{#if data.pkgs?.length > 0}
-	<FaqSection>
-		<span slot="title">Inbyggda formulär i Kaddio</span>
-
-		{#each data.pkgs as pkg}
-			<Qa>
-				<span slot="q">{pkg.name}</span>
-				<span slot="a">
-					{pkg.desc}
-				</span>
-			</Qa>
-		{/each}
-	</FaqSection>
-{/if} -->
+<ContactDanielSv />
