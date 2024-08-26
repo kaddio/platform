10f6af2974ad9ccdf57b83d0e54affa7ec7cbb06
LenaKaddio <lena@kaddio.com>
2024-08-26 11:15:54 +0200

    layout-fixes

--- web/src/routes/(hp)/sv/(feature)/journalsystem/+page.svelte
+++ web/src/routes/(hp)/sv/(feature)/journalsystem/+page.svelte
@@ -4,9 +4,31 @@
     import ModuleSectionWithImage from "$components/moduleSectionWithImage.svelte";
     import ModuleFeatureWithIcon from "$components/moduleFeatureWithIcon.svelte";
 	export let data;
+	import ModuleFeaturesSection from "$components/moduleOverviewFeaturesSection.svelte";
+    import ModuleOverviewFeaturesSection from "$components/moduleOverviewFeaturesSection.svelte";
+    import ModuleOverviewFeature from "$components/moduleOverviewFeature.svelte";
+    import ModuleOverviewFeaturesSegment from "$components/moduleOverviewFeaturesSegment.svelte";
+    import ProductFeatureImageOnLeft from "$components/productFeatureImageOnLeft.svelte";
+    import Quote from "$components/quote.svelte";
+    import QuotesSection from "$components/quotesSection.svelte";
+    import ProductPricingSection from "$components/productPricingSection.svelte";
+    import StatsSection from "$components/statsSection.svelte";
+    import StatsSectionPart from "$components/statsSectionPart.svelte";
+    import StatsSectionWithText from "$components/statsSectionWithText.svelte";
+    import StatsSectionWithTextPart from "$components/statsSectionWithTextPart.svelte";
+	import SalesSection from "$components/salesSection.svelte";
+	import QuoteSvAmanda from "../../quote-sv-amanda.svelte";
+	import QuoteSvJimmy from "../../quote-sv-jimmy.svelte";
+	import QuoteSvJonas from "../../quote-sv-jonas.svelte";
+	import QuoteSvKim from "../../quote-sv-kim.svelte";
+	import QuoteSvRamzi from "../../quote-sv-ramzi.svelte";
+	import QuoteSvSofia from "../../quote-sv-sofia.svelte";
+	import QuoteSvUlrika from "../../quote-sv-ulrika.svelte";
 </script>
 
 <svelte:head>
+	<meta name="robots" content="noindex">
+
 	<title>Journalsystem med allt du behöver</title>
 	<meta name="description" content="Kaddio Journal har allt du behöver för att dokumentera verksamheten och journalföra klienter. Ett komplett journalsystem för vård." />
 </svelte:head>
@@ -15,18 +37,18 @@
 	<span slot="h1">Journalsystem</span>
 	<span slot="title">Kaddio Journal</span>
 	<span slot="sub">
-		Kaddio Journal är journalsystemet som har allt du behöver för att dokumentera verksamheten och journalföra klienter. Ett komplett journalsystem för vård helt enkelt.
+		Kaddio Journal har allt du behöver för att dokumentera verksamheten och journalföra klienter. Ett komplett journalsystem för vård helt enkelt.
 	</span>
 
 	<ModuleFeatureWithIcon icon="pen">
-		<span slot="title">Dokumentera</span>
+		<span slot="title">Dokumentera.</span>
 		<span slot="body">
 			Journalför på ett säkert sätt, där och när du vill. Slipp pappershantering, pärmar och backup.
 		</span> 
 	</ModuleFeatureWithIcon>
 
 	<ModuleFeatureWithIcon icon="sliders">
-		<span slot="title">Anpassa</span>
+		<span slot="title">Anpassa.</span>
 		<span slot="body">
 			Utforma mallar för journalanteckningar utifrån verksamhetens behov. 
 			<!-- Spara tid genom att sätta upp mallar med malltexter, lägg in egna bilder att rita på samt egna kortkommandon för dina vanliga fraser. -->
@@ -34,7 +56,7 @@
 	</ModuleFeatureWithIcon>
 
 	<ModuleFeatureWithIcon icon="gamepad-modern">
-		<span slot="title">Kontrollera</span>
+		<span slot="title">Kontrollera.</span>
 		<span slot="body">
 			Styr åtkomst till klienter och journaler. Du bestämmer själv vem som kommer åt vad.
 			<!-- Följ aktivitet i journaler genom Kaddios log för spårbarhet och regelefterlevnad. -->
@@ -43,8 +65,123 @@
 
 </ModuleSectionWithImage>
 
+<ModuleOverviewFeaturesSection>
+	<span slot="title">Detta är Kaddio Journal</span>
+	<span slot="body">Med Kaddio Journal journalför du säkert och smidigt på det sätt som passar din verksamhet. Kaddio sköter datalagringen. Du får tid över till annat.</span>
 
-<FaqSection>
+	<ModuleOverviewFeaturesSegment>
+		<span slot="title">Dokumentera</span>
+		
+		<ModuleOverviewFeature>Krypterad journalföring</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Bilder med ritfunktion</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Filuppladdning i journalanteckningar</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Diagnoser enligt ICD-10</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Standardformulär direkt i journalen</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Export av journal eller enskilda anteckning som PDF</ModuleOverviewFeature>
+	</ModuleOverviewFeaturesSegment>
+
+	<ModuleOverviewFeaturesSegment>
+		<span slot="title">Anpassa</span>
+		
+		<ModuleOverviewFeature>Fri utformning av anteckningsmallar</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Stöd för malltext</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Stöd för fraser</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Uppladdning av egna bilder att rita på</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Fri utformning av egna formulär</ModuleOverviewFeature>
+	</ModuleOverviewFeaturesSegment>
+
+	<ModuleOverviewFeaturesSegment>
+		<span slot="title">Kontrollera</span>
+		
+		<ModuleOverviewFeature>Behörighetsstyrning för journaler</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Signering och redigering av anteckningar med full historik</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Listning av osignerade anteckningar</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Logg öven läsning och redigering av journaler</ModuleOverviewFeature>
+		
+
+	</ModuleOverviewFeaturesSegment>
+
+</ModuleOverviewFeaturesSection> 
+
+<QuotesSection>
+    <span slot="title">
+        Vad våra kunder säger om Kaddio Journal       
+    </span>
+
+	<QuoteSvRamzi />
+	<QuoteSvJimmy />
+	<QuoteSvJonas />
+	<Quote>
+        Vi på Din Psykolog har haft stor hjälp av Kaddio i vårt arbete i att utveckla framtidens digitala vård, och de har varit väldigt tillmötesgående i utvecklingen av specifika funktioner vi behövt. Vi kan varmt rekommendera Kaddio!
+        <span slot="quoted">
+            Ramzi Jelassi <cite>Grundare  <a href="https://dinpsykolog.se" target="_blank" rel="noreferrer">Din Psykolog</a></cite>
+        </span>
+    </Quote>
+	<Quote>
+        Kaddio är användarvänligt, funktionellt och passar vår verksamhet perfekt!
+        <span slot="quoted">
+            Jimmy Lundqvist <cite>Chef IKSU fysio <a href="https://iksu.se" target="_blank" rel="noreferrer">IKSU</a></cite>
+        </span>
+    </Quote>
+	<Quote>
+        Jag har samma krav på mitt journalföringssystem som mina behandlingar: effektivitet. Därför använder jag Kaddio.
+        <span slot="quoted">
+            Jonas Evander <cite>Leg psykolog  <a href="https://afobia.se" target="_blank" rel="noreferrer">Afobia</a></cite>
+        </span>
+    </Quote>
+	<Quote>
+        Kaddio är det journalprogram jag valt pga enkelheten. Programmet är enkelt att navigera och fungerar smidigt att anpassa efter mina behov, både i journalföring och tidsbokning.
+        <span slot="quoted">
+            Amanda Follin <cite>Certifierad massageterapeut  <a href="https://endorfinkliniken.com" target="_blank" rel="noreferrer">Endorfinkliniken</a></cite>
+        </span>
+    </Quote>
+	<Quote>
+        Programmet har allt jag behöver och det är lätt att använda och anpassa efter mina behov. Jag kan verkligen rekommendera detta journalsystem.
+        <span slot="quoted">
+            Sofia Viotti <cite>Leg Psykolog Compassionfocus
+        </span>
+    </Quote>
+
+    
+</QuotesSection>
+
+<!-- <ProductPricingSection></ProductPricingSection> -->
+
+<!--<StatsSectionWithText>
+    <StatsSectionWithTextPart>
+		<span slot="title">användare</span>
+		<span slot="sub">47 000+</span>
+    </StatsSectionWithTextPart>
+
+    <StatsSectionWithTextPart>
+		<span slot="title">bokningar per dygn</span>
+		<span slot="sub">1300+</span>
+    </StatsSectionWithTextPart>
+
+    <StatsSectionWithTextPart>
+		<span slot="title">videomöten per månad</span>
+		<span slot="sub">8500+</span>
+    </StatsSectionWithTextPart>
+</StatsSectionWithText>-->
+
+<StatsSection>
+	<StatsSectionPart>
+		<span slot="title">användare</span>
+		<span slot="sub">47 000+</span>
+	</StatsSectionPart>
+	<StatsSectionPart>
+		<span slot="title">bokningar per dygn</span>
+		<span slot="sub">1300+</span>
+	</StatsSectionPart>
+	<StatsSectionPart>
+		<span slot="title">videomöten per månad</span>
+		<span slot="sub">8500+</span>
+	</StatsSectionPart>
+</StatsSection>
+
+<SalesSection></SalesSection>
+
+<!-- <FaqSection>
 	<span slot="title">Detaljer</span>
 
 	<Qa>
@@ -139,4 +276,4 @@
 			</Qa>
 		{/each}
 	</FaqSection>
-{/if}
+{/if} -->
