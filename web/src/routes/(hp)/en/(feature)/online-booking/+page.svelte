876a06303cf7a7abf4a1c1db55bbc151b346f3ab
LenaKaddio <lena@kaddio.com>
2024-09-16 10:10:15 +0200

    form

--- web/src/routes/(hp)/en/(feature)/online-booking/+page.svelte
+++ web/src/routes/(hp)/en/(feature)/online-booking/+page.svelte
@@ -1,157 +1,123 @@
 <script>
-	import Qa from "$components/qa.svelte";
-	import FaqSection from "$components/faqSection.svelte";
-	import ModuleSectionWithImage from "$components/moduleSectionWithImage.svelte";
-	import ModuleFeatureWithIcon from "$components/moduleFeatureWithIcon.svelte";
+
+    import ModuleSectionWithImage from "$components/moduleSectionWithImage.svelte";
+    import ModuleFeatureWithIcon from "$components/moduleFeatureWithIcon.svelte";
+    import ModuleOverviewFeaturesSection from "$components/moduleOverviewFeaturesSection.svelte";
+    import ModuleOverviewFeature from "$components/moduleOverviewFeature.svelte";
+    import ModuleOverviewFeaturesSegment from "$components/moduleOverviewFeaturesSegment.svelte";
+	import QuotesSection from "$components/quotesSection.svelte";
+    import StatsSectionEn from "../../statsSectionEn.svelte";
+	import PricesEn from "../../pricesEn.svelte";
+    import ContactDanielEn from "../../contactDanielEn.svelte";
 
 </script>
 
 <svelte:head>
-	<title>Kaddio - Time booking and calendar</title>
-	<meta name="description" content="Easy-to-use time booking and online payment that does everything you expect." />
+	
+	<title>Electronic medical record with everything you need</title>
+	<meta name="description" content="Kaddio Electronic Medical Record is a complete system for documenting your clients." />
 </svelte:head>
 
-<ModuleSectionWithImage src="/img/kaddio-screenshots/cal-se.png" alt="Kaddio onlinebokning och kalender">
-	<span slot="h1">Online booking</span>
-	<span slot="title">Kaddio calendar and booking</span>
+<ModuleSectionWithImage src="/img/kaddio-screenshots/medical-record-overview-en.png" alt="Kaddio Electronic medical record">
+	<span slot="h1">Electronic medical record</span>
+	<span slot="title">Kaddio EMR</span>
 	<span slot="sub">
-		Easy-to-use time booking and online payment that does everything you expect. In Kaddio, the calendar is also your public booking so that you can easily manage bookable times, meetings and group sessions in the same view.
+		Kaddio EMR has everything you need to keep records of your clients and document your business. 
+		Simply a complete electronic medical record system.
 	</span>
 
-	<ModuleFeatureWithIcon icon="calendar-lines-pen">
-		<span slot="title">Plan</span>
+	<ModuleFeatureWithIcon icon="pen">
+		<span slot="title">
+			Document.
+		</span>
 		<span slot="body">
-			Create bookable times directly from your calendar. Configure your booking types in detail. Choose whether your customers book and pay themselves or if you take control of every minute.
+			Keep medical records safely and securely, wherever you are. 
+			Design templates for medical records based on your business needs.
 		</span>
 	</ModuleFeatureWithIcon>
 
-	<ModuleFeatureWithIcon icon="people-pulling">
-		<span slot="title">Collaborate</span>
+	<ModuleFeatureWithIcon icon="sliders">
+		<span slot="title">
+			Follow up.
+		</span>
 		<span slot="body">
-			Use the calendar to schedule meetings and easily choose who should be involved. Decide for yourself which colleagues should see what.				
+			Send letters and referrals. Automate the dispatch and collection of forms. 
+			All information is gathered in the medical records.
 		</span>
 	</ModuleFeatureWithIcon>
 
-	<ModuleFeatureWithIcon icon="stopwatch">
-		<span slot="title">Streamline</span>
+	<ModuleFeatureWithIcon icon="lock-keyhole">
+		<span slot="title">
+			Ensure.
+		</span>
 		<span slot="body">
-			Do you have recurring events in the calendar? Create templates and save time.
+			Regulate access to clients and medical records. Use Kaddio Logs to trace activities in medical records. 
 		</span>
 	</ModuleFeatureWithIcon>
+
 </ModuleSectionWithImage>
-  
 
-<FaqSection>
-	<span slot="title">Details</span>
+<ModuleOverviewFeaturesSection>
+	<span slot="title">This is Kaddio EMR</span>
+	<span slot="body">With Kaddio EMR you keep medical records safely and smoothly in a way that suits 
+		your business. Handle everything in the same system and get more free time.
+ 	</span>
 
-	<Qa>
-		<span slot="q">Calendar</span>
-		<span slot="a">
-			Administrate bookings in the calendar, for example, re-schedule or cancel appointments. The calendar shows bookings side by side with meetings and other activities.
-		</span>
-	</Qa>
+	<ModuleOverviewFeaturesSegment>
+		<span slot="title">Document</span>
+	
+		<ModuleOverviewFeature>Free design of templates for medical records</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Support for template text and phrases</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Upload images to draw on</ModuleOverviewFeature>
+		<ModuleOverviewFeature>File upload to medical record entries</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Diagnoses with ICD-10</ModuleOverviewFeature>
+		<!-- <ModuleOverviewFeature>Standard forms as entries in medical records</ModuleOverviewFeature> -->
+		<ModuleOverviewFeature>Build your own forms</ModuleOverviewFeature>
 
-	<Qa>
-		<span slot="q">Open booking for clients</span>
-		<span slot="a">
-			Allow clients to book their own appointments on a public web page.
-		</span>
-	</Qa>
-<!--
-	<Qa>
-		<span slot="q">Bokning med Mobilt BankID</span>
-		<span slot="a">
-			Klienten loggar in i Kaddio med Mobilt BankID för att kunna boka tid. Passar dig som vill säkerställa identiteten på den som bokar.
-		</span>
-	</Qa>
--->
-	<Qa>
-		<span slot="q">Text reminders</span>
-		<span slot="a">
-			Choose if and when reminders are sent by text messages, before and after booked appointments.
-		</span>
-	</Qa>
+	</ModuleOverviewFeaturesSegment>
 
-	<Qa>
-		<span slot="q">Forms</span>
-		<span slot="a">
-			Automatically send forms in connection with bookings, for instance, assessment forms and health declarations.
-		</span>
-	</Qa>
+	<ModuleOverviewFeaturesSegment>
+		<span slot="title">Follow up</span>
+		
+		<ModuleOverviewFeature>Automated dispatch and collection of forms</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Referrals via letter, email and chat</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Export of medical records as PDF</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Overview of bookings, invoices and tasks</ModuleOverviewFeature>
+		<!-- <ModuleOverviewFeature>Samlingsräkning</ModuleOverviewFeature> -->
+		
+	</ModuleOverviewFeaturesSegment>
 
-	<Qa>
-		<span slot="q">Treatments</span>
-		<span slot="a">
-			Offer multiple treatments. Connect treatments to different users. Adjust text messages and emails to treatments.
-		</span>
-	</Qa>
+	<ModuleOverviewFeaturesSegment>
+		<span slot="title">Ensure</span>
+	
+		<ModuleOverviewFeature>Encrypted record keeping</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Access control of medical records</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Signing of medical record entries</ModuleOverviewFeature>
+		<ModuleOverviewFeature>History of editing of medical record entries</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Listing of unsigned medical record entries</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Log of reading and editing events for medical records</ModuleOverviewFeature>
+		
+	</ModuleOverviewFeaturesSegment>
+</ModuleOverviewFeaturesSection>  
 
-	<Qa>
-		<span slot="q">Group sessions</span>
-		<span slot="a">
-			Schedule time slots that are bookable for more than one participant.
-		</span>
-	</Qa>
+<QuotesSection>
+    <span slot="title">
+       What our customers say about Kaddio EMR       
+    </span>
 
-	<Qa>
-		<span slot="q">Time optimization</span>
-		<span slot="a">
-			Avoid unnecessary gaps in your schedule. Kaddio optimizes bookable times to the duration of your offered treatments and your scheduled working hours.
-		</span>
-	</Qa>
+	<!-- 	Fixa när Quotes EN är släppt
+	<QuoteEnRamzi />
+	<QuoteEnJimmy />
+	<QuoteEnJonas />
+	<QuoteEnAmanda />
+	<QuoteEnSofia /> -->
+	
+</QuotesSection>
 
-	<Qa>
-		<span slot="q">Waiting lists</span>
-		<span slot="a">
-			Create waiting lists with dates, notes and functions for delgation.
-		</span>
-	</Qa>
+<StatsSectionEn />
 
-	<Qa>
-		<span slot="q">Pay by card at booking</span>
-		<span slot="a">
-			Let your clients pay by card directly when booking online.
-		</span>
-	</Qa>
+<PricesEn />
 
-	<Qa>
-		<span slot="q">Invoicing</span>
-		<span slot="a">
-			Invoice bases are created automatically from scheduled bookings.
-		</span>
-	</Qa>
-	<Qa>
-		<span slot="q">Show bookable times on your website</span>
-		<span slot="a">
-			Integrate time booking directly on your website.
-		</span>
-	</Qa>
-	<Qa>
-		<span slot="q">Places</span>
-		<span slot="a">
-			Kaddio handles if you have multiple offices. Information in emails and reminders by text messages are adjusted to the indicated place.
-		</span>
-	</Qa>
+<ContactDanielEn />
 
-	<Qa>
-		<span slot="q">Book rooms</span>
-		<span slot="a">
-			Schedule your rooms. Kaddio ensures that treatments can only be booked if there are available rooms.
-		</span>
-	</Qa>
-<!--
-	<Qa>
-		<span slot="q">Personalliggare</span>
-		<span slot="a">
-			Kaddio kan användas som digital personalliggare genom Arbetstid i Kalendern, med hjälp av Loggning uppfylls även Skatteverkets regler kring kontroll.
-		</span>
-	</Qa>
--->
-	<Qa>
-		<span slot="q">Logs</span>
-		<span slot="a">
-			Reading and editing of medical records are logged. The log shows what actions have been performed for the records of a client; by whom, when, and from what IP address.
-		</span>
-	</Qa>
-
-</FaqSection>
+<!-- Inga listade formulär eftersom dessa är på svenska -->
