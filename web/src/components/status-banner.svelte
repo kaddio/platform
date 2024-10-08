b2075cf084d787759d42ee026cf7048757fbd9cf
rhymn <david@pnd.se>
2024-10-08 23:04:11 +0200

    hide bannercopy

--- web/src/components/status-banner.svelte
+++ web/src/components/status-banner.svelte
@@ -32,7 +32,6 @@
                 class="mx-2 inline h-0.5 w-0.5 fill-current"
                 aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg
             >Kaddio EU is experiencing intermittent outage due to underlying infrastructure with a hosting provider.
-            <a href="https://status.heroku.com/incidents/2684">här</a>.
         </p>
 
         <a href="/{lang}/support" class="whitespace-nowrap text-sm"
