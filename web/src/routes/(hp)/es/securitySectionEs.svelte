f689be5be15dd47e290393051fa6c3530753f018
LenaKaddio <lena@kaddio.com>
2024-10-11 14:47:19 +0200

    securitysectiones-legal-en

--- web/src/routes/(hp)/es/securitySectionEs.svelte
+++ web/src/routes/(hp)/es/securitySectionEs.svelte
@@ -2,7 +2,7 @@
 	import SecuritySection from "$components/securitySection.svelte";
 
 </script>
-<SecuritySection src="/es/legal/privacy">
+<SecuritySection src="/legal/privacy">
 	<span slot="title">Gestión de datos sensibles de manera segura.</span>
 	<span slot="subtitle">Todo en Kaddio está encriptado. Kaddio cuenta con soporte para autenticación de múltiples factores a través de Mobilt BankID y contraseñas de un solo uso a través de SMS.</span>
 </SecuritySection>
