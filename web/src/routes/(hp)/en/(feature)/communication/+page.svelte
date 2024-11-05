9e78a21f65045b82e0b701aff23b40f70fe17c33
LenaKaddio <lena@kaddio.com>
2024-11-05 15:27:22 +0100

    trans-module-feature

--- web/src/routes/(hp)/en/(feature)/communication/+page.svelte
+++ web/src/routes/(hp)/en/(feature)/communication/+page.svelte
@@ -20,39 +20,39 @@
 <svelte:head>
     <meta name="robots" content="noindex">
 
-    <title>Kaddio - Kommunikation</title>
-    <meta name="description" content="Chat och video för upp till 500 samtidiga deltagare" />
+    <title>Kaddio - Communication</title>
+    <meta name="description" content="Chat and video for up to 500 participants" />
 </svelte:head>
 
 
 <ModuleSectionWithImage src="/img/kaddio-screenshots/chat-sv.png" alt="Kaddio Journalsystem">
-	<span slot="h1">Kommunikation</span>
-	<span slot="title">Kaddio Chat och Video</span>
+	<span slot="h1">Communication</span>
+	<span slot="title">Kaddio Chat and Video</span>
 	<span slot="sub">
-		Kaddio Chat och Video gör att du kan kommunicera och dela information säkert med klienter och kollegor. Kaddio uppfyller Socialstyrelsens krav på stark autentisering.
+		Kaddio Chat och Video enables you to communicate and share information securely with 
+		clients and colleagues. Kaddio fulfils requirements for strong authentication.
 	</span>
 
 
 
     <ModuleFeatureWithIcon icon="comment">
-		<span slot="title">Kommunicera.</span>
+		<span slot="title">Communicate.</span>
 		<span slot="body">
-			Videosamtal, chat och kontaktformulär i en krypterad miljö. 
+			Video calls, chat and contact form in an encrypted environment. 
 		</span>
 	</ModuleFeatureWithIcon>
 
 	<ModuleFeatureWithIcon icon="share">
-		<span slot="title">Dela information.</span>
+		<span slot="title">Share information.</span>
 		<span slot="body">
-			Skicka känslig information, filer och bilder till klienter och kollegor på ett säkert.
+			Send sensitive information, files and images securely to clients and colleagues.
 		</span>
 	</ModuleFeatureWithIcon>
 
 	<ModuleFeatureWithIcon icon="lock-keyhole">
-		<span slot="title">Säkerhetsanpassa.</span>
+		<span slot="title">Secure.</span>
 		<span slot="body">
-			Kräv inloggning till videosamtal och chat med Mobilt BankID eller
-            tvåfaktorsautentisering.
+			Require login to video calls with two-factor authentication.
 		</span> 
 	</ModuleFeatureWithIcon>
 
