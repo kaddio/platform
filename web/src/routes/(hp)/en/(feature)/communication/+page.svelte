67ba786abae8b6197dce5a21bca3d8eb7e385e54
LenaKaddio <lena@kaddio.com>
2024-11-05 15:16:15 +0100

    structure

--- web/src/routes/(hp)/en/(feature)/communication/+page.svelte
+++ web/src/routes/(hp)/en/(feature)/communication/+page.svelte
@@ -1,65 +1,132 @@
 <script>
-	import Qa from "$components/qa.svelte";
-	import ModuleFeature from "$components/moduleFeature.svelte";
-	import ModuleSection from "$components/moduleSection.svelte";
-	import FaqSection from "$components/faqSection.svelte";
+    import ModuleSectionWithImage from '$components/moduleSectionWithImage.svelte';
+    import ModuleFeatureWithIcon from '$components/moduleFeatureWithIcon.svelte';
+    import ModuleOverviewFeaturesSection from "$components/moduleOverviewFeaturesSection.svelte";
+    import ModuleOverviewFeature from "$components/moduleOverviewFeature.svelte";
+    import ModuleOverviewFeaturesSegment from "$components/moduleOverviewFeaturesSegment.svelte";
+    import Quote from "$components/quote.svelte";
+    import QuotesSection from "$components/quotesSection.svelte";
+    import StatsSection from "$components/statsSection.svelte";
+    import StatsSectionPart from "$components/statsSectionPart.svelte";
+    import QuoteSvAmanda from '$components/quote-sv-amanda.svelte';
+	import QuoteSvJimmy from '$components/quote-sv-jimmy.svelte';
+	import QuoteSvJonas from '$components/quote-sv-jonas.svelte';
+	import QuoteSvKim from '$components/quote-sv-kim.svelte';
+	import QuoteSvRamzi from '$components/quote-sv-ramzi.svelte';
+	import QuoteSvSofia from '$components/quote-sv-sofia.svelte';
+	import QuoteSvUlrika from '$components/quote-sv-ulrika.svelte';
+	import ContactDanielSv from '$components/contactDanielSv.svelte';
+	import PricesSv from '$components/pricesSv.svelte';
 </script>
 
 <svelte:head>
-	<title>Kaddio - Communication</title>
-	<meta name="description" content="Communicate securely and encrypted via chat or video. With two or 500 participants." />
+    <meta name="robots" content="noindex">
+
+    <title>Kaddio - Kommunikation</title>
+    <meta name="description" content="Chat och video för upp till 500 samtidiga deltagare" />
 </svelte:head>
 
-<ModuleSection>
-	<span slot="title">Kaddio Communication</span>
-	<span slot="sub">Communicate securely and encrypted via chat or video. With two or 500 participants.</span>
 
-	<!-- <ModuleFeature>
-		<span slot="title">Secure</span>
-		<span slot="body">
-			Automatisera ditt schema och låt dina kunder boka och betala själva eller ta kontroll över varje minut. Bokningstyper kan konfigureras i detalj och detta hjälper dig sen i bokningen så att det blir lätt där. Automatiskta påminnelser
-		</span>
-	</ModuleFeature>
+<ModuleSectionWithImage src="/img/kaddio-screenshots/chat-sv.png" alt="Kaddio Journalsystem">
+	<span slot="h1">Kommunikation</span>
+	<span slot="title">Kaddio Chat och Video</span>
+	<span slot="sub">
+		Kaddio Chat och Video gör att du kan kommunicera och dela information säkert med klienter och kollegor. Kaddio uppfyller Socialstyrelsens krav på stark autentisering.
+	</span>
 
-	<ModuleFeature>
-		<span slot="title">Simple</span>
-		<span slot="body">
-			Det är enkelt att byta till video..
-		</span>
-	</ModuleFeature>
 
-	<ModuleFeature>
-		<span slot="title">Integrated</span>
+
+    <ModuleFeatureWithIcon icon="comment">
+		<span slot="title">Kommunicera.</span>
 		<span slot="body">
-			Kaddio Video har stöd för minst 500 deltagare samtidigt.
+			Videosamtal, chat och kontaktformulär i en krypterad miljö. 
 		</span>
-	</ModuleFeature> -->
-</ModuleSection>
+	</ModuleFeatureWithIcon>
+
+	<ModuleFeatureWithIcon icon="share">
+		<span slot="title">Dela information.</span>
+		<span slot="body">
+			Skicka känslig information, filer och bilder till klienter och kollegor på ett säkert.
+		</span>
+	</ModuleFeatureWithIcon>
+
+	<ModuleFeatureWithIcon icon="lock-keyhole">
+		<span slot="title">Säkerhetsanpassa.</span>
+		<span slot="body">
+			Kräv inloggning till videosamtal och chat med Mobilt BankID eller
+            tvåfaktorsautentisering.
+		</span> 
+	</ModuleFeatureWithIcon>
+
+</ModuleSectionWithImage>
+
+<ModuleOverviewFeaturesSection>
+	<span slot="pre">Innehåll</span>
+	<span slot="title">Detta är Kaddio Chat och Video</span>
+	<span slot="body">Med Kaddio Chat och Video kommunicerar du säkert med klienter och kollegor. 
+		<br>Allt i Kaddio är krypterat.</span>
+	
+    <ModuleOverviewFeaturesSegment>
+		<span slot="title">Kommunicera</span>
+		
+		<ModuleOverviewFeature>Videosamtal för 2-500 deltagare</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Skärmdelning i videosamtal</ModuleOverviewFeature>
+        <ModuleOverviewFeature>Chat med klienter och kollegor</ModuleOverviewFeature>
+        <ModuleOverviewFeature>Gruppchat</ModuleOverviewFeature>
+		<ModuleOverviewFeature>E-postmeddelande till klient från chatten</ModuleOverviewFeature>
+        <ModuleOverviewFeature>Kontaktformulär för klient</ModuleOverviewFeature>
+
+	</ModuleOverviewFeaturesSegment>
+
+	<ModuleOverviewFeaturesSegment>
+		<span slot="title">Dela information</span>
+	
+		<ModuleOverviewFeature>
+			Bifoga filer, till exempel journalanteckningar eller annan känslig information.</ModuleOverviewFeature>
+		<ModuleOverviewFeature>
+			Medgivandeblankett med signeringsfunktion </ModuleOverviewFeature>
+		<ModuleOverviewFeature>Inspelning av videosamtal</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Automatiska utskick av formulär från kontaktformuläret</ModuleOverviewFeature>
+		
+	</ModuleOverviewFeaturesSegment>
+
+	<ModuleOverviewFeaturesSegment>
+		<span slot="title">Säkerhetsanpassa</span>
+			
+		<ModuleOverviewFeature>Uppfyller Socialstyrelsens krav på stark autentisering</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Inloggning för klient med Mobilt BankID</ModuleOverviewFeature>
+		<ModuleOverviewFeature>Inloggning för klient med tvåfaktorsautentisering via SMS</ModuleOverviewFeature>
+			
+	</ModuleOverviewFeaturesSegment>
   
+</ModuleOverviewFeaturesSection>  
 
-<FaqSection>
-	<span slot="title">Details</span>
+<QuotesSection>
+    <span slot="title">
+        Vad våra kunder säger om <br>Kaddio Chat och Video       
+    </span>
+    <QuoteSvJimmy />
+    <QuoteSvJonas />
+    <QuoteSvRamzi />
+    <QuoteSvSofia />
+    
+</QuotesSection>
 
-	<Qa>
-		<span slot="q">Screen sharing</span>
-		<span slot="a">
-			Share what you see
-		</span>
-	</Qa>
+<StatsSection>
+	<StatsSectionPart>
+		<span slot="title">användare</span>
+		<span slot="sub">47 000+</span>
+	</StatsSectionPart>
+	<StatsSectionPart>
+		<span slot="title">bokningar per dygn</span>
+		<span slot="sub">1300+</span>
+	</StatsSectionPart>
+	<StatsSectionPart>
+		<span slot="title">videomöten per månad</span>
+		<span slot="sub">8500+</span>
+	</StatsSectionPart>
+</StatsSection>
 
-	<Qa>
-		<span slot="q">Record</span>
-		<span slot="a">
-			Video calls are recordable, the video is uploaded in the conversation and is visible to handlers for 24 hours.
-		</span>
-	</Qa>
+<PricesSv />
 
-	<Qa>
-		<span slot="q">Emojis and reactions</span>
-		<span slot="a">
-			Maybe not the most meaningful, but all the other major video providers have it right?
-		</span>
-	</Qa>
-
-
-</FaqSection>
+<ContactDanielSv />
