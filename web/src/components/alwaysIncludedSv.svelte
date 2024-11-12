e6134b28aeed9d661f14d38217f0f5cea0a396d3
LenaKaddio <lena@kaddio.com>
2024-11-12 15:37:10 +0100

    copy

--- web/src/components/alwaysIncludedSv.svelte
+++ web/src/components/alwaysIncludedSv.svelte
@@ -2,9 +2,9 @@
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-5">
       <div class="col-span-2">
-        <h2 class="text-base/7 font-semibold text-indigo-600">Innehåll</h2>
+        <h2 class="text-base/7 font-semibold text-indigo-600">Kaddio Core</h2>
         <p class="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Ingår alltid i Kaddio</p>
-        <p class="mt-6 text-base/7 text-gray-600">Ett abonnemang i Kaddio inkluderar mer än de moduler du väljer. Följande ingår för alla användare.</p>
+        <p class="mt-6 text-base/7 text-gray-600">Med Kaddio får du mycket på köpet, oavsett hur många moduler du använder eller hur många ni är på organisationen.</p>
       </div>
       <dl class="col-span-3 grid grid-cols-1 gap-x-8 gap-y-10 text-base/7 text-gray-600 sm:grid-cols-2 lg:gap-y-16">
         <div class="relative pl-9">
@@ -12,72 +12,72 @@
             <svg class="absolute left-0 top-1 h-5 w-5 text-indigo-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" />
             </svg>
-            Invite team members
+            Datalagring och backups
           </dt>
-          <dd class="mt-2">Rerum repellat labore necessitatibus reprehenderit molestiae praesentium.</dd>
+          <!-- <dd class="mt-2">Säker lagring och enkel åtkomst till data.</dd> -->
         </div>
         <div class="relative pl-9">
           <dt class="font-semibold text-gray-900">
             <svg class="absolute left-0 top-1 h-5 w-5 text-indigo-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" />
             </svg>
-            List view
+            Teknisk support och säljsupport
           </dt>
-          <dd class="mt-2">Corporis asperiores ea nulla temporibus asperiores non tempore assumenda aut.</dd>
+          <!-- <dd class="mt-2">Kostnadsfri teknisk support och säljsupport.</dd> -->
         </div>
         <div class="relative pl-9">
           <dt class="font-semibold text-gray-900">
             <svg class="absolute left-0 top-1 h-5 w-5 text-indigo-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" />
             </svg>
-            Keyboard shortcuts
+            Hjälpcenter med instruktioner
           </dt>
-          <dd class="mt-2">In sit qui aliquid deleniti et. Ad nobis sunt omnis. Quo sapiente dicta laboriosam.</dd>
+          <!-- <dd class="mt-2">Instruktioner för inställningar i Kaddio.</dd> -->
         </div>
         <div class="relative pl-9">
           <dt class="font-semibold text-gray-900">
             <svg class="absolute left-0 top-1 h-5 w-5 text-indigo-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" />
             </svg>
-            Calendars
+            Händelseloggar för journal, utskick och inställningar
           </dt>
-          <dd class="mt-2">Sed rerum sunt dignissimos ullam. Iusto iure occaecati voluptate eligendi.</dd>
+          <!-- <dd class="mt-2">Sed rerum sunt dignissimos ullam. Iusto iure occaecati voluptate eligendi.</dd> -->
         </div>
         <div class="relative pl-9">
           <dt class="font-semibold text-gray-900">
             <svg class="absolute left-0 top-1 h-5 w-5 text-indigo-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" />
             </svg>
-            Notifications
+            Statistikverktyg med export av rapporter
           </dt>
-          <dd class="mt-2">Quos inventore harum enim nesciunt. Aut repellat rerum omnis adipisci.</dd>
+          <!-- <dd class="mt-2">Statistikverktyg.</dd> -->
         </div>
         <div class="relative pl-9">
           <dt class="font-semibold text-gray-900">
             <svg class="absolute left-0 top-1 h-5 w-5 text-indigo-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" />
             </svg>
-            Boards
+            Hemsida
           </dt>
-          <dd class="mt-2">Quae sit sunt excepturi fugit veniam voluptatem ipsum commodi.</dd>
+          <!-- <dd class="mt-2">Quae sit sunt excepturi fugit veniam voluptatem ipsum commodi.</dd> -->
         </div>
         <div class="relative pl-9">
           <dt class="font-semibold text-gray-900">
             <svg class="absolute left-0 top-1 h-5 w-5 text-indigo-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" />
             </svg>
-            Reporting
+            Söktjänst
           </dt>
-          <dd class="mt-2">Eos laudantium repellat sed architecto earum unde incidunt.</dd>
+          <!-- <dd class="mt-2">Eos laudantium repellat sed architecto earum unde incidunt.</dd> -->
         </div>
         <div class="relative pl-9">
           <dt class="font-semibold text-gray-900">
             <svg class="absolute left-0 top-1 h-5 w-5 text-indigo-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" />
             </svg>
-            Mobile app
+            Uppgifter
           </dt>
-          <dd class="mt-2">Nulla est saepe accusamus nostrum est est fugit omnis.</dd>
+          <!-- <dd class="mt-2">Att-göra-listor med notiser och delegeringsfunktion.</dd> -->
         </div>
       </dl>
     </div>
