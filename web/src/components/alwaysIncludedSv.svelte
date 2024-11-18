5816b9e7146eacc69564ad7727e40cba03ac6ff0
LenaKaddio <lena@kaddio.com>
2024-11-18 11:50:27 +0100

    copy-pricingsection

--- web/src/components/alwaysIncludedSv.svelte
+++ web/src/components/alwaysIncludedSv.svelte
@@ -21,7 +21,7 @@
             <svg class="absolute left-0 top-1 h-5 w-5 text-indigo-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" />
             </svg>
-            Teknisk support
+            Support
           </dt>
           <dd class="mt-2">Kostnadsfri teknisk support och säljsupport.</dd>
         </div>
