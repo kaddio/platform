9412958677a3584801a8413a0b8d6ee9b639b2cd
LenaKaddio <lena@kaddio.com>
2024-11-29 14:54:48 +0100

    gen-structure-component-timeline

--- web/src/components/npo-timeline.svelte
+++ web/src/components/npo-timeline.svelte
@@ -6,44 +6,44 @@
           <svg viewBox="0 0 4 4" class="mr-4 h-1 w-1 flex-none" aria-hidden="true">
             <circle cx="2" cy="2" r="2" fill="currentColor" />
           </svg>
-          Mar 2016
+          <slot name="date-one" />
           <div class="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0" aria-hidden="true"></div>
         </time>
-        <p class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">Company founded</p>
-        <p class="mt-1 text-base leading-7 text-gray-600">Kaddio AB, at the time named ePsy is founded by Arvid and David.</p>
+        <p class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900"><slot name="title-one"/></p>
+        <p class="mt-1 text-base leading-7 text-gray-600"><slot name="sub-one" /></p>
       </div>
       <div>
         <time datetime="2021-12" class="flex items-center text-sm font-semibold leading-6 text-oldpink">
           <svg viewBox="0 0 4 4" class="mr-4 h-1 w-1 flex-none" aria-hidden="true">
             <circle cx="2" cy="2" r="2" fill="currentColor" />
           </svg>
-          Dec 2016
+          <slot name="date-two" />
           <div class="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0" aria-hidden="true"></div>
         </time>
-        <p class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">3rd employee</p>
-        <p class="mt-1 text-base leading-7 text-gray-600">Ylva joins our team.</p>
+        <p class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900"><slot name="title-two"/></p>
+        <p class="mt-1 text-base leading-7 text-gray-600"><slot name="sub-two" /></p>
       </div>
       <div>
         <time datetime="2022-02" class="flex items-center text-sm font-semibold leading-6 text-oldpink">
           <svg viewBox="0 0 4 4" class="mr-4 h-1 w-1 flex-none" aria-hidden="true">
             <circle cx="2" cy="2" r="2" fill="currentColor" />
           </svg>
-          Nov 2017
+          <slot name="date-three" />
           <div class="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0" aria-hidden="true"></div>
         </time>
-        <p class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">1st paying customer</p>
-        <p class="mt-1 text-base leading-7 text-gray-600">Until now, Kaddio was freeware. We wanted to start charging our users 6 months earlier but it took longer than we expected to "automate" the billing process :)</p>
+        <p class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900"><slot name="title-three"/></p>
+        <p class="mt-1 text-base leading-7 text-gray-600"><slot name="sub-three" /></p>
       </div>
       <div>
         <time datetime="2022-12" class="flex items-center text-sm font-semibold leading-6 text-oldpink">
           <svg viewBox="0 0 4 4" class="mr-4 h-1 w-1 flex-none" aria-hidden="true">
             <circle cx="2" cy="2" r="2" fill="currentColor" />
           </svg>
-          Dec 2020
+          <slot name="date-four" />
           <div class="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0" aria-hidden="true"></div>
         </time>
-        <p class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">Black numbers!</p>
-        <p class="mt-1 text-base leading-7 text-gray-600">Kaddio has been independent from external investors since the beginning, now we are also economically independent from our parent company.</p>
+        <p class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900"><slot name="title-four"/></p>
+        <p class="mt-1 text-base leading-7 text-gray-600"><slot name="sub-four" /></p>
       </div>
     </div>
   </div>
--- web/src/routes/(hp)/sv/(feature)/npo/+page.svelte
+++ web/src/routes/(hp)/sv/(feature)/npo/+page.svelte
@@ -5,6 +5,8 @@ import WithStickyScreenshot from "$components/withStickyScreenshot.svelte";
 import ContentCentered from "$components/contentCentered.svelte";
 import WithImageTitles from "$components/withImageTitles.svelte";
 import Timeline from "$components/timeline.svelte";
+import NpoTimeline from "$components/npo-timeline.svelte";
+
 // import ContactDanielSv from "$components/contactDanielSv.svelte";
 // import PricesSv from "$components/pricesSv.svelte";
 // import StatsSectionSv from "$components/statsSectionSv.svelte";
@@ -18,6 +20,24 @@ import Timeline from "$components/timeline.svelte";
 </svelte:head>
 
 <WithStickyScreenshot />
+
+<NpoTimeline>
+	<span slot="date-one">Dec 2024</span>
+	<span slot="title-one">Testrubrik 1</span>
+	<span slot="sub-one">Test av brödtext 1</span>
+
+	<span slot="date-two">Jan 2025</span>
+	<span slot="title-two">Testrubrik 2</span>
+	<span slot="sub-two">Test av brödtext 2</span>
+
+	<span slot="date-three">Mar 2025</span>
+	<span slot="title-three">Testrubrik 3</span>
+	<span slot="sub-three">Test av brödtext 3</span>
+
+	<span slot="date-four">Jun 2025</span>
+	<span slot="title-four">Testrubrik 4</span>
+	<span slot="sub-four">Test av brödtext 4</span>
+</NpoTimeline>
 
 <!-- <ModuleSectionWithImage src="/img/kaddio-screenshots/chat-sv.png" alt="Kaddio Journalsystem">
 	<span slot="h1">Kommunikation</span>
