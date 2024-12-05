5137e5b1dc4d3a9d8121c3b5e82dcfc1460721a7
LenaKaddio <lena@kaddio.com>
2024-12-05 13:54:20 +0100

    addition

--- web/src/routes/(hp)/sv/(feature)/1177/+page.svelte
+++ web/src/routes/(hp)/sv/(feature)/1177/+page.svelte
@@ -21,12 +21,13 @@ import StatsSectionSv from "$components/statsSectionSv.svelte";
 <WithStickyScreenshot>
 	<span slot="pre">På gång i Kaddio</span>
 	<span slot="h1">Integration till <br>NPÖ och 1177 </span>
-	<span slot="sub">Kaddio bygger integration till 1177 och nationell patientöversikt för journaldata. 
+	<span slot="sub">Kaddio bygger integration till 1177 och nationell patientöversikt för journaldata som en tilläggstjänst till modulen Journal. 
 		Lanseringen beräknas till våren 2025.</span>
 	<span slot="p1">Nationell patientöversikt, NPÖ, är ett verktyg som gör att vårdpersonal kan få en samlad bild av 
 		en klients journaler hos samtliga vårdgivare. De uppgifter som vårdpersonal kan se i NPÖ kan också klienten 
 		själv ta del av genom att logga in på 1177.se.</span>
-	<span slot="p2"><br>Kaddio bygger nu en integration till NPÖ och 1177 vilket innebär nya möjligheter för dig som journalför i Kaddio: </span>
+	<span slot="p2"><br>Kaddio bygger nu en integration till NPÖ och 1177 vilket innebär nya möjligheter för dig som 
+		journalför i Kaddio: </span>
 	<span slot="strong1">Lagkrav &nbsp;</span>
 	<span slot="li1"> Uppfyll de krav som ställs för att kunna arbeta mot region och kommun.</span>
 	<span slot="strong2">Access &nbsp;</span>
@@ -36,7 +37,7 @@ import StatsSectionSv from "$components/statsSectionSv.svelte";
 	<span slot="li3">Få fördelarna med sammanhållen journalföring samtidigt som du jobbar i ett användarvänligt och webbaserat system.</span>
 	<span slot="h3">Vad händer nu?</span>
 	<span slot="p3">Kaddio har godkänts som agent av Inera, det bolag som utvecklar och förvaltar digitala tjänster för invånare genom 1177. 
-		Kodbygget är i full gång och lanseringen av integrationen kommer preliminärt att ske under våren 2025.<br>
+		Kodbygget är i full gång och lanseringen av integrationen kommer preliminärt att ske under våren 2025. Integrationen blir en tillägstjänst till modulen Journal.<br>
 		<br>
 	Är du nyfiken eller har frågor? Kontakta oss på support@kaddio.com.</span>
 </WithStickyScreenshot>
