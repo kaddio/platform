71d99a684287d2ea63fb1d5b09874c025eb64cf0
rhymn <david@pnd.se>
2025-02-16 09:20:44 +0100

    log

--- proxy/index.js
+++ proxy/index.js
@@ -61,6 +61,7 @@ https.createServer(options, async (req, res) => {
   console.log(req.url);
 
   const secret = crypto.createHash('sha256').update(privateKey).digest('hex');
+  
 
   try {
     const backendResponse = await fetch(target + req.url, {
