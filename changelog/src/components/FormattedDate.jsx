d6c0595fae71ae2938a73ac86fa14149c7f1ee81
rhymn <david@pnd.se>
2025-03-05 21:19:38 +0100

    add coming if in future

--- changelog/src/components/FormattedDate.jsx
+++ changelog/src/components/FormattedDate.jsx
@@ -7,9 +7,12 @@ const dateFormatter = new Intl.DateTimeFormat('en-US', {
 
 export function FormattedDate({ date, ...props }) {
   date = typeof date === 'string' ? new Date(date) : date
+  const now = new Date()
+  const isFutureDate = date > now
 
   return (
     <time dateTime={date.toISOString()} {...props}>
+      {isFutureDate ? '(Coming) ' : ''}
       {dateFormatter.format(date)}
     </time>
   )
