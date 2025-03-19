bf992ada0c6c6addad9ada9c2fb7e9e8293f0b4f
rhymn <david@pnd.se>
2025-03-19 20:48:30 +0100

    without pwd

--- proxy/create-certs.mjs
+++ proxy/create-certs.mjs
@@ -37,13 +37,13 @@ console.log("Creating the self-signed certificate for the CA...");
 await $`openssl req -new -x509 -key ${caKey} -passin pass:${password} -out ${caCert} -days 365 -subj "/CN=CA"`;
 
 console.log("Creating a Certificate Signing Request (CSR) for the server...");
-await $`openssl req -new -key ${serverKey} -passin pass:${password} -out ${serverCsr} -subj "/CN=kaddiotestarnpo.kaddio.com"`;
+await $`openssl req -new -key ${serverKey} -passin pass:${password} -out ${serverCsr} -subj "/CN=localhost"`;
 
 console.log("Signing the server CSR with the CA to generate the server certificate...");
 await $`openssl x509 -req -in ${serverCsr} -CA ${caCert} -CAkey ${caKey} -passin pass:${password} -CAcreateserial -out ${serverCert} -days 365`;
 
 console.log("Generating the private key for the client...");
-await $`openssl genrsa -aes256 -passout pass:${password} -out ${clientKey} 2048`;
+await $`openssl genrsa -out ${clientKey} 2048`;
 
 console.log("Creating a Certificate Signing Request (CSR) for the client...");
 await $`openssl req -new -key ${clientKey} -passin pass:${password} -out ${clientCsr} -subj "/CN=client"`;
