b8984df9241e4e6ee97121876c2948a9a962b04b
Ivanna Junuzovic <ivanna.junuzovic@live.se>
2025-03-25 14:06:12 +0100

    fakturering es

--- web/src/routes/(hp)/es/(feature)/sistema-de-facturacion/+page.svelte
+++ web/src/routes/(hp)/es/(feature)/sistema-de-facturacion/+page.svelte
@@ -1,95 +1,104 @@
 <script>
-	import Qa from "$components/qa.svelte";
-	import ModuleFeature from "$components/moduleFeature.svelte";
-	import ModuleSection from "$components/moduleSection.svelte";
-	import FaqSection from "$components/faqSection.svelte";
+    import ModuleSectionWithImage from "$components/moduleSectionWithImage.svelte";
+    import ModuleFeatureWithIcon from "$components/moduleFeatureWithIcon.svelte";
+    import ModuleOverviewFeaturesSection from "$components/moduleOverviewFeaturesSection.svelte";
+    import ModuleOverviewFeature from "$components/moduleOverviewFeature.svelte";
+    import ModuleOverviewFeaturesSegment from "$components/moduleOverviewFeaturesSegment.svelte";
+    import QuotesSection from "$components/quotesSection.svelte";
+    import PricesEs from "$components/pricesEs.svelte";
+    import QuoteEnJimmy from "$components/quoteEnJimmy.svelte";
+    import QuoteEnJonas from "$components/quoteEnJonas.svelte";
+    import QuoteEnRamzi from "$components/quoteEnRamzi.svelte";
+    import QuoteEnSofia from "$components/quoteEnSofia.svelte";
+    import StatsSectionEn from "$components/statsSectionEn.svelte";
+    import ContactDanielEs from "$components/contactDanielEs.svelte";
 
 </script>
 
 <svelte:head>
-	<title>Kaddio - Sistema de facturación</title>
-	<meta name="description" content="Las facturas se crean automáticamente en función de las citas reservadas. También puedes crearlas de forma independiente. Todas las facturas se guardan en Kaddio." />
+    <title>Facturación para pequeñas y medianas empresas</title>
+    <meta name="description" content="Kaddio Facturación es un programa de facturación completo para pequeñas y medianas empresas." />
 </svelte:head>
 
-<ModuleSection>
-	<span slot="title">Sistema de facturación</span>
-	<span slot="sub">Las facturas se crean automáticamente en función de las citas reservadas. También puedes crearlas de forma independiente. Todas las facturas se guardan en Kaddio.</span>
-<!-- 
-	<ModuleFeature>
-		<span slot="title">Spara tid</span>
-		<span slot="body">
-			Spara tid med integrerad fakturering. Kaddio föreslår automatiskt underlag utifrån bokade tider eller journalanteckningar så att du skicka en fakturera med tre klick.
-		</span>
-	</ModuleFeature>
-	 -->
-</ModuleSection>
-  
+<ModuleSectionWithImage src="/img/kaddio-screenshots/invoice-en.png" alt="Facturación de Kaddio">
+    <span slot="h1">Sistema de facturación</span>
+    <span slot="title">Kaddio Facturación</span>
+    <span slot="sub">
+        Kaddio Facturación es un sistema de facturación sencillo para pequeñas y medianas empresas. Combínalo con Kaddio Reservas o Kaddio Sistema de Historias Clínicas para facturar de manera rápida y flexible.
+    </span>
 
-<FaqSection>
-	<span slot="title">Funciones</span>
+    <ModuleFeatureWithIcon icon="timer">
+        <span slot="title">Ahorra tiempo.</span>
+        <span slot="body">
+            Genera facturas automáticamente a partir de reservas o notas de registro.
+        </span>
+    </ModuleFeatureWithIcon>
 
-	<Qa>
-		<span slot="q">Facturas</span>
-		<span slot="a">
-			Basándose en las reservas, Kaddio crea automáticamente las facturas.
-		</span>
-	</Qa>
+    <ModuleFeatureWithIcon icon="arrows-spin">
+        <span slot="title">Automatiza.</span>
+        <span slot="body">
+            Deja que Kaddio gestione recordatorios y conciliaciones de pagos.
+        </span>
+    </ModuleFeatureWithIcon>
 
-	<Qa>
-		<span slot="q">Envío de facturas</span>
-		<span slot="a">
-			Envía tus facturas por correo postal, correo electrónico, a través del chat de Kaddio o como factura electrónica.
-		</span>
-	</Qa>
+    <ModuleFeatureWithIcon icon="envelope">
+        <span slot="title">Entrega.</span>
+        <span slot="body">
+            Envía facturas por correo electrónico, carta o en el chat. <br>Envía facturas electrónicas a empresas de todo el mundo.
+        </span>
+    </ModuleFeatureWithIcon>
+    
+</ModuleSectionWithImage>
 
-	<Qa>
-		<span slot="q">Recordatorios</span>
-		<span slot="a">
-			Envía recordatorios de forma automática o manual.
-		</span>
-	</Qa>
+<ModuleOverviewFeaturesSection>
+    
+    <span slot="title">Esto es Kaddio Facturación</span>
+    <span slot="body">Con Kaddio Facturación envías facturas fácilmente a partir de reservas o notas de registro. Gestiona todo en el mismo sistema y dedica más tiempo a tus clientes.</span>
 
-	<Qa>
-		<span slot="q">Marcado automático</span>
-		<span slot="a">
-			Kaddio marca las facturas automáticamente cuando el cliente paga mediante transferencia bancaria.
-		</span>
-	</Qa>
+    <ModuleOverviewFeaturesSegment>
+        <span slot="title">Ahorra tiempo</span>
+        <ModuleOverviewFeature>Duplicación de facturas</ModuleOverviewFeature>
+        <ModuleOverviewFeature>Base contable con exportación en SIE-4, CSV y para Excel</ModuleOverviewFeature>
+        <ModuleOverviewFeature>Lista de clientes para empresas</ModuleOverviewFeature>
+        <ModuleOverviewFeature>Libro auxiliar de clientes</ModuleOverviewFeature>
+        <ModuleOverviewFeature>Informes de ingresos</ModuleOverviewFeature>
+                
+    </ModuleOverviewFeaturesSegment>
 
-	<Qa>
-		<span slot="q">Factura electrónica</span>
-		<span slot="a">
-			Envía facturas electrónicas con Kaddio según el estándar PEPPOL de la UE.
-		</span>
-	</Qa>
+    <ModuleOverviewFeaturesSegment>
+        <span slot="title">Automatiza</span>
 
-	<Qa>
-		<span slot="q">Cofacturación</span>
-		<span slot="a">
-			Factura varios clientes a una misma empresa, como un grupo o una compañía de seguros.
-		</span>
-	</Qa>
+        <ModuleOverviewFeature>Facturas a partir de reservas o notas de registro</ModuleOverviewFeature>
+        <ModuleOverviewFeature>Conciliación de pagos con Bankgirot o cuenta bancaria</ModuleOverviewFeature>
+        <ModuleOverviewFeature>Recordatorios de pago</ModuleOverviewFeature>
+        <ModuleOverviewFeature>Clasificación para exportación a cobranzas</ModuleOverviewFeature>
+                
+    </ModuleOverviewFeaturesSegment>
 
-	<Qa>
-		<span slot="q">Exportación de datos para empresa de cobros</span>
-		<span slot="a">
-			Exporta facturas impagadas que luego podrán ser importadas a una empresa de cobros.
-		</span>
-	</Qa>
-
-	<Qa>
-		<span slot="q">Contabilidad</span>
-		<span slot="a">
-			Exporta facturas como archivo .sie o archivo .csv para cargar en un programa de contabilidad.
-		</span>
-	</Qa>
-
-	<Qa>
-		<span slot="q">Monedas</span>
-		<span slot="a">
-			Envía facturas en las 33 monedas más comunes del mundo, con tipos de cambio automáticos actualizados diariamente por el BCE. Kaddio publica automáticamente las ganancias y pérdidas de tipo de cambio en la exportación .sie.
-		</span>
-	</Qa>
+    <ModuleOverviewFeaturesSegment>
+        <span slot="title">Entrega</span>
+        <ModuleOverviewFeature>Facturación por correo electrónico, carta y chat</ModuleOverviewFeature>
+        <ModuleOverviewFeature>Facturación electrónica a empresas de todo el mundo</ModuleOverviewFeature>
+        <ModuleOverviewFeature>Factura consolidada para clientes empresariales</ModuleOverviewFeature>
 
 
-</FaqSection>
+    </ModuleOverviewFeaturesSegment>
+</ModuleOverviewFeaturesSection>  
+
+<QuotesSection>
+    <span slot="title">
+        Lo que nuestros clientes dicen sobre Kaddio Facturación       
+    </span>
+
+    <QuoteEnJimmy />
+    <QuoteEnJonas/>
+    <QuoteEnRamzi />
+    <QuoteEnSofia />
+
+</QuotesSection>
+
+<StatsSectionEn />
+
+<PricesEs />
+
+<ContactDanielEs />
