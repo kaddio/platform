568009ed9dbc603ea641d72f07faf809ac0ab19b
Ivanna Junuzovic <ivanna.junuzovic@live.se>
2025-03-25 14:27:49 +0100

    quotes och stats för es

---
+++ web/src/components/quote-es-amanda.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Quote from "$components/quote.svelte";
+</script>
+
+<Quote>
+    Kaddio es el programa de registros que he elegido por su simplicidad. El programa es fácil de navegar y se adapta perfectamente a mis necesidades, tanto en el registro como en la gestión de reservas.
+    <span slot="quoted">
+        Amanda Follin
+    </span>
+    <span slot="author">
+        Terapeuta de masaje certificada, <a href="https://www.endorfinkliniken.com" target="_blank" rel="noreferrer">Endorfinkliniken</a>
+    </span>
+</Quote>
---
+++ web/src/components/quote-es-sofia.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Quote from "$components/quote.svelte";
+</script>
+
+<Quote>
+    El programa tiene todo lo que necesito y es fácil de usar y adaptar a mis necesidades. Realmente puedo recomendar este sistema de registros.
+    <span slot="quoted">
+        Sofia Viotti
+    </span>
+    <span slot="author">
+        Psicóloga licenciada, Compassionfocus
+    </span>
+</Quote>
