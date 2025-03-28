7509848f777b6ff88bc79cef11b00b588a9c8ef5
Ivanna Junuzovic <ivanna.junuzovic@live.se>
2025-03-28 12:14:35 +0100

    restore components

--- web/src/components/quoteEnRamzi.svelte
+++ web/src/components/quoteEnRamzi.svelte
@@ -1,33 +1,15 @@
-<script lang="ts">
+
+<script>
     import Quote from "$components/quote.svelte";
-
-    export let lang: "eng" | "sv" | "es" = "eng"; 
-
-    const texts: Record<"eng" | "sv" | "es", { quote: string; quoted: string; author: string }> = {
-        eng: {
-            quote: "At Din Psykolog, we have had great help from Kaddio in our work with developing digital healthcare for the future, and they have been most accommodating with developing specific functions for our needs. We warmly recommend Kaddio!",
-            quoted: "Ramzi Jelassi",
-            author: 'Founder, <a href="https://dinpsykolog.se" target="_blank" rel="noreferrer">Din Psykolog</a>'
-        },
-        sv: {
-            quote: "På Din Psykolog har vi haft stor hjälp av Kaddio i vårt arbete med att utveckla framtidens digitala vård, och de har varit mycket tillmötesgående med att utveckla specifika funktioner för våra behov. Vi rekommenderar varmt Kaddio!",
-            quoted: "Ramzi Jelassi",
-            author: 'Grundare, <a href="https://dinpsykolog.se" target="_blank" rel="noreferrer">Din Psykolog</a>'
-        },
-        es: {
-            quote: "En Din Psykolog, hemos recibido una gran ayuda de Kaddio en nuestro trabajo para desarrollar la atención médica digital del futuro, y han sido muy complacientes al desarrollar funciones específicas para nuestras necesidades. ¡Recomendamos calurosamente a Kaddio!",
-            quoted: "Ramzi Jelassi",
-            author: 'Fundador, <a href="https://dinpsykolog.se" target="_blank" rel="noreferrer">Din Psykolog</a>'
-        }
-    };
 </script>
 
 <Quote>
-    {texts[lang].quote}
+    At Din Psykolog, we have had great help from Kaddio in our work with developing digital healthcare for the future, 
+    and they have been most accommodating with developing specific functions for our needs. We warmly recommend Kaddio!
     <span slot="quoted">
-        {@html texts[lang].quoted}
+        Ramzi Jelassi
     </span>
     <span slot="author">
-        {@html texts[lang].author}
+        Founder, <a href="https://dinpsykolog.se" target="_blank" rel="noreferrer">Din Psykolog</a>
     </span>
 </Quote>
--- web/src/components/statsSectionSv.svelte
+++ web/src/components/statsSectionSv.svelte
@@ -1,40 +1,19 @@
-<script lang="ts">
+<script>
     import StatsSection from "$components/statsSection.svelte";
     import StatsSectionPart from "$components/statsSectionPart.svelte";
-
-    export let lang: "sv" | "eng" | "es" = "sv"; 
-
-
-    const texts: Record<"sv" | "eng" | "es", { users: string; bookings: string; meetings: string }> = {
-        sv: {
-            users: "användare",
-            bookings: "bokningar per dygn",
-            meetings: "videomöten per månad"
-        },
-        eng: {
-            users: "users",
-            bookings: "bookings per day",
-            meetings: "video meetings per month"
-        },
-        es: {
-            users: "usuarios",
-            bookings: "reservas por día",
-            meetings: "reuniones por video al mes"
-        }
-    };
 </script>
 
 <StatsSection>
-    <StatsSectionPart>
-        <span slot="title">{texts[lang].users}</span>
-        <span slot="sub">47 000+</span>
-    </StatsSectionPart>
-    <StatsSectionPart>
-        <span slot="title">{texts[lang].bookings}</span>
-        <span slot="sub">1300+</span>
-    </StatsSectionPart>
-    <StatsSectionPart>
-        <span slot="title">{texts[lang].meetings}</span>
-        <span slot="sub">8500+</span>
-    </StatsSectionPart>
+	<StatsSectionPart>
+		<span slot="title">användare</span>
+		<span slot="sub">47 000+</span>
+	</StatsSectionPart>
+	<StatsSectionPart>
+		<span slot="title">bokningar per dygn</span>
+		<span slot="sub">1300+</span>
+	</StatsSectionPart>
+	<StatsSectionPart>
+		<span slot="title">videomöten per månad</span>
+		<span slot="sub">8500+</span>
+	</StatsSectionPart>
 </StatsSection>
